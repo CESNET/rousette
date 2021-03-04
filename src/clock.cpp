433d13e99f1c3b079a244133c34b226b65c2bb8f
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-03-04 14:59:58 +0100

    Refactor text/event-stream handler into a separate class
    Change-Id: Ie5eeb99c11bc377624902aef6365057ce460954d

--- src/clock.cpp
+++ src/clock.cpp
@@ -1,120 +1,43 @@
+/*
+ * Copyright (C) 2016-2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
 #include <boost/asio/io_service.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/signals2.hpp>
 #include <chrono>
-#include <list>
 #include <nghttp2/asio_http2_server.h>
 #include <spdlog/spdlog.h>
 #include <thread>
+#include "http/EventStream.h"
 
-using namespace nghttp2::asio_http2;
 using namespace std::literals;
-
-using Signal = boost::signals2::signal<void(const std::string& message)>;
-
-class Client {
-    const server::response& res;
-    enum State {
-        HasEvents,
-        WaitingForEvents,
-    };
-    std::atomic<State> state;
-
-    std::list<std::string> queue;
-    mutable std::mutex mtx;
-    boost::signals2::scoped_connection subscription;
-
-    size_t send_chunk(uint8_t* destination, std::size_t len, uint32_t* data_flags [[maybe_unused]])
-    {
-        std::size_t written{0};
-        std::lock_guard lock{mtx};
-        if (state != HasEvents) throw std::logic_error{std::to_string(__LINE__)};
-        while (!queue.empty()) {
-            auto num = std::min(queue.front().size(), len - written);
-            std::copy_n(queue.front().begin(), num, destination + written);
-            written += num;
-            if (num < queue.front().size()) {
-                queue.front() = queue.front().substr(num);
-                spdlog::debug("{} send_chunk: partial write", (void*)this);
-                return written;
-            }
-            queue.pop_front();
-            spdlog::debug("{} send_chunk: sent one event", (void*)this);
-        }
-        state = WaitingForEvents;
-        return written;
-    }
-
-public:
-    Client(const server::request& req, const server::response& res, Signal& signal)
-    : res{res}
-    , state{WaitingForEvents}
-    , subscription{signal.connect([this](const auto& msg) {
-        enqueue(msg);
-    })}
-    {
-        spdlog::warn("{}: {} {} {}", (void*)this, boost::lexical_cast<std::string>(req.remote_endpoint()), req.method(), req.uri().raw_path);
-        res.write_head(200, {{"content-type", {"text/event-stream", false}}});
-    }
-
-    void onClose([[maybe_unused]] const uint32_t ec)
-    {
-        spdlog::error("{} onClose", (void*)this);
-        subscription.disconnect();
-    }
-
-    ssize_t process(uint8_t* destination, std::size_t len, uint32_t* data_flags)
-    {
-        spdlog::trace("{} process", (void*)this);
-        switch (state) {
-        case HasEvents:
-            return send_chunk(destination, len, data_flags);
-        case WaitingForEvents:
-            return NGHTTP2_ERR_DEFERRED;
-        }
-        __builtin_unreachable();
-    }
-
-    void enqueue(const std::string& what)
-    {
-        {
-            std::lock_guard lock{mtx};
-            queue.push_back("data: " + what + "\n\n");
-        }
-        state = HasEvents;
-        res.resume();
-    }
-};
 
 int main(int argc [[maybe_unused]], char** argv [[maybe_unused]])
 {
     spdlog::set_level(spdlog::level::trace);
 
-    Signal sig;
+    rousette::http::EventStream::Signal sig;
     std::thread timer{[&sig]() {
         for (int i = 0; /* forever */; ++i) {
-            std::this_thread::sleep_for(std::chrono::milliseconds{666});
+            std::this_thread::sleep_for(666ms);
             spdlog::info("tick: {}", i);
             sig("ping #" + std::to_string(i));
         }
     }};
 
-    server::http2 server;
+    nghttp2::asio_http2::server::http2 server;
     server.num_threads(4);
 
-    server.handle("/events", [&sig](const server::request& req, const server::response& res) {
-        auto client = std::make_shared<Client>(req, res, sig);
-
-        res.on_close([client](const auto ec) {
-            client->onClose(ec);
-        });
-        res.end([client](uint8_t* destination, std::size_t len, uint32_t* data_flags) {
-            return client->process(destination, len, data_flags);
-        });
+    server.handle("/events", [&sig](const auto& req, const auto& res) {
+        auto client = std::make_shared<rousette::http::EventStream>(req, res);
+        client->activate(sig);
     });
 
     server.handle("/", [](const auto& req, const auto& resp) {
-        spdlog::warn("{} {} {}", boost::lexical_cast<std::string>(req.remote_endpoint()), req.method(), req.uri().raw_path);
+        spdlog::info("{}: {} {}", boost::lexical_cast<std::string>(req.remote_endpoint()), req.method(), req.uri().raw_path);
         resp.write_head(200, {{"content-type", {"text/html", false}}});
         resp.end(R"(<html><head><title>nghttp2 event stream</title></head>
 <body><h1>events</h1><ul id="x"></ul>
---
+++ src/http/EventStream.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2016-2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include <boost/lexical_cast.hpp>
+#include <nghttp2/asio_http2_server.h>
+#include <nghttp2/nghttp2.h>
+#include <numeric>
+#include <spdlog/spdlog.h>
+#include "http/EventStream.h"
+
+using namespace nghttp2::asio_http2;
+
+namespace rousette::http {
+
+/** @short After constructing, make sure to call activate() immediately. */
+EventStream::EventStream(const server::request& req, const server::response& res)
+    : res{res}
+    , peer{boost::lexical_cast<std::string>(req.remote_endpoint())}
+{
+    spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+}
+
+/** @short Start event processing and data delivery
+
+This cannot be a part of the constructor because of enable_shared_from_this<> semantics. When in constructor,
+shared_from_this() throws bad_weak_ptr, so we need a two-phase construction.
+*/
+void EventStream::activate(Signal& signal)
+{
+    subscription = signal.connect([this](const auto& msg) {
+        enqueue(msg);
+    });
+
+    auto client = shared_from_this();
+    res.write_head(200, {{"content-type", {"text/event-stream", false}}});
+
+    res.on_close([client](const auto ec) {
+        spdlog::debug("{}: closed ({})", client->peer, nghttp2_http2_strerror(ec));
+        client->subscription.disconnect();
+    });
+
+    res.end([client](uint8_t* destination, std::size_t len, uint32_t* data_flags) {
+        return client->process(destination, len, data_flags);
+    });
+}
+
+size_t EventStream::send_chunk(uint8_t* destination, std::size_t len, uint32_t* data_flags [[maybe_unused]])
+{
+    std::size_t written{0};
+    std::lock_guard lock{mtx};
+    if (state != HasEvents) throw std::logic_error{std::to_string(__LINE__)};
+    while (!queue.empty()) {
+        auto num = std::min(queue.front().size(), len - written);
+        std::copy_n(queue.front().begin(), num, destination + written);
+        written += num;
+        if (num < queue.front().size()) {
+            queue.front() = queue.front().substr(num);
+            // spdlog::trace("{}: send_chunk: partial write: {}", peer, num);
+            return written;
+        }
+        queue.pop_front();
+        spdlog::debug("{}: sent one event", peer);
+    }
+    state = WaitingForEvents;
+    return written;
+}
+
+ssize_t EventStream::process(uint8_t* destination, std::size_t len, uint32_t* data_flags)
+{
+    switch (state) {
+    case HasEvents:
+        return send_chunk(destination, len, data_flags);
+    case WaitingForEvents:
+        spdlog::trace("{}: sleeping", peer);
+        return NGHTTP2_ERR_DEFERRED;
+    }
+    __builtin_unreachable();
+}
+
+void EventStream::enqueue(const std::string& what)
+{
+    {
+        std::lock_guard lock{mtx};
+        auto len = std::accumulate(queue.begin(), queue.end(), 0, [](const auto a, const auto b){
+                return a + b.size();
+                });
+        spdlog::trace("{}: new event, ∑ queue size = {}", peer, len);
+        queue.push_back("data: " + what + "\n\n");
+    }
+    state = HasEvents;
+    res.resume();
+}
+}
