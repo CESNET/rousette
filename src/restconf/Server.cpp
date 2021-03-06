f3c0203ef066d10d04858e73e7f2cadd68658ebf
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-03-06 23:26:05 +0100

    Retrieve data from the from operational datastore
    NACM is not implemented in sysrepo, so this is a quick-n-dirty way of
    making sure that we don't leak password hashes and stuff like that. We
    should *not* call Session::set_user() because we're already
    unprivileged.
    
    Change-Id: I74731eb1ffc1237ca530bdbbdc4d9bf70c6c162c

--- src/restconf/Server.cpp
+++ src/restconf/Server.cpp
@@ -5,6 +5,7 @@
  *
 */
 
+#include <boost/algorithm/string/predicate.hpp>
 #include <nghttp2/asio_http2_server.h>
 #include <regex>
 #include <spdlog/spdlog.h>
@@ -30,8 +31,24 @@ namespace pattern {
 const auto atom = "[a-zA-Z_][a-zA-Z_.-]*"s;
 const std::regex moduleWildcard{"^"s + restconfRoot + "data/(" + atom + ":\\*)$"};
 const std::regex subtree{"^"s + restconfRoot + "data/(" + atom + ":" + atom + "(/(" + atom + ":)?" + atom + ")*)$"};
+
+const std::initializer_list<std::string> allowedPrefixes {
+    {"czechlight-roadm-device"s},
+    {"czechlight-coherent-add-drop"s},
+    {"czechlight-inline-amp"s},
+    {"ietf-yang-library"s},
+    {"ietf-hardware"s},
+    {"ietf-interfaces"s},
+    {"ietf-system"s},
+    {"czechlight-lldp"s},
+    {"czechlight-system:firmware"s},
+    {"czechlight-system:networking"s},
+};
 }
 }
+
+using nghttp2::asio_http2::server::request;
+using nghttp2::asio_http2::server::response;
 
 namespace rousette::restconf {
 
@@ -45,6 +62,25 @@ std::optional<std::string> as_subtree_path(const std::string& path)
         return match[1].str();
     }
     return std::nullopt;
+}
+
+bool allow_anonymous_read_for(const std::string& path)
+{
+    return std::any_of(std::begin(pattern::allowedPrefixes), std::end(pattern::allowedPrefixes),
+            [path](const auto& prefix) {
+                if (prefix.find(':') == std::string::npos) {
+                    return boost::starts_with(path, prefix + ":");
+                } else {
+                    return boost::starts_with(path, prefix);
+                }
+            });
+}
+
+void rejectResponse(const request& req, const response& res, const int code, const std::string& message)
+{
+    spdlog::debug("{}: {}", http::peer_from_request(req), message);
+    res.write_head(code, {{"content-type", {"text/plain", false}}});
+    res.end("go away");
 }
 
 Server::~Server() = default;
@@ -61,6 +97,40 @@ Server::Server(std::shared_ptr<sysrepo::Connection> conn)
         auto client = std::make_shared<http::EventStream>(req, res);
         client->activate(opticsChange, as_restconf_push_update(dwdmEvents->currentData()));
     });
+
+    server->handle(restconfRoot,
+        [conn](const auto& req, const auto& res) {
+            const auto& peer = http::peer_from_request(req);
+            spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+
+            if (req.method() != "GET") {
+                rejectResponse(req, res, 400, "nothing but GET works");
+                return;
+            }
+
+            auto path = as_subtree_path(req.uri().path);
+            if (!path) {
+                rejectResponse(req, res, 400, "not a subtree path");
+                return;
+            }
+
+            if (!allow_anonymous_read_for(*path)) {
+                rejectResponse(req, res, 400, "module not allowed");
+                return;
+            }
+
+            auto sess = std::make_shared<sysrepo::Session>(conn);
+            sess->session_switch_ds(SR_DS_OPERATIONAL);
+            auto data = sess->get_data(('/' + *path).c_str());
+
+            if (!data) {
+                rejectResponse(req, res, 404, "no data from sysrepo");
+                return;
+            }
+
+            res.write_head(200, {{"content-type", {"application/yang-data+json", false}}});
+            res.end(data->print_mem(LYD_JSON, 0));
+        });
 }
 
 void Server::listen_and_serve(const std::string& address, const std::string& port)
