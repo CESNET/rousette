793d0be80d487c4bc21e9750ebc611f9f705bd67
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-03-07 01:35:42 +0100

    path subtree filter
    This is not a RESTCONF path yet:
    
    - an explicit top-level wildcard is allowed,
    - some module must be always defined,
    - there's no support for list keys
    
    Change-Id: I24f6cea46c01e60d7358240d984ec55b61b0842d

--- src/restconf/Server.cpp
+++ src/restconf/Server.cpp
@@ -6,11 +6,14 @@
 */
 
 #include <nghttp2/asio_http2_server.h>
+#include <regex>
 #include <spdlog/spdlog.h>
 #include <sysrepo-cpp/Session.hpp>
 #include "http/utils.hpp"
 #include "restconf/Server.h"
 #include "sr/OpticalEvents.h"
+
+using namespace std::literals;
 
 namespace {
 constexpr auto notifPrefix = R"json({"ietf-restconf:notification":{"ietf-yang-push:push-update":{"datastore-contents":)json";
@@ -20,9 +23,29 @@ auto as_restconf_push_update(const std::string& content)
 {
     return notifPrefix + content + notifSuffix;
 }
+
+constexpr auto restconfRoot = "/restconf/";
+
+namespace pattern {
+const auto atom = "[a-zA-Z_][a-zA-Z_.-]*"s;
+const std::regex moduleWildcard{"^"s + restconfRoot + "data/(" + atom + ":\\*)$"};
+const std::regex subtree{"^"s + restconfRoot + "data/(" + atom + ":" + atom + "(/(" + atom + ":)?" + atom + ")*)$"};
+}
 }
 
 namespace rousette::restconf {
+
+std::optional<std::string> as_subtree_path(const std::string& path)
+{
+    std::smatch match;
+    if (std::regex_match(path, match, pattern::moduleWildcard) && match.size() > 1) {
+        return match[1].str();
+    }
+    if (std::regex_match(path, match, pattern::subtree) && match.size() > 1) {
+        return match[1].str();
+    }
+    return std::nullopt;
+}
 
 Server::~Server() = default;
 
