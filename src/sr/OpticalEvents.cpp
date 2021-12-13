e473d362f5e4363bad786cfc4c786c20c7841e16
Václav Kubernát <kubernat@cesnet.cz>
2021-12-13 09:37:32 +0100

    Migrate to libyang2
    Change-Id: I6910c1efa44a24d097513fca25a329c3a2c417b2

--- src/sr/OpticalEvents.cpp
+++ src/sr/OpticalEvents.cpp
@@ -8,38 +8,39 @@
 #include <memory>
 #include <set>
 #include <spdlog/spdlog.h>
-#include <sysrepo-cpp/Session.hpp>
+#include <sysrepo-cpp/utils/exception.hpp>
 #include <sysrepo.h>
 #include "sr/OpticalEvents.h"
 
 using namespace std::literals;
 
 namespace {
-std::string dumpDataFrom(std::shared_ptr<sysrepo::Session> session, const std::string& module)
+std::string dumpDataFrom(sysrepo::Session session, const std::string& module)
 {
-    return session->get_data(('/' + module + ":*").c_str())->print_mem(LYD_JSON, LYP_WITHSIBLINGS);
+    return std::string{*session.getData(('/' + module + ":*").c_str())->printStr(libyang::DataFormat::JSON, libyang::PrintFlags::WithSiblings)};
 }
 }
 
 namespace rousette::sr {
 
-OpticalEvents::OpticalEvents(std::shared_ptr<sysrepo::Session> session)
-    : sub(std::make_shared<sysrepo::Subscribe>(session))
+OpticalEvents::OpticalEvents(sysrepo::Session session)
+    : sub(std::nullopt)
 {
-    session->session_switch_ds(SR_DS_OPERATIONAL);
+    session.switchDatastore(sysrepo::Datastore::Operational);
 
     // Because it's "tricky" to request data from several top-level modules via sysrepo (and nothing else),
     // just pick a first module to listen for.
     for (const auto& mod : {"czechlight-roadm-device", "czechlight-coherent-add-drop", "czechlight-inline-amp"}) {
         try {
-            sub->module_change_subscribe(mod, [this](const auto sess, const auto name, const auto, const auto, const auto) {
-                        return onChange(sess, name);
-                    }, nullptr, 0, SR_SUBSCR_CTX_REUSE | SR_SUBSCR_DONE_ONLY | SR_SUBSCR_PASSIVE);
+            sysrepo::ModuleChangeCb cb = [this](const auto sess, auto, std::string_view name, auto, auto, auto) {
+                return onChange(sess, std::string{name});
+            };
+            sub = session.onModuleChange(mod, cb, nullptr, 0, sysrepo::SubscribeOptions::DoneOnly | sysrepo::SubscribeOptions::Passive);
             spdlog::debug("Listening for module {}", mod);
             lastData = dumpDataFrom(session, mod);
             return;
-        } catch (sysrepo::sysrepo_exception& e) {
-            if (e.error_code() == SR_ERR_NOT_FOUND) {
+        } catch (sysrepo::ErrorWithCode& e) {
+            if (e.code() == sysrepo::ErrorCode::NotFound) {
                 // nothing to listen for, just ignore this
                 continue;
             }
@@ -49,16 +50,16 @@ OpticalEvents::OpticalEvents(std::shared_ptr<sysrepo::Session> session)
     throw std::runtime_error{"No DWDM modules found"};
 }
 
-int OpticalEvents::onChange(std::shared_ptr<sysrepo::Session> session, const std::string& module)
+sysrepo::ErrorCode OpticalEvents::onChange(sysrepo::Session session, const std::string& module)
 {
-    assert(session->session_get_ds() == SR_DS_OPERATIONAL);
+    assert(session.activeDatastore() == sysrepo::Datastore::Operational);
     lastData = dumpDataFrom(session, module);
     spdlog::debug("change: {} bytes", lastData.size());
 
     // I wanted this to be a bit smarter, with a subtree filter to remove "unwated changes" and what not.
     // Given that we do not have a full-blown subtree filtering (yet), let's just return the data upon any change.
     change(lastData);
-    return SR_ERR_OK;
+    return sysrepo::ErrorCode::Ok;
 }
 
 std::string OpticalEvents::currentData() const
