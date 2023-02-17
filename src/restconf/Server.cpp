37ca592d3a55671b807e83778442f988cf938c3e
Vaclav Zajic <vaclav.zajic@cesnet.cz>
2023-02-17 11:20:55 +0100

    Added logging of request path
    Another catch-all handler has been added. It logs each request.
    
    Change-Id: I7ccc1c94de2c3a0ad6655917964aa3f3971c2bf0

--- src/restconf/Server.cpp
+++ src/restconf/Server.cpp
@@ -109,6 +109,12 @@ Server::Server(sysrepo::Connection conn)
         opticsChange(as_restconf_push_update(content, std::chrono::system_clock::now()));
     });
 
+    server->handle("/", [](const auto& req, const auto& res) {
+        const auto& peer = http::peer_from_request(req);
+        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        rejectResponse(req, res, 404, "resource does not exist");
+    });
+
     server->handle("/telemetry/optics", [this](const auto& req, const auto& res) {
         auto client = std::make_shared<http::EventStream>(req, res);
         client->activate(opticsChange, as_restconf_push_update(dwdmEvents->currentData(), std::chrono::system_clock::now()));
