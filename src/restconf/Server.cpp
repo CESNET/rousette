CESNET/rousette

commit fe464e24c9c84395e2edc82a484654222291ed0d

server: extract actual sysrepo data fetch into separate function
This will become useful in next commits where we instruct sysrepo to
consider NACM and tests for it.

Change-Id: I1fd4fe06f499be5e49ab2b62f5d2d38b0cb344af

src/restconf/Server.cpp
@@ -99,6 +99,10 @@ void rejectResponse(const request& req, const response& res, const int code, con
     res.end("go away");
 }
 
+std::optional<libyang::DataNode> getData(sysrepo::Session sess, const std::string& path) {
+    return sess.getData('/' + path);
+}
+
 Server::~Server() = default;
 
 Server::Server(sysrepo::Connection conn)
@@ -154,32 +158,29 @@ Server::Server(sysrepo::Connection conn)
             }
 
             try {
-              auto sess = conn.sessionStart(sysrepo::Datastore::Operational);
+                auto sess = conn.sessionStart(sysrepo::Datastore::Operational);
 
-              if (!hasModuleForPath(sess, *path)) {
-                  rejectResponse(req, res, 404, "module not implemented");
-                  return;
-              }
+                if (!hasModuleForPath(sess, *path)) {
+                    rejectResponse(req, res, 404, "module not implemented");
+                    return;
+                }
 
-              auto data = sess.getData('/' + *path);
-
-              if (!data) {
-                rejectResponse(req, res, 404, "no data from sysrepo");
-                return;
-              }
-
-              res.write_head(
-                  200,
-                  {
-                      {"content-type", {"application/yang-data+json", false}},
-                      {"access-control-allow-origin", {"*", false}},
-                  });
-              res.end(*data->printStr(libyang::DataFormat::JSON,
-                                      libyang::PrintFlags::WithSiblings));
-            } catch (const sysrepo::ErrorWithCode &e) {
-              spdlog::error("Sysrepo exception: {}", e.what());
-              rejectResponse(req, res, 500,
-                             "sysrepo exception: internal server error");
+                if (auto data = getData(sess, *path); data) {
+                    res.write_head(
+                        200,
+                        {
+                            {"content-type", {"application/yang-data+json", false}},
+                            {"access-control-allow-origin", {"*", false}},
+                        });
+                    res.end(*data->printStr(libyang::DataFormat::JSON,
+                                            libyang::PrintFlags::WithSiblings));
+                } else {
+                    rejectResponse(req, res, 404, "no data from sysrepo");
+                    return;
+                }
+            } catch (const sysrepo::ErrorWithCode& e) {
+                spdlog::error("Sysrepo exception: {}", e.what());
+                rejectResponse(req, res, 500, "sysrepo exception: internal server error");
             }
         });
 }

commit bcb727ea4aa9670d7b5353249082c59b65a06416

server: fix formatting
Fixes: fe464e24c9c84395e2edc82a484654222291ed0d
Change-Id: I5bd728f5fa499c539ce06751c6fef37ae47906c2

src/restconf/Server.cpp
@@ -99,7 +99,10 @@ void rejectResponse(const request& req, const response& res, const int code, con
     res.end("go away");
 }
 
-std::optional<libyang::DataNode> getData(sysrepo::Session sess, const std::string& path) {
+std::optional<libyang::DataNode> getData(sysrepo::Session sess, const std::string& path, const std::string& nacmUser)
+{
+    sess.setNacmUser(nacmUser);
+    spdlog::info("  nacm user: {}", nacmUser);
     return sess.getData('/' + path);
 }
 