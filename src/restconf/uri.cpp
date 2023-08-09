9a554d29a69aa73d7d83c23ae35ee89484b8ddf7
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-08-09 10:00:22 +0200

    server: parser for data resources in URI paths
    We add a parser for request URIs for data resource identifiers[1,
    sec. 3.5.3]. We allow to parse path expressions for lists and leaf-lists
    which is something the current code can not.
    
    However, the key specifiers for lists and leaf-lists differ from Path
    used by libyang[2]. For example, RESTCONF identifier path
    `/foo:list=k1,k2` corresponds to libyang's
    `/foo/list[first_key='k1'][second_key='k2']`.
    
    This commit only parses the URI and returns slash-separated path
    segments. It is not yet used. Firstly, the translation from RESTCONF
    identifier paths for (leaf-)lists must be implemented.
    
    All the structs defined in the header are a implementation detail of the
    parser and the declarations are located in the "impl" header file.
    
    [1] https://www.rfc-editor.org/rfc/rfc8040.html
    [2] https://netopeer.liberouter.org/doc/libyang/master/html/howto_x_path.html
    
    Change-Id: I5a968a83369575efbf1b213a0cd41bc327575834

---
+++ src/restconf/uri.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ */
+
+#include "restconf/uri_impl.h"
+
+namespace rousette::restconf::impl {
+
+namespace {
+namespace x3 = boost::spirit::x3;
+
+auto set_zero = [](auto& ctx) { _val(ctx) = 0u; };
+auto add = [](auto& ctx) {
+    char c = std::tolower(_attr(ctx));
+    _val(ctx) = _val(ctx) * 16 + (c >= 'a' ? c - 'a' + 10 : c - '0');
+};
+const auto urlEncodedChar = x3::rule<class urlEncodedChar, unsigned>{"urlEncodedChar"} = x3::lit('%')[set_zero] >> x3::xdigit[add] >> x3::xdigit[add];
+
+/* reserved characters according to RFC 3986, sec. 2.2 with '%' added. The '%' character is not specified as reserved but it effectively is because
+ * "Percent sign serves as the indicator for percent-encoded octets, it must be percent-encoded (...)" [RFC 3986, sec. 2.4]. */
+const auto reservedChars = x3::lit(':') | '/' | '?' | '#' | '[' | ']' | '@' | '!' | '$' | '&' | '\'' | '(' | ')' | '*' | '+' | ',' | ',' | ';' | '=' | '%';
+const auto keyValue = x3::rule<class keyValue, std::string>{"keyValue"} = *(urlEncodedChar | (x3::char_ - reservedChars));
+
+const auto keyList = x3::rule<class keyList, std::vector<std::string>>{"keyList"} = keyValue % ',';
+const auto identifier = x3::rule<class apiIdentifier, std::string>{"identifier"} = (x3::alpha | x3::char_('_')) >> *(x3::alnum | x3::char_('_') | x3::char_('-') | x3::char_('.'));
+const auto apiIdentifier = x3::rule<class identifier, ApiIdentifier>{"apiIdentifier"} = -(identifier >> ':') >> identifier;
+const auto listInstance = x3::rule<class keyList, PathSegment>{"listInstance"} = apiIdentifier >> -('=' >> keyList);
+const auto fullyQualifiedApiIdentifier = x3::rule<class identifier, ApiIdentifier>{"apiIdentifier"} = identifier >> ':' >> identifier;
+const auto fullyQualifiedListInstance = x3::rule<class keyList, PathSegment>{"listInstance"} = fullyQualifiedApiIdentifier >> -('=' >> keyList);
+const auto uriGrammar = x3::rule<class grammar, std::vector<PathSegment>>{"grammar"} = x3::lit("/") >> x3::lit("restconf") >> "/" >> x3::lit("data") >> "/"
+    >> fullyQualifiedListInstance >> -("/" >> listInstance % "/"); // RFC 8040, sec 3.5.3
+}
+
+std::optional<ResourcePath> parseUriPath(const std::string& uriPath)
+{
+    std::vector<PathSegment> out;
+    auto iter = std::begin(uriPath);
+    auto end = std::end(uriPath);
+
+    if (!x3::parse(iter, end, uriGrammar >> x3::eoi, out)) {
+        return std::nullopt;
+    }
+
+    return out;
+}
+
+ApiIdentifier::ApiIdentifier() = default;
+
+ApiIdentifier::ApiIdentifier(const std::string& prefix, const std::string& identifier)
+    : prefix(prefix)
+    , identifier(identifier)
+{
+}
+
+ApiIdentifier::ApiIdentifier(const std::string& identifier)
+    : prefix(boost::none)
+    , identifier(identifier)
+{
+}
+
+PathSegment::PathSegment() = default;
+
+PathSegment::PathSegment(const ApiIdentifier& apiIdent, const std::vector<std::string>& keys)
+    : apiIdent(apiIdent)
+    , keys(keys)
+{
+}
+
+ResourcePath::ResourcePath(const std::vector<PathSegment>& segments)
+    : segments(segments)
+{
+}
+}
