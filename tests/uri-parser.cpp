ec613870a306d728f7dad92d0e0d66c4e8c3296d
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-08-10 21:28:11 +0200

    server: construct libyang paths from URIs
    The RESTCONF data resource identifiers [1, sec 3.5] and libyang paths
    [2] differ a bit. This patch translates the former into the later.
    This is needed so we can use libyang/sysrepo to perform the data
    manipulation.
    
    The RFC also states what the data resource is [1, sec. 3.5]. For
    instance, you cannot query a whole list. So, some checks are needed when
    translating the path.
    
    [1] https://www.rfc-editor.org/rfc/rfc8040.html#section-3.5.3.1
    [2] https://netopeer.liberouter.org/doc/libyang/master/html/howto_x_path.html
    
    Change-Id: I541895e612860e4ba37329f7bab377e372aaa111

--- tests/uri-parser.cpp
+++ tests/uri-parser.cpp
@@ -8,6 +8,8 @@
 
 #include "trompeloeil_doctest.h"
 #include <experimental/iterator>
+#include <sysrepo-cpp/Connection.hpp>
+#include "restconf/uri.h"
 #include "restconf/uri_impl.h"
 
 namespace rousette::restconf::impl {
@@ -44,6 +46,22 @@ std::ostream& operator<<(std::ostream& os, const ResourcePath& obj)
 }
 
 namespace doctest {
+template <>
+struct StringMaker<std::optional<std::string>> {
+    static String convert(const std::optional<std::string>& obj)
+    {
+        std::ostringstream oss;
+
+        if (obj) {
+            oss << "optional{" << *obj << "}";
+        } else {
+            oss << "nullopt{}";
+        }
+
+        return oss.str().c_str();
+    }
+};
+
 template <>
 struct StringMaker<std::optional<rousette::restconf::impl::ResourcePath>> {
     static String convert(const std::optional<rousette::restconf::impl::ResourcePath>& obj)
@@ -232,6 +250,72 @@ TEST_CASE("URI path parser")
 
             CAPTURE(uriPath);
             REQUIRE(!rousette::restconf::impl::parseUriPath(uriPath));
+        }
+    }
+
+    SECTION("Translation to libyang path")
+    {
+        auto conn = sysrepo::Connection{};
+        auto sess = conn.sessionStart();
+
+        REQUIRE(sess.getContext().getModuleImplemented("example"));
+        REQUIRE(sess.getContext().getModuleImplemented("example-augment"));
+
+        SECTION("Contextually valid paths")
+        {
+            for (const auto& [uriPath, expectedLyPath] : {
+                     std::pair<std::string, std::optional<std::string>>{"/restconf/data/example:top-level-leaf", "/example:top-level-leaf"},
+                     {"/restconf/data/example:top-level-list=hello", "/example:top-level-list[name='hello']"},
+                     {"/restconf/data/example:l/list=eth0", "/example:l/list[name='eth0']"},
+                     {R"(/restconf/data/example:l/list=et"h0)", R"(/example:l/list[name='et"h0'])"},
+                     {R"(/restconf/data/example:l/list=et%22h0)", R"(/example:l/list[name='et"h0'])"},
+                     {R"(/restconf/data/example:l/list=et%27h0)", R"(/example:l/list[name="et'h0"])"},
+                     {"/restconf/data/example:l/list=eth0/name", "/example:l/list[name='eth0']/name"},
+                     {"/restconf/data/example:l/list=eth0/nested=1,2,3", "/example:l/list[name='eth0']/nested[first='1'][second='2'][third='3']"},
+                     {"/restconf/data/example:l/list=eth0/nested=,2,3", "/example:l/list[name='eth0']/nested[first=''][second='2'][third='3']"},
+                     {"/restconf/data/example:l/list=eth0/nested=,2,", "/example:l/list[name='eth0']/nested[first=''][second='2'][third='']"},
+                     {"/restconf/data/example:l/list=eth0/choice1", "/example:l/list[name='eth0']/choice1"},
+                     {"/restconf/data/example:l/list=eth0/choice2", "/example:l/list[name='eth0']/choice2"},
+                     {"/restconf/data/example:l/list=eth0/collection=val", "/example:l/list[name='eth0']/collection[.='val']"},
+                     {"/restconf/data/example:l/status", "/example:l/status"},
+                     // container example:a has a container b inserted locally and also via an augment. Check that we return the correct one
+                     {"/restconf/data/example:a/b", "/example:a/b"},
+                     {"/restconf/data/example:a/b/c", "/example:a/b/c"},
+                     {"/restconf/data/example:a/b/c/enabled", "/example:a/b/c/enabled"},
+                     {"/restconf/data/example:a/example-augment:b", "/example:a/example-augment:b"},
+                     {"/restconf/data/example:a/example-augment:b/c", "/example:a/example-augment:b/c"},
+                     {"/restconf/data/example:a/example-augment:b/example-augment:c", "/example:a/example-augment:b/c"},
+                     {"/restconf/data/example:a/example-augment:b/c/enabled", "/example:a/example-augment:b/c/enabled"},
+                 }) {
+                CAPTURE(uriPath);
+                REQUIRE(rousette::restconf::impl::parseUriPath(uriPath));
+                REQUIRE(rousette::restconf::asLibyangPath(sess.getContext(), uriPath) == expectedLyPath);
+            }
+        }
+
+        SECTION("Contextually invalid paths") {
+            for (const auto& uriPath : std::vector<std::string>{
+                     "/restconf/data/hello:world", // nonexistent module
+                     "/restconf/data/example:foo", // nonexistent top-level node
+                     "/restconf/data/example-augment:b", // nonexistent top-level node
+                     "/restconf/data/example:l/hello-world", // nonexistent node
+                     "/restconf/data/example:f", // feature not enabled
+                     "/restconf/data/example:top-level-list", // list is not a data resource
+                     "/restconf/data/example:l/key-less-list", // list is not a data resource
+                     "/restconf/data/example:l/list=eth0/collection", // leaf-list is not a data resource
+                     "/restconf/data/example:test-rpc", // RPC is not a data resource
+                     "/restconf/data/example:test-rpc/i", // RPC input node is not a data resource
+                     "/restconf/data/example:test-rpc/o", // RPC output node is not a data resource
+                     "/restconf/data/example:l=eth0", // node not a (leaf-)list
+                     "/restconf/data/example:l/list=eth0,eth1", // wrong number of list elements
+                     "/restconf/data/example:l/list=eth0/collection=br0,eth1", // wrong number of keys for a leaf-list
+                     "/restconf/data/example:l/list=eth0/choose", // schema nodes should not be visible
+                     "/restconf/data/example:l/list=eth0/choose/choice1", // schema nodes should not be visible
+                 }) {
+                CAPTURE(uriPath);
+                REQUIRE(rousette::restconf::impl::parseUriPath(uriPath));
+                REQUIRE_THROWS_AS(rousette::restconf::asLibyangPath(sess.getContext(), uriPath), rousette::restconf::InvalidURIException);
+            }
         }
     }
 }
