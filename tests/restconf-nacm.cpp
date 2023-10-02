c2a4ecba61df78a3e89c97f6e14818a75317e818
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-10-02 14:05:11 +0200

    server: process PUT requests
    This patch introduces basic support for HTTP PUT method which can create
    or replace resources [1]. Both JSON and XML encodings are supported.
    
    [1] https://www.rfc-editor.org/rfc/rfc8040.html#section-4.5
    
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/6511
    Change-Id: I7dee2e72246d93766bc6fb76a02d15757490a2bf

--- tests/restconf-nacm.cpp
+++ tests/restconf-nacm.cpp
@@ -227,6 +227,7 @@ TEST_CASE("NACM")
             const auto start = std::chrono::steady_clock::now();
             REQUIRE(clientRequest("GET",
                         "/ietf-system:system",
+                        "",
                         {{"authorization", "Basic ZHdkbTpGQUlM"}},
                         boost::posix_time::seconds(5))
                     == Response{401, jsonHeaders, R"({
@@ -252,6 +253,7 @@ TEST_CASE("NACM")
             const auto start = std::chrono::steady_clock::now();
             REQUIRE_THROWS_WITH(clientRequest("GET",
                         "/ietf-system:system",
+                        "",
                         {{"authorization", "Basic ZHdkbTpGQUlM"}},
                         boost::posix_time::milliseconds(100)),
                     "HTTP client error: Connection timed out");
