cad920ec7f3b39c1fd4e47d082adc82addec57bb
Jan Kundrát <jan.kundrat@cesnet.cz>
2023-10-25 11:06:17 +0200

    PAM authentication
    After a few back-and-forth attempts (such as trying to teach
    nghttp2-asio how to talk over a Unix socket, or playing out with
    nghttpx' mruby support for PAM) I decided to implement the
    authentication within Rousette; I mainly wanted to avoid dealing with
    yet another reverse proxy to delegate authentication to. As a result,
    Rousette now has native linux-pam integration (and a new requirement).
    The actual PAM-talking code was taken from Dovecot (and I rewrote it in
    modern C++, so all bugs are mine).
    
    If the standard HTTP header for basic auth is provided, it's checked
    against the system configuration. If it's an anonymous request *and* if
    anonymous requests are properly configured, it's handled as such.
    
    Testing is done via a custom PAM configuration. There's a Berkley DB
    authentication backend that ships with the upstream PAM, so I used this
    pam_userdb thingy. However, since I had no clue how the BDB storage
    works, I used this Python script for generating it first (insert more
    passwords below):
    
     import bsddb3
     db=bsddb3.db.DB()
     db.open('/full/path/to/tests/pam/database.db', bsddb3.db.DB_HASH, bsddb3.db.DB_THREAD | bsddb3.db.DB_CREATE)
     db.put(b'root', b'sekrit')
     db.put(b'dwdm', b'DWDM')
     db.put(b'yangnobody', b'no chance')
     db.put(b'Aladdin', b'open sesame')
    
    ...and then dumped it through a `db_dump -p tests/pam/database.db`. I
    guess I could have just as well used the `-T` option to `db_load`. Maybe
    next time.
    
    PAM apparently hard-requires a file named `other` in the specified
    directory; it's probably taken as a last-resort auth method. I would
    have preferred to ship a file called `tests/pam/rousette`, but hey,
    `other` it is.
    
    Change-Id: I656d40a7ff34d11b3886afdb7f4ae47deeef910b
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/br2-external/+/6495

---
+++ tests/pam.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+#include "trompeloeil_doctest.h"
+#include "restconf/PAM.h"
+#include "configure.cmake.h"
+
+TEST_CASE("URI path parser")
+{
+    using rousette::auth::authenticate_pam;
+
+    SECTION("success") {
+        std::string username, blob;
+
+        SECTION("RFC Aladdin") {
+            username = "Aladdin";
+            blob = "QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
+        }
+
+        SECTION("dwdm") {
+            username = "dwdm";
+            blob = "ZHdkbTpEV0RN";
+        }
+
+        SECTION("root") {
+            username = "root";
+            blob = "cm9vdDpzZWtyaXQ=";
+        }
+
+        SECTION("yangnobody") {
+            username = "yangnobody";
+            blob = "eWFuZ25vYm9keTpubyBjaGFuY2U=";
+        }
+
+        SECTION("norules") {
+            username = "norules";
+            blob = "bm9ydWxlczplbXB0eQ==";
+        }
+
+        REQUIRE(authenticate_pam("Basic " + blob,
+                    std::filesystem::path(CMAKE_CURRENT_BINARY_DIR) / "tests" / "pam",
+                    "[::1]:666") == username);
+    }
+
+    SECTION("failed") {
+        std::string input;
+        std::string error;
+
+        SECTION("invalid auth method") {
+            input = "wtf xxx";
+            error = "Cannot parse the Basic authorization header";
+        }
+
+        SECTION("invalid base64") {
+            input = "Basic xxx";
+            error = "Cannot parse the user-pass authorization blob";
+        }
+
+        SECTION("invalid user") {
+            input = "Basic MDox";
+            error = "PAM: pam_authenticate: User not known to the underlying authentication module";
+        }
+
+        SECTION("wrong password") {
+            input = "Basic cm9vdDpyb290";
+            error = "PAM: pam_authenticate: Authentication failure";
+        }
+
+        REQUIRE_THROWS_WITH_AS(
+                authenticate_pam(input, std::filesystem::path(CMAKE_CURRENT_BINARY_DIR) / "tests" / "pam", ""),
+                error.c_str(),
+                rousette::auth::Error);
+    }
+}
