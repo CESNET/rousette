dbff3081c1ada3520379c3bc5e38d98b87b5fdac
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-10-30 14:08:05 +0100

    tests: using invalid HTTP methods should return error
    Change-Id: I9fd6cdf52d51f569a0df3096e621e4f01f7f21e1

--- tests/http-communication.cpp
+++ tests/http-communication.cpp
@@ -213,6 +213,23 @@ TEST_CASE("HTTP")
     srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='dwdm rule']/rule[name='1']/action", "permit"); // overrides nacm:default-deny-* rules in ietf-system model
     srSess.applyChanges();
 
+    // we do not support these http methods yet
+    for (const auto& httpMethod : {"OPTIONS"s, "POST"s, "PUT"s, "PATCH"s, "DELETE"s}) {
+        CAPTURE(httpMethod);
+        REQUIRE(clientRequest(httpMethod, "/ietf-system:system", {}) == Response{405, jsonHeaders, R"({
+  "ietf-restconf:errors": {
+    "error": [
+      {
+        "error-type": "application",
+        "error-tag": "operation-not-supported",
+        "error-message": "Method not allowed."
+      }
+    ]
+  }
+}
+)"});
+    }
+
     REQUIRE(get("/ietf-system:system", {}) == Response{200, jsonHeaders, R"({
   "ietf-system:system": {
     "contact": "contact",
