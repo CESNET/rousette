CESNET/rousette

commit 542a8bd2f67fd6d77a5dcaab7bd810191e15721b

PAM authentication
After a few back-and-forth attempts (such as trying to teach
nghttp2-asio how to talk over a Unix socket, or playing out with
nghttpx' mruby support for PAM) I decided to implement the
authentication within Rousette; I mainly wanted to avoid dealing with
yet another reverse proxy to delegate authentication to. As a result,
Rousette now has native linux-pam integration (and a new requirement).

The actual PAM-talking code was taken from Dovecot (and I rewrote it in
modern C++, so all bugs are mine). There's no real concept of a
"password" in PAM; the authentication is performed through a
"conversation" with the user. This function gets a sequence of prompts
to show to the user. There's nothing which indicates that something is
asking for a password, though; the only hint is either "echo on" or
"echo off". We do what Dovecot has been doing for ages, and assume that
an "echo on" is asking for username, whereas "echo off" is asking for
password.

If the standard HTTP header for basic auth is provided, it's checked
against the system configuration. If it's an anonymous request *and* if
anonymous requests are properly configured, it's handled as such.

Testing is done via a custom PAM configuration. There's a Berkley DB
authentication backend that ships with the upstream PAM, so I used this
pam_userdb thingy. However, since I had no clue how the BDB storage
works, I used this Python script for generating it first (insert more
passwords below):

 import bsddb3
 db=bsddb3.db.DB()
 db.open('/full/path/to/tests/pam/database.db', bsddb3.db.DB_HASH, bsddb3.db.DB_THREAD | bsddb3.db.DB_CREATE)
 db.put(b'root', b'sekrit')
 db.put(b'dwdm', b'DWDM')
 db.put(b'yangnobody', b'no chance')
 db.put(b'Aladdin', b'open sesame')

...and then dumped it through a `db_dump -p tests/pam/database.db`. I
guess I could have just as well used the `-T` option to `db_load`. Maybe
next time.

PAM apparently hard-requires a file named `other` in the specified

CMakeLists.txt
@@ -75,6 +75,7 @@ find_package(Boost REQUIRED COMPONENTS system thread)
 pkg_check_modules(SYSREPO-CPP REQUIRED IMPORTED_TARGET sysrepo-cpp>=1.1.0)
 pkg_check_modules(LIBYANG-CPP REQUIRED IMPORTED_TARGET libyang-cpp>=1.1.0)
 pkg_check_modules(SYSTEMD REQUIRED IMPORTED_TARGET libsystemd)
+pkg_check_modules(PAM REQUIRED IMPORTED_TARGET pam)
 
 add_library(rousette-http STATIC
     src/http/EventStream.cpp
@@ -88,13 +89,18 @@ add_library(rousette-sysrepo STATIC
 )
 target_link_libraries(rousette-sysrepo PUBLIC spdlog::spdlog PkgConfig::SYSREPO-CPP PkgConfig::LIBYANG-CPP)
 
-add_library(rousette-restconf STATIC
+add_library(rousette-auth STATIC
     src/restconf/Nacm.cpp
+    src/restconf/PAM.cpp
+)
+target_link_libraries(rousette-auth PUBLIC spdlog::spdlog PkgConfig::SYSREPO-CPP PkgConfig::PAM)
+
+add_library(rousette-restconf STATIC
     src/restconf/Server.cpp
     src/restconf/uri.cpp
     src/restconf/utils.cpp
 )
-target_link_libraries(rousette-restconf PUBLIC rousette-http rousette-sysrepo Boost::system Threads::Threads)
+target_link_libraries(rousette-restconf PUBLIC rousette-http rousette-sysrepo rousette-auth Boost::system Threads::Threads)
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/restconf/NacmIdentities.h.in ${CMAKE_CURRENT_BINARY_DIR}/NacmIdentities.h @ONLY)
 include_directories(${CMAKE_CURRENT_BINARY_DIR})
@@ -133,6 +139,8 @@ if(BUILD_TESTING)
 
     find_program(SYSREPOCTL sysrepoctl)
     find_program(SYSREPOCFG sysrepocfg)
+    find_program(BERKLEY_DB_LOAD db_load)
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tests/configure.cmake.h.in ${CMAKE_CURRENT_BINARY_DIR}/configure.cmake.h @ONLY)
 
     function(rousette_test)
         cmake_parse_arguments(TEST "" "NAME;FIXTURE;RESOURCE_LOCK" "LIBRARIES;COMMAND" ${ARGN})
@@ -181,12 +189,24 @@ if(BUILD_TESTING)
     endfunction()
 
     rousette_test(NAME http-utils LIBRARIES rousette-http)
+    rousette_test(NAME pam LIBRARIES rousette-auth)
+
+    set(TEST_PAM_USERDB_NOSUFFIX ${CMAKE_CURRENT_BINARY_DIR}/tests/pam/userdb)
+    set(TEST_PAM_USERDB_FILE ${TEST_PAM_USERDB_NOSUFFIX}.db)
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/other.in ${CMAKE_CURRENT_BINARY_DIR}/tests/pam/other @ONLY)
+    add_custom_command(OUTPUT ${TEST_PAM_USERDB_FILE}
+        COMMAND ${CMAKE_COMMAND} -E remove ${TEST_PAM_USERDB_FILE}
+        COMMAND ${BERKLEY_DB_LOAD} -f ${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/database.dump ${TEST_PAM_USERDB_FILE}
+        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/database.dump)
+    add_custom_target(generate_pam_userdb DEPENDS ${TEST_PAM_USERDB_FILE})
+    add_dependencies(test-pam generate_pam_userdb)
 
     set(fixture-http
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/ietf-system@2014-08-06.yang FEATURE radius
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-restconf@2017-01-26.yang
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang FEATURE f1)
     rousette_test(NAME http-communication LIBRARIES rousette-restconf FIXTURE fixture-http)
+    add_dependencies(test-http-communication generate_pam_userdb)
 
     set(fixture-uri-parser
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang FEATURE f1

ci/pre.yaml
@@ -5,3 +5,9 @@
         name: libnghttp2-devel
         state: present
       become: true
+
+    - name: install libdb-utils
+      package:
+        name: libdb-utils
+        state: present
+      become: true

src/restconf/PAM.cpp
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include <boost/algorithm/string.hpp>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/fusion/adapted/struct/adapt_struct.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <exception>
+#include <security/pam_appl.h>
+#include <spdlog/spdlog.h>
+#include "restconf/PAM.h"
+
+using namespace std::literals;
+
+namespace rousette::auth {
+namespace {
+
+namespace x3 = boost::spirit::x3;
+
+const auto base64Blob = x3::rule<class base64Blob, std::string>{"base64Blob"} = *(x3::alnum | x3::char_('+') | x3::char_('/')) >> x3::repeat(0, 2)[x3::lit('=')];
+const auto headerGrammar = x3::no_skip[x3::no_case[x3::lit("basic")] >> x3::lit(' ') >> base64Blob];
+const auto username = x3::rule<class username, std::string>{"username"} = +(x3::char_ - x3::lit(':'));
+const auto password = x3::rule<class password, std::string>{"password"} = +x3::char_;
+const auto userPass = x3::no_skip[username >> x3::lit(':') >> password];
+
+std::string b64_decode(const std::string& val)
+{
+    using namespace boost::archive::iterators;
+    using It = transform_width<binary_from_base64<std::string::const_iterator>, 8, 6>;
+    return boost::algorithm::trim_right_copy_if(std::string(It(std::begin(val)), It(std::end(val))), [](char c) {
+        return c == '\0';
+    });
+}
+
+struct UserPass {
+    std::string username;
+    std::string password;
+};
+
+UserPass parseBasicAuth(const std::string& blob)
+{
+    std::string b64;
+    auto iter = std::begin(blob);
+    auto end = std::end(blob);
+
+    if (!x3::parse(iter, end, headerGrammar >> x3::eoi, b64)) {
+        throw Error{"Cannot parse the Basic authorization header"};
+    }
+
+    b64 = b64_decode(b64);
+
+    iter = b64.begin();
+    end = b64.end();
+
+    UserPass res;
+    if (!x3::parse(iter, end, userPass >> x3::eoi, res)) {
+        throw Error{"Cannot parse the user-pass authorization blob"};
+    }
+    return res;
+}
+
+static int pam_userpass_conv(int num_msg, const struct pam_message** msg, struct pam_response** resp_r, void* appdata_ptr)
+{
+    const auto& userPass = *(const UserPass*)(appdata_ptr);
+
+    auto release_response = [&num_msg](struct pam_response* resp) {
+        for (int i = 0; i < num_msg; ++i) {
+            free(resp[i].resp);
+        }
+        free(resp);
+    };
+
+    std::unique_ptr<struct pam_response, decltype(release_response)> resp{
+        (pam_response*)calloc(num_msg, sizeof(struct pam_response)), release_response};
+
+    for (int i = 0; i < num_msg; ++i) {
+        char* str = nullptr;
+        switch (msg[i]->msg_style) {
+        case PAM_PROMPT_ECHO_ON:
+            // assume that we're being asked about the username
+            str = strdup(userPass.username.c_str());
+            if (!str) {
+                spdlog::critical("PAM: strdup(username) failed");
+                return PAM_BUF_ERR;
+            }
+            break;
+        case PAM_PROMPT_ECHO_OFF:
+            // assume that this is the password
+            str = strdup(userPass.password.c_str());
+            if (!str) {
+                spdlog::critical("PAM: strdup(password) failed");
+                return PAM_BUF_ERR;
+            }
+            break;
+        /* case PAM_ERROR_MSG: */
+        /* case PAM_TEXT_INFO: */
+        default:
+            spdlog::critical("PAM: pam_userpass_conv: unexpected msg_style {}", msg[i]->msg_style);
+            return PAM_CONV_ERR;
+        }
+        resp.get()[i].resp_retcode = PAM_SUCCESS;
+        resp.get()[i].resp = str;
+    }
+    *resp_r = resp.release();
+    return PAM_SUCCESS;
+}
+
+std::string authenticate_pam(const UserPass& userPass, const std::optional<std::filesystem::path>& dir, const std::optional<std::string>& remoteHost)
+{
+    pam_handle_t* pamh = nullptr;
+    int res;
+    pam_conv conv = {
+        .conv = pam_userpass_conv,
+        .appdata_ptr = (void*)(&userPass),
+    };
+
+    auto check = [&res, &pamh](const std::string& fun) {
+        if (res != PAM_SUCCESS) {
+            throw Error{"PAM: " + fun + ": " + pam_strerror(pamh, res)};
+        }
+    };
+
+    auto pamh_deleter = [&res](pam_handle_t* pamh) {
+        pam_end(pamh, res);
+    };
+
+    res = pam_start_confdir("rousette", userPass.username.c_str(), &conv, dir ? std::string(*dir).c_str() : nullptr, &pamh);
+    std::unique_ptr<pam_handle_t, decltype(pamh_deleter)> guard{pamh, pamh_deleter};
+    check("pam_start_confdir(" + (dir ? std::string(*dir) : std::string{}) + ")");
+
+    if (remoteHost) {
+        res = pam_set_item(pamh, PAM_RHOST, remoteHost->c_str());
+        check("pam_set_item(PAM_RHOST)");
+    }
+
+    res = pam_authenticate(pamh, 0);
+    check("pam_authenticate");
+
+    res = pam_acct_mgmt(pamh, 0);
+    check("pam_acct_mgmt");
+
+    const void* item;
+    res = pam_get_item(pamh, PAM_USER, &item);
+    check("pam_get_item(PAM_USER)");
+    return (const char*)(item);
+}
+}
+
+std::string authenticate_pam(const std::string& blob, const std::optional<std::filesystem::path>& pamConfigDir, const std::optional<std::string>& remoteHost)
+{
+    return authenticate_pam(parseBasicAuth(blob), pamConfigDir, remoteHost);
+}
+}
+
+BOOST_FUSION_ADAPT_STRUCT(rousette::auth::UserPass, username, password);

src/restconf/PAM.h
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#pragma once
+#include <filesystem>
+#include <optional>
+#include <string>
+
+namespace rousette::auth {
+
+class Error : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+/** @brief Talk to PAM
+ *
+ * @param blob Raw data from the Authorization HTTP header
+ * @param pamConfigDir Override systemwide PAM configuration
+ * @param remoteHost Arbitrary debugging info about the remote host which triggered this action
+ *
+ * @return the authenticated username
+ */
+std::string authenticate_pam(const std::string& blob, const std::optional<std::filesystem::path>& pamConfigDir, const std::optional<std::string>& remoteHost);
+}

src/restconf/Server.cpp
@@ -14,10 +14,12 @@
 #include "http/utils.hpp"
 #include "restconf/Exceptions.h"
 #include "restconf/Nacm.h"
+#include "restconf/PAM.h"
 #include "restconf/Server.h"
 #include "restconf/uri.h"
 #include "restconf/utils.h"
 #include "sr/OpticalEvents.h"
+#include "NacmIdentities.h"
 
 using namespace std::literals;
 
@@ -186,7 +188,7 @@ Server::~Server()
     server->join();
 }
 
-Server::Server(sysrepo::Connection conn, const std::string& address, const std::string& port)
+Server::Server(sysrepo::Connection conn, const std::string& address, const std::string& port, const std::optional<std::filesystem::path>& pamConfigDir)
     : nacm(conn)
     , server{std::make_unique<nghttp2::asio_http2::server::http2>()}
     , dwdmEvents{std::make_unique<sr::OpticalEvents>(conn.sessionStart())}
@@ -225,7 +227,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle(restconfRoot,
-        [conn /* intentionally by value, otherwise conn gets destroyed when the ctor returns */, this](const auto& req, const auto& res) mutable {
+        [conn /* intentionally by value, otherwise conn gets destroyed when the ctor returns */, this, pamConfigDir](const auto& req, const auto& res) mutable {
             const auto& peer = http::peer_from_request(req);
             spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
 
@@ -238,10 +240,15 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
                 dataFormat = chooseDataEncoding(req.header());
 
                 std::string nacmUser;
-                if (auto userHeader = getHeaderValue(req.header(), "x-remote-user"); userHeader && !userHeader->empty()) {
-                     nacmUser = *userHeader;
+                if (auto authHeader = getHeaderValue(req.header(), "authorization")) {
+                    try {
+                        nacmUser = rousette::auth::authenticate_pam(*authHeader, pamConfigDir, peer);
+                    } catch (auth::Error& e) {
+                        spdlog::error("PAM failed: {}", e.what());
+                        throw ErrorResponse{401, "protocol", "access-denied", "Access denied."};
+                    }
                 } else {
-                     throw ErrorResponse(401, "protocol", "access-denied", "HTTP header x-remote-user not found or empty.");
+                    nacmUser = ANONYMOUS_USER;
                 }
 
                 if (!nacm.authorize(sess, nacmUser)) {

src/restconf/Server.h
@@ -6,6 +6,7 @@
 */
 
 #pragma once
+#include <filesystem>
 #include <sysrepo-cpp/Connection.hpp>
 #include <sysrepo-cpp/Subscription.hpp>
 #include "http/EventStream.h"
@@ -28,7 +29,8 @@ std::optional<std::string> as_subtree_path(const std::string& path);
 /** @short A RESTCONF-ish server */
 class Server {
 public:
-    explicit Server(sysrepo::Connection conn, const std::string& address, const std::string& port);
+    explicit Server(sysrepo::Connection conn, const std::string& address, const std::string& port,
+            const std::optional<std::filesystem::path>& pamConfigDir);
     ~Server();
 
 private:

src/restconf/main.cpp
@@ -72,7 +72,7 @@ int main(int argc [[maybe_unused]], char* argv [[maybe_unused]] [])
     }
 
     auto conn = sysrepo::Connection{};
-    auto server = rousette::restconf::Server{conn, "::1", "10080"};
+    auto server = rousette::restconf::Server{conn, "::1", "10080", std::nullopt};
 
     signal(SIGTERM, [](int) {});
     signal(SIGINT, [](int) {});

tests/configure.cmake.h.in
@@ -1,0 +1,2 @@
+#define CMAKE_CURRENT_SOURCE_DIR "@CMAKE_CURRENT_SOURCE_DIR@"
+#define CMAKE_CURRENT_BINARY_DIR "@CMAKE_CURRENT_BINARY_DIR@"

commit 248e5aa0916313a048cbc4fad644be6c52dc64df

tests: PAM: switch to pam_wrapper from pam_userdb
When I selected pam_userdb for testing, I choose it because it is a part
of upstream linux-PAM. However, distros such as Arch do not ship it by
default, and Fedora 40 will configure that module with a different
database backend (and that would require a different DB format).

It turns out that there's another mocking library for PAM, the
`pam_wrap` from `cwrap`, and its `pam_matrix` module. It has a nice
advantage that it doesn't require any fancy PAM overrides, it's just
something that gets activated by `LD_PRELOAD`. On the other hand,
pam_matrix doesn't appear to support colons in passwords, so there's one
less things that we can test.

On the plus side, it uncovered some fancy PAM bits:

- there's a `verbose` option which enables us to test the
  `PAM_ERROR_MSG` and `PAM_TEXT_INFO` branches (yay!)
- as a result, it calls the conversation function twice, the second time
  with zero number of messages, which triggered a UB in my code before

Also, now there's no need to mess with extra PAM config directories.

The upstream docs need changing because they talk about
`PAM_WRAPPER_DISABLE_DEEPBIND` environment variable whereas the actual
variable is called `UID_WRAPPER_DISABLE_DEEPBIND` in the C source code.
I'll send a patch upstream to clarify that.

Finally, it turned out that `pam_userdb` is suuuuuper sloooow on Fedora
38. A successful authentication takes about 2s, which is just
unacceptable. Note that this is definitely not the "PAM failure delay";
it's in the successful case as well.

Change-Id: Iffc7baff5b1772003f321cec2be6073857a4b502
Fixes: 542a8bd2f67fd6d77a5dcaab7bd810191e15721b PAM authentication

CMakeLists.txt
@@ -139,8 +139,10 @@ if(BUILD_TESTING)
 
     find_program(SYSREPOCTL sysrepoctl)
     find_program(SYSREPOCFG sysrepocfg)
-    find_program(BERKLEY_DB_LOAD db_load)
-    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tests/configure.cmake.h.in ${CMAKE_CURRENT_BINARY_DIR}/configure.cmake.h @ONLY)
+    pkg_check_modules(pam_wrapper REQUIRED IMPORTED_TARGET pam_wrapper)
+    pkg_get_variable(PAM_WRAPPER_MODULES_DIR pam_wrapper modules)
+    set(TEST_PAM_WRAPPER_DB ${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/users)
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/other.in ${CMAKE_CURRENT_BINARY_DIR}/tests/pam/other @ONLY)
 
     function(rousette_test)
         cmake_parse_arguments(TEST "" "NAME;FIXTURE;RESOURCE_LOCK" "LIBRARIES;COMMAND" ${ARGN})
@@ -188,25 +190,25 @@ if(BUILD_TESTING)
         endif()
     endfunction()
 
+    function(wrap_test_pam TEST_NAME)
+        set_property(TEST test-${TEST_NAME} APPEND PROPERTY ENVIRONMENT
+            "LD_PRELOAD=${pam_wrapper_LDFLAGS}"
+            "PAM_WRAPPER_SERVICE_DIR=${CMAKE_CURRENT_BINARY_DIR}/tests/pam"
+            "PAM_WRAPPER=1"
+            "UID_WRAPPER_DISABLE_DEEPBIND=1"
+            )
+    endfunction()
+
     rousette_test(NAME http-utils LIBRARIES rousette-http)
     rousette_test(NAME pam LIBRARIES rousette-auth)
-
-    set(TEST_PAM_USERDB_NOSUFFIX ${CMAKE_CURRENT_BINARY_DIR}/tests/pam/userdb)
-    set(TEST_PAM_USERDB_FILE ${TEST_PAM_USERDB_NOSUFFIX}.db)
-    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/other.in ${CMAKE_CURRENT_BINARY_DIR}/tests/pam/other @ONLY)
-    add_custom_command(OUTPUT ${TEST_PAM_USERDB_FILE}
-        COMMAND ${CMAKE_COMMAND} -E remove ${TEST_PAM_USERDB_FILE}
-        COMMAND ${BERKLEY_DB_LOAD} -f ${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/database.dump ${TEST_PAM_USERDB_FILE}
-        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tests/pam/database.dump)
-    add_custom_target(generate_pam_userdb DEPENDS ${TEST_PAM_USERDB_FILE})
-    add_dependencies(test-pam generate_pam_userdb)
+    wrap_test_pam(pam)
 
     set(fixture-http
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/ietf-system@2014-08-06.yang FEATURE radius
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-restconf@2017-01-26.yang
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang FEATURE f1)
     rousette_test(NAME http-communication LIBRARIES rousette-restconf FIXTURE fixture-http)
-    add_dependencies(test-http-communication generate_pam_userdb)
+    wrap_test_pam(http-communication)
 
     set(fixture-uri-parser
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang FEATURE f1

ci/pre.yaml
@@ -6,8 +6,8 @@
         state: present
       become: true
 
-    - name: install libdb-utils
+    - name: install pam_wrapper
       package:
-        name: libdb-utils
+        name: pam_wrapper
         state: present
       become: true

src/restconf/PAM.cpp
@@ -97,20 +97,28 @@ static int pam_userpass_conv(int num_msg, const struct pam_message** msg, struct
                 return PAM_BUF_ERR;
             }
             break;
-        /* case PAM_ERROR_MSG: */
-        /* case PAM_TEXT_INFO: */
+        case PAM_ERROR_MSG:
+            spdlog::error("PAM: pam_userpass_conv: PAM_ERROR_MSG: {}", msg[i]->msg);
+            break;
+        case PAM_TEXT_INFO:
+            spdlog::info("PAM: pam_userpass_conv: PAM_TEXT_INFO: {}", msg[i]->msg);
+            break;
         default:
-            spdlog::critical("PAM: pam_userpass_conv: unexpected msg_style {}", msg[i]->msg_style);
+            spdlog::critical("PAM: pam_userpass_conv: unexpected msg_style {}: {}", msg[i]->msg_style, msg[i]->msg);
             return PAM_CONV_ERR;
         }
         resp.get()[i].resp_retcode = PAM_SUCCESS;
         resp.get()[i].resp = str;
     }
-    *resp_r = resp.release();
+    if (resp_r) {
+        *resp_r = resp.release();
+    } else {
+        spdlog::warn("PAM: pam_userpass_conv: no reply expected, weird");
+    }
     return PAM_SUCCESS;
 }
 
-std::string authenticate_pam(const UserPass& userPass, const std::optional<std::filesystem::path>& dir, const std::optional<std::string>& remoteHost)
+std::string authenticate_pam(const UserPass& userPass, const std::optional<std::string>& remoteHost)
 {
     pam_handle_t* pamh = nullptr;
     int res;
@@ -129,9 +137,9 @@ std::string authenticate_pam(const UserPass& userPass, const std::optional<std::
         pam_end(pamh, res);
     };
 
-    res = pam_start_confdir("rousette", userPass.username.c_str(), &conv, dir ? std::string(*dir).c_str() : nullptr, &pamh);
+    res = pam_start("rousette", userPass.username.c_str(), &conv, &pamh);
     std::unique_ptr<pam_handle_t, decltype(pamh_deleter)> guard{pamh, pamh_deleter};
-    check("pam_start_confdir(" + (dir ? std::string(*dir) : std::string{}) + ")");
+    check("pam_start()");
 
     if (remoteHost) {
         res = pam_set_item(pamh, PAM_RHOST, remoteHost->c_str());
@@ -151,9 +159,9 @@ std::string authenticate_pam(const UserPass& userPass, const std::optional<std::
 }
 }
 
-std::string authenticate_pam(const std::string& blob, const std::optional<std::filesystem::path>& pamConfigDir, const std::optional<std::string>& remoteHost)
+std::string authenticate_pam(const std::string& blob, const std::optional<std::string>& remoteHost)
 {
-    return authenticate_pam(parseBasicAuth(blob), pamConfigDir, remoteHost);
+    return authenticate_pam(parseBasicAuth(blob), remoteHost);
 }
 }
 

src/restconf/PAM.h
@@ -6,7 +6,6 @@
 */
 
 #pragma once
-#include <filesystem>
 #include <optional>
 #include <string>
 
@@ -19,10 +18,9 @@ class Error : public std::runtime_error {
 /** @brief Talk to PAM
  *
  * @param blob Raw data from the Authorization HTTP header
- * @param pamConfigDir Override systemwide PAM configuration
  * @param remoteHost Arbitrary debugging info about the remote host which triggered this action
  *
  * @return the authenticated username
  */
-std::string authenticate_pam(const std::string& blob, const std::optional<std::filesystem::path>& pamConfigDir, const std::optional<std::string>& remoteHost);
+std::string authenticate_pam(const std::string& blob, const std::optional<std::string>& remoteHost);
 }

src/restconf/Server.cpp
@@ -192,7 +192,7 @@ Server::~Server()
     server->join();
 }
 
-Server::Server(sysrepo::Connection conn, const std::string& address, const std::string& port, const std::optional<std::filesystem::path>& pamConfigDir)
+Server::Server(sysrepo::Connection conn, const std::string& address, const std::string& port)
     : nacm(conn)
     , server{std::make_unique<nghttp2::asio_http2::server::http2>()}
     , dwdmEvents{std::make_unique<sr::OpticalEvents>(conn.sessionStart())}
@@ -231,7 +231,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle(restconfRoot,
-        [conn /* intentionally by value, otherwise conn gets destroyed when the ctor returns */, this, pamConfigDir](const auto& req, const auto& res) mutable {
+        [conn /* intentionally by value, otherwise conn gets destroyed when the ctor returns */, this](const auto& req, const auto& res) mutable {
             const auto& peer = http::peer_from_request(req);
             spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
 
@@ -246,7 +246,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
                 std::string nacmUser;
                 if (auto authHeader = getHeaderValue(req.header(), "authorization")) {
                     try {
-                        nacmUser = rousette::auth::authenticate_pam(*authHeader, pamConfigDir, peer);
+                        nacmUser = rousette::auth::authenticate_pam(*authHeader, peer);
                     } catch (auth::Error& e) {
                         spdlog::error("PAM failed: {}", e.what());
                         throw ErrorResponse{401, "protocol", "access-denied", "Access denied."};

src/restconf/Server.h
@@ -6,7 +6,6 @@
 */
 
 #pragma once
-#include <filesystem>
 #include <sysrepo-cpp/Connection.hpp>
 #include <sysrepo-cpp/Subscription.hpp>
 #include "http/EventStream.h"
@@ -29,8 +28,7 @@ std::optional<std::string> as_subtree_path(const std::string& path);
 /** @short A RESTCONF-ish server */
 class Server {
 public:
-    explicit Server(sysrepo::Connection conn, const std::string& address, const std::string& port,
-            const std::optional<std::filesystem::path>& pamConfigDir);
+    explicit Server(sysrepo::Connection conn, const std::string& address, const std::string& port);
     ~Server();
 
 private:

src/restconf/main.cpp
@@ -72,7 +72,7 @@ int main(int argc [[maybe_unused]], char* argv [[maybe_unused]] [])
     }
 
     auto conn = sysrepo::Connection{};
-    auto server = rousette::restconf::Server{conn, "::1", "10080", std::nullopt};
+    auto server = rousette::restconf::Server{conn, "::1", "10080"};
 
     signal(SIGTERM, [](int) {});
     signal(SIGINT, [](int) {});

tests/configure.cmake.h.in
@@ -1,2 +1,0 @@
-#define CMAKE_CURRENT_SOURCE_DIR "@CMAKE_CURRENT_SOURCE_DIR@"
-#define CMAKE_CURRENT_BINARY_DIR "@CMAKE_CURRENT_BINARY_DIR@"

tests/http-communication.cpp
@@ -14,7 +14,6 @@
 #include <sysrepo-cpp/Session.hpp>
 #include "restconf/Server.h"
 #include "tests/UniqueResource.h"
-#include "configure.cmake.h"
 
 using namespace std::string_literals;
 namespace ng = nghttp2::asio_http2;
@@ -126,7 +125,7 @@ TEST_CASE("HTTP")
     auto srSess = srConn.sessionStart(sysrepo::Datastore::Running);
     srSess.copyConfig(sysrepo::Datastore::Startup, "ietf-netconf-acm");
 
-    auto server = rousette::restconf::Server{srConn, SERVER_ADDRESS, SERVER_PORT, std::filesystem::path(CMAKE_CURRENT_BINARY_DIR) / "tests" / "pam"};
+    auto server = rousette::restconf::Server{srConn, SERVER_ADDRESS, SERVER_PORT};
     auto guard = make_unique_resource([] {},
                                       [&]() {
                                           srSess.switchDatastore(sysrepo::Datastore::Running);

tests/pam.cpp
@@ -7,7 +7,6 @@
 
 #include "trompeloeil_doctest.h"
 #include "restconf/PAM.h"
-#include "configure.cmake.h"
 
 TEST_CASE("URI path parser")
 {
@@ -41,14 +40,7 @@ TEST_CASE("URI path parser")
             blob = "bm9ydWxlczplbXB0eQ==";
         }
 
-        SECTION("double colon") {
-            username = "foo";
-            blob = "Zm9vOmJhcjpiYXo=";
-        }
-
-        REQUIRE(authenticate_pam("Basic " + blob,
-                    std::filesystem::path(CMAKE_CURRENT_BINARY_DIR) / "tests" / "pam",
-                    "[::1]:666") == username);
+        REQUIRE(authenticate_pam("Basic " + blob, "[::1]:666") == username);
     }
 
     SECTION("failed") {
@@ -67,7 +59,7 @@ TEST_CASE("URI path parser")
 
         SECTION("invalid user") {
             input = "Basic MDox";
-            error = "PAM: pam_authenticate: User not known to the underlying authentication module";
+            error = "PAM: pam_authenticate: Authentication failure";
         }
 
         SECTION("wrong password") {
@@ -76,7 +68,7 @@ TEST_CASE("URI path parser")
         }
 
         REQUIRE_THROWS_WITH_AS(
-                authenticate_pam(input, std::filesystem::path(CMAKE_CURRENT_BINARY_DIR) / "tests" / "pam", ""),
+                authenticate_pam(input, ""),
                 error.c_str(),
                 rousette::auth::Error);
     }

tests/pam/database.dump
@@ -1,19 +1,0 @@
-VERSION=3
-format=print
-type=hash
-h_nelem=6
-db_pagesize=4096
-HEADER=END
- Aladdin
- open sesame
- dwdm
- DWDM
- root
- sekrit
- yangnobody
- no chance
- norules
- empty
- foo
- bar:baz
-DATA=END

tests/pam/other.in
@@ -1,6 +1,4 @@
-auth required pam_userdb.so db=@TEST_PAM_USERDB_NOSUFFIX@ debug crypt=none
-account required pam_userdb.so db=@TEST_PAM_USERDB_NOSUFFIX@ debug crypt=none
-password required pam_warn.so
-password required pam_deny.so
-session  required pam_warn.so
-session  required pam_deny.so
+auth required @PAM_WRAPPER_MODULES_DIR@/pam_matrix.so passdb=@TEST_PAM_WRAPPER_DB@ verbose
+account required @PAM_WRAPPER_MODULES_DIR@/pam_matrix.so passdb=@TEST_PAM_WRAPPER_DB@ verbose
+password required @PAM_WRAPPER_MODULES_DIR@/pam_matrix.so passdb=@TEST_PAM_WRAPPER_DB@ verbose
+session required @PAM_WRAPPER_MODULES_DIR@/pam_matrix.so passdb=@TEST_PAM_WRAPPER_DB@ verbose

tests/pam/users
@@ -1,0 +1,5 @@
+Aladdin:open sesame:rousette
+dwdm:DWDM:rousette
+root:sekrit:rousette
+yangnobody:no chance:rousette
+norules:empty:rousette