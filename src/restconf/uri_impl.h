2c07ace3925a10d32c9b587a5f97245955805589
Tomáš Pecka <tomas.pecka@cesnet.cz>
2023-11-27 11:19:39 +0100

    server: collect type of resource from URI
    RESTCONF has several resources under /restconf. We already implement
    /restconf/data (RFC 8040 [1]) and /restconf/ds (RFC 8527 [2]).
    
    In the next commit we will add also /restconf/operations for RPC calls.
    However, to verify that RPCs were invoked with corrent resource we need
    to remember the type of the resource. This commit introduces the basic
    support as a preparation for the next commit adding RPC/Action support.
    
    Change-Id: I6ade004d405d406569babe9d7629663fd67d05b2

--- src/restconf/uri_impl.h
+++ src/restconf/uri_impl.h
@@ -15,14 +15,28 @@ namespace rousette::restconf {
 
 namespace impl {
 
+enum class ResourceType {
+    DATA,
+    DATASTORE_DATA,
+};
+
+struct Resource {
+    ResourceType resourceType;
+    boost::optional<ApiIdentifier> datastore;
+
+    Resource();
+    Resource(ResourceType resourceType, const boost::optional<ApiIdentifier>& datastore);
+    bool operator==(const Resource&) const = default;
+};
+
 /** @brief Represents parsed URI path split into segments delimited by a `/` separator. */
 struct ResourcePath {
-    boost::optional<ApiIdentifier> datastore;
+    Resource resource;
     std::vector<PathSegment> segments;
 
     ResourcePath();
     ResourcePath(const std::vector<PathSegment>& pathSegments);
-    ResourcePath(const boost::optional<ApiIdentifier>& datastore, const std::vector<PathSegment>& pathSegments);
+    ResourcePath(const Resource& resource, const std::vector<PathSegment>& pathSegments);
 
     bool operator==(const ResourcePath&) const = default;
 };
@@ -31,6 +45,7 @@ std::optional<ResourcePath> parseUriPath(const std::string& uriPath);
 }
 }
 
-BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::ResourcePath, datastore, segments);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::Resource, resourceType, datastore);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::ResourcePath, resource, segments);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::PathSegment, apiIdent, keys);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ApiIdentifier, prefix, identifier);
