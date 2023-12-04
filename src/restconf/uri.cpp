CESNET/rousette

commit 600875981b18c1dca02f539dac4a0e1441973b27

restconf: rename ResourcePath to URI
The struct ResourcePath which encapsulates parsed URI (i.e., the
datastore and path segments) may be confusing a bit. It represents the
parsed URI so why not call it just URI?

Yeah, it is not exactly "URI" we are missing schema and other stuff, but
we think this name represents the purpose of the struct better.

Signed-off-by: Jan Kundrát <jan.kundrat@cesnet.cz>
Change-Id: If1845171cff2368837c74bc9b0c61734f71a9030

src/restconf/uri.cpp
@@ -34,12 +34,12 @@ const auto fullyQualifiedApiIdentifier = x3::rule<class identifier, ApiIdentifie
 const auto fullyQualifiedListInstance = x3::rule<class keyList, PathSegment>{"listInstance"} = fullyQualifiedApiIdentifier >> -('=' >> keyList);
 const auto datastore = x3::rule<class datastore, boost::optional<ApiIdentifier>>{"datastore"} = x3::lit("data") | (x3::lit("ds") >> "/" >> fullyQualifiedApiIdentifier);
 const auto uriPath = x3::rule<class uriPath, std::vector<PathSegment>>{"uriPath"} = -x3::lit("/") >> -(fullyQualifiedListInstance >> -("/" >> listInstance % "/")); // RFC 8040, sec 3.5.3
-const auto uriGrammar = x3::rule<class grammar, ResourcePath>{"grammar"} = x3::lit("/") >> x3::lit("restconf") >> "/" >> datastore >> uriPath;
+const auto uriGrammar = x3::rule<class grammar, URI>{"grammar"} = x3::lit("/") >> x3::lit("restconf") >> "/" >> datastore >> uriPath;
 }
 
-std::optional<ResourcePath> parseUriPath(const std::string& uriPath)
+std::optional<URI> parseUriPath(const std::string& uriPath)
 {
-    ResourcePath out;
+    URI out;
     auto iter = std::begin(uriPath);
     auto end = std::end(uriPath);
 
@@ -50,15 +50,15 @@ std::optional<ResourcePath> parseUriPath(const std::string& uriPath)
     return out;
 }
 
-ResourcePath::ResourcePath() = default;
+URI::URI() = default;
 
-ResourcePath::ResourcePath(const boost::optional<ApiIdentifier>& datastore, const std::vector<PathSegment>& segments)
+URI::URI(const boost::optional<ApiIdentifier>& datastore, const std::vector<PathSegment>& segments)
     : datastore(datastore)
     , segments(segments)
 {
 }
 
-ResourcePath::ResourcePath(const std::vector<PathSegment>& segments)
+URI::URI(const std::vector<PathSegment>& segments)
     : segments(segments)
 {
 }

src/restconf/uri_impl.h
@@ -16,21 +16,21 @@ namespace rousette::restconf {
 namespace impl {
 
 /** @brief Represents parsed URI path split into segments delimited by a `/` separator. */
-struct ResourcePath {
+struct URI {
     boost::optional<ApiIdentifier> datastore;
     std::vector<PathSegment> segments;
 
-    ResourcePath();
-    ResourcePath(const std::vector<PathSegment>& pathSegments);
-    ResourcePath(const boost::optional<ApiIdentifier>& datastore, const std::vector<PathSegment>& pathSegments);
+    URI();
+    URI(const std::vector<PathSegment>& pathSegments);
+    URI(const boost::optional<ApiIdentifier>& datastore, const std::vector<PathSegment>& pathSegments);
 
-    bool operator==(const ResourcePath&) const = default;
+    bool operator==(const URI&) const = default;
 };
 
-std::optional<ResourcePath> parseUriPath(const std::string& uriPath);
+std::optional<URI> parseUriPath(const std::string& uriPath);
 }
 }
 
-BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::ResourcePath, datastore, segments);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::URI, datastore, segments);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::PathSegment, apiIdent, keys);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ApiIdentifier, prefix, identifier);

tests/uri-parser.cpp
@@ -47,7 +47,7 @@ std::ostream& operator<<(std::ostream& os, const std::vector<PathSegment>& v)
 }
 
 namespace rousette::restconf::impl {
-std::ostream& operator<<(std::ostream& os, const ResourcePath& obj)
+std::ostream& operator<<(std::ostream& os, const URI& obj)
 {
     os << "[";
     std::copy(obj.segments.begin(), obj.segments.end(), std::experimental::make_ostream_joiner(os, ", "));
@@ -73,8 +73,8 @@ struct StringMaker<std::optional<std::string>> {
 };
 
 template <>
-struct StringMaker<std::optional<rousette::restconf::impl::ResourcePath>> {
-    static String convert(const std::optional<rousette::restconf::impl::ResourcePath>& obj)
+struct StringMaker<std::optional<rousette::restconf::impl::URI>> {
+    static String convert(const std::optional<rousette::restconf::impl::URI>& obj)
     {
         std::ostringstream oss;
 
@@ -93,140 +93,140 @@ TEST_CASE("URI path parser")
 {
     using rousette::restconf::ApiIdentifier;
     using rousette::restconf::PathSegment;
-    using rousette::restconf::impl::ResourcePath;
+    using rousette::restconf::impl::URI;
 
     SECTION("Valid paths")
     {
         for (const auto& [uriPath, expected] : {
-                 std::pair<std::string, ResourcePath>{"/restconf/data/x333:y666", ResourcePath({
-                                                                                      {{"x333", "y666"}},
-                                                                                  })},
-                 {"/restconf/data/foo:bar", ResourcePath(std::vector<PathSegment>{
+                 std::pair<std::string, URI>{"/restconf/data/x333:y666", URI({
+                                                                             {{"x333", "y666"}},
+                                                                         })},
+                 {"/restconf/data/foo:bar", URI(std::vector<PathSegment>{
                                                 {{"foo", "bar"}},
                                             })},
-                 {"/restconf/data/foo:bar/baz", ResourcePath({
+                 {"/restconf/data/foo:bar/baz", URI({
                                                     {{"foo", "bar"}},
                                                     {{"baz"}},
                                                 })},
-                 {"/restconf/data/foo:bar/meh:baz", ResourcePath({
+                 {"/restconf/data/foo:bar/meh:baz", URI({
                                                         {{"foo", "bar"}},
                                                         {{"meh", "baz"}},
                                                     })},
-                 {"/restconf/data/foo:bar/yay/meh:baz", ResourcePath({
+                 {"/restconf/data/foo:bar/yay/meh:baz", URI({
                                                             {{"foo", "bar"}},
                                                             {{"yay"}},
                                                             {{"meh", "baz"}},
                                                         })},
-                 {"/restconf/data/foo:bar/Y=val", ResourcePath({
+                 {"/restconf/data/foo:bar/Y=val", URI({
                                                       {{"foo", "bar"}},
                                                       {{"Y"}, {"val"}},
                                                   })},
-                 {"/restconf/data/foo:bar/Y=val-ue", ResourcePath({
+                 {"/restconf/data/foo:bar/Y=val-ue", URI({
                                                          {{"foo", "bar"}},
                                                          {{"Y"}, {"val-ue"}},
                                                      })},
-                 {"/restconf/data/foo:bar/p:lst=key1", ResourcePath({
+                 {"/restconf/data/foo:bar/p:lst=key1", URI({
                                                            {{"foo", "bar"}},
                                                            {{"p", "lst"}, {"key1"}},
                                                        })},
 
-                 {"/restconf/data/foo:bar/p:lst=key1/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/p:lst=key1/leaf", URI({
                                                                 {{"foo", "bar"}},
                                                                 {{"p", "lst"}, {"key1"}},
                                                                 {{"leaf"}},
                                                             })},
-                 {"/restconf/data/foo:bar/lst=key1,", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,", URI({
                                                           {{"foo", "bar"}},
                                                           {{"lst"}, {"key1", ""}},
                                                       })},
-                 {"/restconf/data/foo:bar/lst=key1,,,", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,,,", URI({
                                                             {{"foo", "bar"}},
                                                             {{"lst"}, {"key1", "", "", ""}},
                                                         })},
-                 {"/restconf/data/foo:bar/lst=key1,/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,/leaf", URI({
                                                                {{"foo", "bar"}},
                                                                {{"lst"}, {"key1", ""}},
                                                                {{"leaf"}},
                                                            })},
-                 {"/restconf/data/foo:bar/lst=key1,key2", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,key2", URI({
                                                               {{"foo", "bar"}},
                                                               {{"lst"}, {"key1", "key2"}},
                                                           })},
-                 {"/restconf/data/foo:bar/lst=key1,key2/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,key2/leaf", URI({
                                                                    {{"foo", "bar"}},
                                                                    {{"lst"}, {"key1", "key2"}},
                                                                    {{"leaf"}},
                                                                })},
-                 {"/restconf/data/foo:bar/lst=key1,key2/lst2=key1/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,key2/lst2=key1/leaf", URI({
                                                                              {{"foo", "bar"}},
                                                                              {{"lst"}, {"key1", "key2"}},
                                                                              {{"lst2"}, {"key1"}},
                                                                              {{"leaf"}},
                                                                          })},
-                 {"/restconf/data/foo:bar/lst=,key2/lst2=key1/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=,key2/lst2=key1/leaf", URI({
                                                                          {{"foo", "bar"}},
                                                                          {{"lst"}, {"", "key2"}},
                                                                          {{"lst2"}, {"key1"}},
                                                                          {{"leaf"}},
                                                                      })},
-                 {"/restconf/data/foo:bar/lst=,/lst2=key1/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=,/lst2=key1/leaf", URI({
                                                                      {{"foo", "bar"}},
                                                                      {{"lst"}, {"", ""}},
                                                                      {{"lst2"}, {"key1"}},
                                                                      {{"leaf"}},
                                                                  })},
-                 {"/restconf/data/foo:bar/lst=", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=", URI({
                                                      {{"foo", "bar"}},
                                                      {{"lst"}, {""}},
                                                  })},
-                 {"/restconf/data/foo:bar/lst=/leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=/leaf", URI({
                                                           {{"foo", "bar"}},
                                                           {{"lst"}, {""}},
                                                           {{"leaf"}},
                                                       })},
-                 {"/restconf/data/foo:bar/prefix:lst=key1/prefix:leaf", ResourcePath({
+                 {"/restconf/data/foo:bar/prefix:lst=key1/prefix:leaf", URI({
                                                                             {{"foo", "bar"}},
                                                                             {{"prefix", "lst"}, {"key1"}},
                                                                             {{"prefix", "leaf"}},
                                                                         })},
-                 {"/restconf/data/foo:bar/lst=key1,,key3", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key1,,key3", URI({
                                                                {{"foo", "bar"}},
                                                                {{"lst"}, {"key1", "", "key3"}},
                                                            })},
-                 {"/restconf/data/foo:bar/lst=key%2CWithCommas,,key2C", ResourcePath({
+                 {"/restconf/data/foo:bar/lst=key%2CWithCommas,,key2C", URI({
                                                                             {{"foo", "bar"}},
                                                                             {{"lst"}, {"key,WithCommas", "", "key2C"}},
                                                                         })},
-                 {R"(/restconf/data/foo:bar/list1=%2C%27"%3A"%20%2F,,foo)", ResourcePath({
+                 {R"(/restconf/data/foo:bar/list1=%2C%27"%3A"%20%2F,,foo)", URI({
                                                                                 {{"foo", "bar"}},
                                                                                 {{"list1"}, {R"(,'":" /)", "", "foo"}},
                                                                             })},
-                 {"/restconf/data/foo:bar/list1= %20,%20,foo", ResourcePath({
+                 {"/restconf/data/foo:bar/list1= %20,%20,foo", URI({
                                                                    {{"foo", "bar"}},
                                                                    {{"list1"}, {"  ", " ", "foo"}},
                                                                })},
-                 {"/restconf/data/foo:bar/list1= %20,%20, ", ResourcePath({
+                 {"/restconf/data/foo:bar/list1= %20,%20, ", URI({
                                                                  {{"foo", "bar"}},
                                                                  {{"list1"}, {"  ", " ", " "}},
                                                              })},
-                 {"/restconf/data/foo:bar/list1=žluťoučkýkůň", ResourcePath({
+                 {"/restconf/data/foo:bar/list1=žluťoučkýkůň", URI({
                                                                    {{"foo", "bar"}},
                                                                    {{"list1"}, {"žluťoučkýkůň"}},
                                                                })},
-                 {"/restconf/data/foo:list=A%20Z", ResourcePath({
+                 {"/restconf/data/foo:list=A%20Z", URI({
                                                        {{"foo", "list"}, {"A Z"}},
                                                    })},
-                 {"/restconf/data/foo:list=A%25Z", ResourcePath({
+                 {"/restconf/data/foo:list=A%25Z", URI({
                                                        {{"foo", "list"}, {"A%Z"}},
                                                    })},
-                 {"/restconf/data", ResourcePath({}, {})},
-                 {"/restconf/data/", ResourcePath({}, {})},
+                 {"/restconf/data", URI({}, {})},
+                 {"/restconf/data/", URI({}, {})},
 
                  // RFC 8527 uris
-                 {"/restconf/ds/hello:world", ResourcePath(ApiIdentifier{"hello", "world"}, {})},
-                 {"/restconf/ds/ietf-datastores:running/foo:bar/list1=a", ResourcePath(ApiIdentifier{"ietf-datastores", "running"}, {{{"foo", "bar"}}, {{"list1"}, {"a"}}})},
-                 {"/restconf/ds/ietf-datastores:operational", ResourcePath(ApiIdentifier{"ietf-datastores", "operational"}, {})},
-                 {"/restconf/ds/ietf-datastores:operational/", ResourcePath(ApiIdentifier{"ietf-datastores", "operational"}, {})},
+                 {"/restconf/ds/hello:world", URI(ApiIdentifier{"hello", "world"}, {})},
+                 {"/restconf/ds/ietf-datastores:running/foo:bar/list1=a", URI(ApiIdentifier{"ietf-datastores", "running"}, {{{"foo", "bar"}}, {{"list1"}, {"a"}}})},
+                 {"/restconf/ds/ietf-datastores:operational", URI(ApiIdentifier{"ietf-datastores", "operational"}, {})},
+                 {"/restconf/ds/ietf-datastores:operational/", URI(ApiIdentifier{"ietf-datastores", "operational"}, {})},
              }) {
 
             CAPTURE(uriPath);

commit 02b7a6c45670b42401c3d751f73a8888bbfda2b7

restconf: rename variables after renaming the datatypes
In 6008759 ("restconf: rename ResourcePath to URI") I renamed the
structs but forgot about variables.

Fixes: 600875981b18c1dca02f539dac4a0e1441973b27 ("restconf: rename ResourcePath to URI")
Change-Id: I3c6eb3167ffb53754b866e1fb57dd7cc6e1e062e

src/restconf/uri.cpp
@@ -63,8 +63,8 @@ URIPrefix::URIPrefix(URIPrefix::Type resourceType, const boost::optional<ApiIden
 
 URI::URI() = default;
 
-URI::URI(const URIPrefix& resource, const std::vector<PathSegment>& segments)
-    : resource(resource)
+URI::URI(const URIPrefix& prefix, const std::vector<PathSegment>& segments)
+    : prefix(prefix)
     , segments(segments)
 {
 }
@@ -268,14 +268,14 @@ std::string asLibyangPath(const libyang::Context& ctx, const std::vector<PathSeg
  */
 DatastoreAndPath asLibyangPath(const libyang::Context& ctx, const std::string& uriPath)
 {
-    auto resourcePath = impl::parseUriPath(uriPath);
-    if (!resourcePath) {
+    auto uri = impl::parseUriPath(uriPath);
+    if (!uri) {
         throw InvalidURIException("Syntax error");
     }
-    if (resourcePath->segments.empty()) {
-        return {resourcePath->resource.datastore, "/*"};
+    if (uri->segments.empty()) {
+        return {uri->prefix.datastore, "/*"};
     }
-    return {resourcePath->resource.datastore, asLibyangPath(ctx, resourcePath->segments.begin(), resourcePath->segments.end())};
+    return {uri->prefix.datastore, asLibyangPath(ctx, uri->segments.begin(), uri->segments.end())};
 }
 
 /** @brief Transforms URI path (i.e., data resource identifier) into a path that is understood by libyang and a datastore (RFC 8527).
@@ -290,26 +290,26 @@ DatastoreAndPath asLibyangPath(const libyang::Context& ctx, const std::string& u
  */
 std::pair<DatastoreAndPath, PathSegment> asLibyangPathSplit(const libyang::Context& ctx, const std::string& uriPath)
 {
-    auto resourcePath = impl::parseUriPath(uriPath);
-    if (!resourcePath) {
+    auto uri = impl::parseUriPath(uriPath);
+    if (!uri) {
         throw InvalidURIException("Syntax error");
     }
-    if (resourcePath->segments.empty()) {
+    if (uri->segments.empty()) {
         throw InvalidURIException("Cannot split the datastore resource URI");
     }
 
 
-    auto lastSegment = resourcePath->segments.back();
-    auto parentPath = asLibyangPath(ctx, resourcePath->segments.begin(), resourcePath->segments.end() - 1);
+    auto lastSegment = uri->segments.back();
+    auto parentPath = asLibyangPath(ctx, uri->segments.begin(), uri->segments.end() - 1);
 
     // we know that the path is valid so we can get last segment module from ly
     if (!lastSegment.apiIdent.prefix) {
-        auto fullPath = asLibyangPath(ctx, resourcePath->segments.begin(), resourcePath->segments.end());
+        auto fullPath = asLibyangPath(ctx, uri->segments.begin(), uri->segments.end());
         auto lastNode = ctx.findPath(fullPath);
         lastSegment.apiIdent.prefix = std::string(lastNode.module().name());
     }
 
-    return {{resourcePath->resource.datastore, parentPath}, lastSegment};
+    return {{uri->prefix.datastore, parentPath}, lastSegment};
 }
 
 }

src/restconf/uri_impl.h
@@ -33,12 +33,12 @@ struct URIPrefix {
 
 /** @brief Represents parsed URI path split into segments delimited by a `/` separator. */
 struct URI {
-    URIPrefix resource;
+    URIPrefix prefix;
     std::vector<PathSegment> segments;
 
     URI();
     URI(const std::vector<PathSegment>& pathSegments);
-    URI(const URIPrefix& resource, const std::vector<PathSegment>& pathSegments);
+    URI(const URIPrefix& prefix, const std::vector<PathSegment>& pathSegments);
 
     bool operator==(const URI&) const = default;
 };
@@ -48,6 +48,6 @@ std::optional<URI> parseUriPath(const std::string& uriPath);
 }
 
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::URIPrefix, resourceType, datastore);
-BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::URI, resource, segments);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::impl::URI, prefix, segments);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::PathSegment, apiIdent, keys);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ApiIdentifier, prefix, identifier);