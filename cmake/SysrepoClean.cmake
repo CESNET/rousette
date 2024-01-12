8042ff7ed336151b4b2e7b9c2c84e321d316def8
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-01-12 19:57:07 +0100

    cmake: tests: cleanup: don't error out if there is nothing to remove
    I'd like to be able to keep running this "test" (it's a cleanup code for
    a test, but in CMake/CTest, that means that it's a test) no matter
    whether the initialization and the actual test have run. Before this
    commit, it would fail because these globs produced no results, and
    therefore the CMake code would call something with too few arguments.
    
    This relies on CMake silently ignoring nonexisting files. That's easier
    than checking for list length because that would require an extra
    variable, etc.
    
    Change-Id: Iabd23b6cb98ac81150bb38dd44b5e3f01cf33e2b

--- cmake/SysrepoClean.cmake
+++ cmake/SysrepoClean.cmake
@@ -5,10 +5,10 @@ foreach(var THIS_BINARY_DIR TEST_NAME SYSREPO_SHM_PREFIX)
 endforeach()
 
 set(shm_files_pattern "/dev/shm/${SYSREPO_SHM_PREFIX}_*")
-
 file(GLOB shm_files ${shm_files_pattern})
+set(dummy_nonexisting_file_to_silence_warnings ${CMAKE_CURRENT_BUILD_DIR}/sysrepo-dummy-non-existing-file-for-cleanup)
 
 message(STATUS "Removing ${shm_files_pattern}")
-file(REMOVE ${shm_files})
+file(REMOVE ${shm_files} ${dummy_nonexisting_file_to_silence_warnings})
 message(STATUS "Removing ${THIS_BINARY_DIR}/test_repositories/test_${TEST_NAME}")
 file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/test_repositories/test_${TEST_NAME}")
