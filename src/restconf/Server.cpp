CESNET/rousette

commit 2e19ce3c9923792ac28c7549d121476b7b595f93

restconf: add endpoint for getting YANG schemas
This is a preparation for implementing Schema Resource defined in
RESTCONF RFC [1, sec. 3.7].

This commit adds endpoint which returns YANG schemas for queried module.
The RFC states that

  Note that there is no required structure for this URL.

I have chosen a /yang/module(@revision)? endpoint for that. I did not
want to put this under the /restconf root so we do not clash with
paths defined by some possible future restconf RFCs.

[1] https://www.rfc-editor.org/rfc/rfc8040#section-3.7

Change-Id: I6a58fd88a33209b13dc5dc0f96b06993d890daed

CMakeLists.txt
@@ -181,4 +181,5 @@ if(BUILD_TESTING)
     rousette_test(NAME restconf-writing LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-rpc LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-nacm LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
+    rousette_test(NAME restconf-yang-schema LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
 endif()

src/restconf/Server.cpp
@@ -41,6 +41,7 @@ auto as_restconf_push_update(const std::string& content, const T& time)
 }
 
 constexpr auto restconfRoot = "/restconf/";
+constexpr auto yangSchemaRoot = "/yang/";
 
 std::string asMimeType(libyang::DataFormat dataFormat)
 {
@@ -468,6 +469,36 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     server->handle("/telemetry/optics", [this](const auto& req, const auto& res) {
         auto client = std::make_shared<http::EventStream>(req, res);
         client->activate(opticsChange, as_restconf_push_update(dwdmEvents->currentData(), std::chrono::system_clock::now()));
+    });
+
+    server->handle(yangSchemaRoot, [conn /* intentional copy */](const auto& req, const auto& res) mutable {
+        const auto& peer = http::peer_from_request(req);
+        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+
+        if (req.method() != "GET") {
+            res.write_head(405, {{"access-control-allow-origin", {"*", false}}});
+            res.end();
+            return;
+        }
+
+        auto sess = conn.sessionStart(sysrepo::Datastore::Operational);
+
+        // TODO: Perhaps authorize users before providing the schemas so they could not scan for "known vulnerabilities"?
+
+        auto mod = asYangModule(sess.getContext(), req.uri().path);
+        if (!mod) {
+            res.write_head(404, {{"access-control-allow-origin", {"*", false}}});
+            res.end();
+            return;
+        }
+
+        res.write_head(
+            200,
+            {
+                {"content-type", {"application/yang", false}},
+                {"access-control-allow-origin", {"*", false}},
+            });
+        res.end(mod->printStr(libyang::SchemaOutputFormat::Yang));
     });
 
     server->handle(restconfRoot,

tests/restconf-yang-schema.cpp
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ */
+
+static const auto SERVER_PORT = "10085";
+#include <nghttp2/asio_http2.h>
+#include <spdlog/spdlog.h>
+#include "restconf/Server.h"
+#include "tests/aux-utils.h"
+
+TEST_CASE("obtaining YANG schemas")
+{
+    spdlog::set_level(spdlog::level::trace);
+    auto srConn = sysrepo::Connection{};
+    auto srSess = srConn.sessionStart(sysrepo::Datastore::Running);
+    auto nacmGuard = manageNacm(srSess);
+    auto server = rousette::restconf::Server{srConn, SERVER_ADDRESS, SERVER_PORT};
+
+    SECTION("unsupported methods")
+    {
+        for (const std::string httpMethod : {"POST", "PUT", "OPTIONS", "PATCH", "DELETE"}) {
+            CAPTURE(httpMethod);
+            REQUIRE(clientRequest(httpMethod, YANG_ROOT "/ietf-yang-library@2019-01-04", "", {}) == Response{405, noContentTypeHeaders, ""});
+        }
+    }
+
+    SECTION("loaded modules")
+    {
+        SECTION("module with revision")
+        {
+            SECTION("no revision in uri")
+            {
+                REQUIRE(get(YANG_ROOT "/ietf-system", {}) == Response{404, noContentTypeHeaders, ""});
+            }
+            SECTION("correct revision in uri")
+            {
+                auto resp = get(YANG_ROOT "/ietf-system@2014-08-06", {});
+                auto expectedShortenedResp = Response{200, yangHeaders, "module ietf-system {\n  namespa"};
+
+                REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
+                REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
+            }
+            SECTION("wrong revision in uri")
+            {
+                REQUIRE(get(YANG_ROOT "/ietf-system@1999-12-13", {}) == Response{404, noContentTypeHeaders, ""});
+                REQUIRE(get(YANG_ROOT "/ietf-system@abcd-ef-gh", {}) == Response{404, noContentTypeHeaders, ""});
+            }
+        }
+
+        SECTION("module without revision")
+        {
+            SECTION("no revision in uri")
+            {
+                auto resp = get(YANG_ROOT "/example", {});
+                auto expectedShortenedResp = Response{200, yangHeaders, "module example {\n  yang-versio"};
+
+                REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
+                REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
+            }
+            SECTION("revision in uri")
+            {
+                REQUIRE(get(YANG_ROOT "/example@2020-02-02", {}) == Response{404, noContentTypeHeaders, ""});
+            }
+        }
+    }
+}

tests/aux-utils.h
@@ -84,6 +84,7 @@ static const auto SERVER_ADDRESS_AND_PORT = "http://["s + SERVER_ADDRESS + "]" +
 #define AUTH_NORULES {"authorization", "Basic bm9ydWxlczplbXB0eQ=="}
 #define AUTH_ROOT {"authorization", "Basic cm9vdDpzZWtyaXQ="}
 
+#define YANG_ROOT "/yang"
 #define RESTCONF_ROOT "/restconf"
 #define RESTCONF_DATA_ROOT RESTCONF_ROOT "/data"
 #define RESTCONF_OPER_ROOT RESTCONF_ROOT "/operations"
@@ -101,6 +102,11 @@ const ng::header_map xmlHeaders{
 
 const ng::header_map noContentTypeHeaders{
     {"access-control-allow-origin", {"*", false}},
+};
+
+const ng::header_map yangHeaders{
+    {"access-control-allow-origin", {"*", false}},
+    {"content-type", {"application/yang", false}},
 };
 
 Response clientRequest(auto method,

commit aacc5fbe638a515c6cd6616fb1c3f2f5e5744cc3

restconf: allow fetching submodules
In 2e19ce3c ("restconf: add endpoint for getting YANG schemas") we added
a support for fetching YANG modules via HTTP. However, this approach
does not work for submodules, because they could not be retrieved using
libyang's Context::getModule function. There was actually no function in
libyang-cpp, now we have it, so let's use it.

Fixes: 2e19ce3c9923792ac28c7549d121476b7b595f93
Depends-on: https://gerrit.cesnet.cz/c/CzechLight/libyang-cpp/+/6994
Change-Id: I4d9964ffb678b3a63d13a9a9d01fc42eaa015945

CMakeLists.txt
@@ -182,5 +182,8 @@ if(BUILD_TESTING)
     rousette_test(NAME restconf-writing LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-rpc LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-nacm LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
-    rousette_test(NAME restconf-yang-schema LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
+    set(nested-models
+        ${common-models}
+        --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/root-mod.yang)
+    rousette_test(NAME restconf-yang-schema LIBRARIES rousette-restconf FIXTURE nested-models WRAP_PAM)
 endif()

src/restconf/Server.cpp
@@ -512,7 +512,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
                 {"content-type", {"application/yang", false}},
                 {"access-control-allow-origin", {"*", false}},
             });
-        res.end(mod->printStr(libyang::SchemaOutputFormat::Yang));
+        res.end(std::visit([](auto&& arg) { return arg.printStr(libyang::SchemaOutputFormat::Yang); }, *mod));
     });
 
     server->handle(restconfRoot,

src/restconf/uri.cpp
@@ -160,6 +160,17 @@ std::optional<sysrepo::Datastore> datastoreFromApiIdentifier(const boost::option
 
     throw ErrorResponse(400, "application", "operation-failed", "Unsupported datastore " + *datastore->prefix + ":" + datastore->identifier);
 }
+
+std::optional<std::variant<libyang::Module, libyang::Submodule>> getModuleOrSubmodule(const libyang::Context& ctx, const std::string& name, const std::optional<std::string>& revision)
+{
+    if (auto mod = ctx.getModule(name, revision)) {
+        return *mod;
+    }
+    if (auto mod = ctx.getSubmodule(name, revision)) {
+        return *mod;
+    }
+    return std::nullopt;
+}
 }
 
 RestconfRequest::RestconfRequest(Type type, const boost::optional<ApiIdentifier>& datastore, const std::string& path)
@@ -429,14 +440,14 @@ std::pair<std::string, PathSegment> asLibyangPathSplit(const libyang::Context& c
     return {parentLyPath, lastSegment};
 }
 
-std::optional<libyang::Module> asYangModule(const libyang::Context& ctx, const std::string& uriPath)
+std::optional<std::variant<libyang::Module, libyang::Submodule>> asYangModule(const libyang::Context& ctx, const std::string& uriPath)
 {
     if (auto parsedModule = impl::parseModuleWithRevision(uriPath)) {
         // Converting between boost::optional and std::optional is not trivial
         if (parsedModule->revision) {
-            return ctx.getModule(parsedModule->name, *parsedModule->revision);
+            return getModuleOrSubmodule(ctx, parsedModule->name, *parsedModule->revision);
         } else {
-            return ctx.getModule(parsedModule->name, std::nullopt);
+            return getModuleOrSubmodule(ctx, parsedModule->name, std::nullopt);
         }
     }
     return std::nullopt;

src/restconf/uri.h
@@ -10,10 +10,12 @@
 #include <stdexcept>
 #include <string>
 #include <sysrepo-cpp/Enum.hpp>
+#include <variant>
 
 namespace libyang {
 class Context;
 class Module;
+class Submodule;
 }
 
 namespace rousette::restconf {
@@ -64,5 +66,5 @@ struct RestconfRequest {
 
 RestconfRequest asRestconfRequest(const libyang::Context& ctx, const std::string& httpMethod, const std::string& uriPath);
 std::pair<std::string, PathSegment> asLibyangPathSplit(const libyang::Context& ctx, const std::string& uriPath);
-std::optional<libyang::Module> asYangModule(const libyang::Context& ctx, const std::string& uriPath);
+std::optional<std::variant<libyang::Module, libyang::Submodule>> asYangModule(const libyang::Context& ctx, const std::string& uriPath);
 }

tests/restconf-yang-schema.cpp
@@ -137,17 +137,41 @@ TEST_CASE("obtaining YANG schemas")
 
             SECTION("module without revision")
             {
-                SECTION("no revision in uri")
-                {
-                    auto resp = get(YANG_ROOT "/example", {});
-                    auto expectedShortenedResp = Response{200, yangHeaders, "module example {\n  yang-versio"};
-
-                    REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
-                    REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
-                }
                 SECTION("revision in uri")
                 {
                     REQUIRE(get(YANG_ROOT "/example@2020-02-02", {}) == Response{404, noContentTypeHeaders, ""});
+                }
+                SECTION("no revision in uri")
+                {
+                    std::string moduleName;
+                    std::string expectedResponseStart;
+
+                    SECTION("loaded module")
+                    {
+                        moduleName = "example";
+                        expectedResponseStart = "module example {";
+                    }
+                    SECTION("loaded submodule")
+                    {
+                        moduleName = "root-submod";
+                        expectedResponseStart = "submodule root-submod {";
+                    }
+                    SECTION("imported module")
+                    {
+                        moduleName = "imp-mod";
+                        expectedResponseStart = "module imp-mod {";
+                    }
+                    SECTION("imported submodule")
+                    {
+                        moduleName = "imp-submod";
+                        expectedResponseStart = "submodule imp-submod {";
+                    }
+
+                    auto resp = get(YANG_ROOT "/" + moduleName, {});
+                    auto expectedShortenedResp = Response{200, yangHeaders, expectedResponseStart};
+
+                    REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
+                    REQUIRE(resp.data.substr(0, expectedResponseStart.size()) == expectedShortenedResp.data);
                 }
             }
         }

tests/uri-parser.cpp
@@ -737,20 +737,56 @@ TEST_CASE("URI path parser")
             auto ctx = libyang::Context{std::filesystem::path{CMAKE_CURRENT_SOURCE_DIR} / "tests" / "yang"};
             auto mod = ctx.loadModule("example", std::nullopt, {"f1"});
             ctx.loadModule("ietf-netconf-acm", "2018-02-14");
+            ctx.loadModule("root-mod", std::nullopt);
+
+            auto modName = [](auto&& mod) { return std::visit([](auto&& arg) { return arg.name(); }, mod); };
+            auto modRevision = [](auto&& mod) { return std::visit([](auto&& arg) { return arg.revision(); }, mod); };
 
             SECTION("Module without revision")
             {
-                SECTION("No revision in URI")
-                {
-                    auto mod = rousette::restconf::asYangModule(ctx, "/yang/example");
-                    REQUIRE(mod);
-                    REQUIRE(mod->name() == "example");
-                    REQUIRE(!mod->revision());
-                }
-
                 SECTION("Revision in URI")
                 {
                     REQUIRE(!rousette::restconf::asYangModule(ctx, "/yang/example@2020-02-02"));
+                }
+
+                SECTION("No revision in URI") {
+                    std::string uri;
+                    std::string expectedModuleName;
+
+                    SECTION("Module")
+                    {
+                        uri = "/yang/example";
+                        expectedModuleName = "example";
+                    }
+
+                    SECTION("Module with imports and submodules")
+                    {
+                        uri = "/yang/root-mod";
+                        expectedModuleName = "root-mod";
+                    }
+
+                    SECTION("Submodule")
+                    {
+                        uri = "/yang/root-submod";
+                        expectedModuleName = "root-submod";
+                    }
+
+                    SECTION("Imported module")
+                    {
+                        uri = "/yang/imp-mod";
+                        expectedModuleName = "imp-mod";
+                    }
+
+                    SECTION("Imported submodule")
+                    {
+                        uri = "/yang/imp-submod";
+                        expectedModuleName = "imp-submod";
+                    }
+
+                    auto mod = rousette::restconf::asYangModule(ctx, uri);
+                    REQUIRE(mod);
+                    REQUIRE(modName(*mod) == expectedModuleName);
+                    REQUIRE(!modRevision(*mod));
                 }
             }
 
@@ -760,8 +796,8 @@ TEST_CASE("URI path parser")
                 {
                     auto mod = rousette::restconf::asYangModule(ctx, "/yang/ietf-netconf-acm@2018-02-14");
                     REQUIRE(mod);
-                    REQUIRE(mod->name() == "ietf-netconf-acm");
-                    REQUIRE(mod->revision() == "2018-02-14");
+                    REQUIRE(modName(*mod) == "ietf-netconf-acm");
+                    REQUIRE(modRevision(*mod) == "2018-02-14");
                 }
 
                 SECTION("Incorrect revision in URI")

tests/yang/imp-mod.yang
@@ -1,0 +1,6 @@
+module imp-mod {
+  namespace "im";
+  prefix "im";
+  include imp-submod;
+  identity foo;
+}

tests/yang/imp-submod.yang
@@ -1,0 +1,8 @@
+submodule imp-submod {
+  belongs-to "imp-mod" {
+    prefix "im";
+  }
+  identity bar {
+    base foo;
+  }
+}

tests/yang/root-mod.yang
@@ -1,0 +1,21 @@
+module root-mod {
+  namespace "rm";
+  prefix "rm";
+  import imp-mod {
+    prefix "im";
+  }
+  include root-submod;
+  leaf r {
+    type string;
+  }
+  leaf i1 {
+    type identityref {
+      base im:foo;
+    }
+  }
+  leaf i2 {
+    type identityref {
+      base im:bar;
+    }
+  }
+}

tests/yang/root-submod.yang
@@ -1,0 +1,8 @@
+submodule root-submod {
+  belongs-to "root-mod" {
+    prefix "rm";
+  }
+  leaf r-s {
+    type string;
+  }
+}