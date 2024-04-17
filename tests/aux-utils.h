557bbabf08274c84222c8a5287d545e994e3fa1a
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-04-17 18:07:28 +0200

    restconf: publish capabilities via ietf-restconf-monitoring module
    This module from RFC 8040 [1] reports capabilities of the server. This
    means the implemented optional query parameters and the basic default
    value handling mode.
    
    The stuff is in another YANG module. In order to let anonymous users
    read the data, the NACM rules must be updated.
    
    [1] https://datatracker.ietf.org/doc/html/rfc8040#section-9.1
    
    Change-Id: I0a2f3a90f94f05ad0f930263acc6d381b1959b7b

--- tests/aux-utils.h
+++ tests/aux-utils.h
@@ -223,6 +223,9 @@ void setupRealNacm(sysrepo::Session session)
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='13']/module-name", "example");
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='13']/action", "permit");
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='13']/access-operations", "read");
+    session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='14']/module-name", "ietf-restconf-monitoring");
+    session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='14']/action", "permit");
+    session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='14']/access-operations", "read");
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='99']/module-name", "*");
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='anon rule']/rule[name='99']/action", "deny");
     session.setItem("/ietf-netconf-acm:nacm/rule-list[name='dwdm rule']/group[.='optics']", "");
