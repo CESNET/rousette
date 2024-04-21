e1ecf3f17e60fe83eb0b16b04a4055103d890a94
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-04-21 17:58:51 +0200

    tests: add function that subscribes to module running DS
    Sometimes we'd like to see running DS data in operational DS. Sysrepo
    fills the running DS data into operational only if the running DS is
    subscribed.
    
    I will need this in another test, so I think we can move the function
    into another file and use DRY.
    
    Change-Id: I2dfa8275e4e0f67cfadd8f33be332d22aa1212a2

--- tests/datastoreUtils.h
+++ tests/datastoreUtils.h
@@ -25,3 +25,9 @@ struct DatastoreChangesMock {
 
 sysrepo::Subscription datastoreNewStateSubscription(sysrepo::Session& session, DatastoreChangesMock& dsChangesMock, const std::string& moduleName);
 sysrepo::Subscription datastoreChangesSubscription(sysrepo::Session& session, DatastoreChangesMock& dsChangesMock, const std::string& moduleName);
+
+static DatastoreChangesMock testMockForUntrackedModuleWrites;
+
+#define SUBSCRIBE_MODULE(SUBNAME, SESSION, MODULE) \
+    ALLOW_CALL(testMockForUntrackedModuleWrites, change(trompeloeil::_)); \
+    auto SUBNAME = datastoreChangesSubscription(SESSION, testMockForUntrackedModuleWrites, MODULE);
