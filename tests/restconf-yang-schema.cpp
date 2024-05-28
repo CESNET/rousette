CESNET/rousette

commit 9ff26d45b9c6535017c673b9aff0a57a64142c8c

restconf: show schema locations for modules in ietf-yang-library
The ietf-yang-library module [1] contains a list of all modules the
server knows about.
It can also point to a file from which the schema was loaded from.
Sysrepo takes care of filling the lists and locations to the files.

The RESTCONF server should, however, not point to internal filepaths
but rather point to a HTTP resource where users can download the file.
This commit implements it by replacing the location values in the YANG
tree.

We, however, have to provide full URLs. Our server runs behind a reverse
proxy. We try to parse the forwarded HTTP header and guess the protocol
and host from its value. In case we can't parse both values then the
locations leafs are not provided.

[1] https://datatracker.ietf.org/doc/html/rfc8525

Change-Id: I7c3e4d8c92c01da0f472bcfa2573dffb3cbc85cd

src/restconf/YangSchemaLocations.cpp
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ */
+
+#include <libyang-cpp/Collection.hpp>
+#include <libyang-cpp/Set.hpp>
+#include "YangSchemaLocations.h"
+
+namespace {
+const auto moduleNodesXPath = "/ietf-yang-library:yang-library/module-set/module |"
+                              "/ietf-yang-library:yang-library/module-set/module/submodule |"
+                              "/ietf-yang-library:yang-library/module-set/import-only-module |"
+                              "/ietf-yang-library:yang-library/module-set/import-only-module/submodule |"
+                              "/ietf-yang-library:modules-state/module |"
+                              "/ietf-yang-library:modules-state/module/submodule";
+}
+
+namespace rousette::restconf {
+libyang::DataNode replaceYangLibraryLocations(const std::optional<std::string>& schemeAndHost, const std::string& urlPrefix, libyang::DataNode& node)
+{
+    std::vector<libyang::DataNode> moduleNodes;
+    for (const auto& n : node.findXPath(moduleNodesXPath)) {
+        moduleNodes.emplace_back(n);
+    }
+
+    /* unlink location leaf-list nodes.
+     * In the yang-library container there is a leaf-list location.
+     * In the modules-state container there is a leaf called schema
+     * The names of these leaves are not used in another context in the other container so we can just remove all without checking what node are we in
+     */
+    for (const auto& n : moduleNodes) {
+        // remove all possible location nodes; unlink invalidates the collection so first copy the nodes into a vector
+        std::vector<libyang::DataNode> locationNodes;
+
+        for (const auto& child : n.findXPath("location | schema")) {
+            locationNodes.emplace_back(child);
+        }
+
+        for (auto& child : locationNodes) {
+            child.unlink();
+        }
+    }
+
+    // if we were unable to parse scheme and hosts, end without providing URLs of the YANG modules
+    if (!schemeAndHost) {
+        return node;
+    }
+
+    for (const auto& n : moduleNodes) {
+        const std::string moduleName = std::string{n.findPath("name")->asTerm().valueStr()};
+
+        std::optional<std::string> revision; // in some lists the revision leaf is optional, in some lists it is mandatory but can be empty string
+        if (auto revisionNode = n.findPath("revision")) {
+            if (auto val = revisionNode->asTerm().valueStr(); !val.empty()) {
+                revision = val;
+            }
+        }
+
+        std::string locationLeafName;
+
+        if (n.parent()->schema().name() == "module-set") {
+            locationLeafName = "location";
+        } else if (n.parent()->schema().name() == "modules-state") {
+            locationLeafName = "schema";
+        }
+
+        const std::string path = moduleName + (revision ? ("@" + *revision) : "");
+        n.newPath(locationLeafName, *schemeAndHost + urlPrefix + path);
+    }
+
+    return node;
+}
+}

tests/restconf-yang-schema.cpp
@@ -12,6 +12,8 @@ static const auto SERVER_PORT = "10085";
 #include "restconf/Server.h"
 #include "tests/aux-utils.h"
 
+#define FORWARDED {"forward", "proto=http;host=example.net"}
+
 TEST_CASE("obtaining YANG schemas")
 {
     spdlog::set_level(spdlog::level::trace);
@@ -20,50 +22,131 @@ TEST_CASE("obtaining YANG schemas")
     auto nacmGuard = manageNacm(srSess);
     auto server = rousette::restconf::Server{srConn, SERVER_ADDRESS, SERVER_PORT};
 
-    SECTION("unsupported methods")
+    SECTION("Locations are overwritten")
     {
-        for (const std::string httpMethod : {"POST", "PUT", "OPTIONS", "PATCH", "DELETE"}) {
-            CAPTURE(httpMethod);
-            REQUIRE(clientRequest(httpMethod, YANG_ROOT "/ietf-yang-library@2019-01-04", "", {}) == Response{405, noContentTypeHeaders, ""});
-        }
+        REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:yang-library/module-set=complete/module=ietf-yang-library", {AUTH_ROOT, FORWARDED}) == Response{200, jsonHeaders, R"({
+  "ietf-yang-library:yang-library": {
+    "module-set": [
+      {
+        "name": "complete",
+        "module": [
+          {
+            "name": "ietf-yang-library",
+            "revision": "2019-01-04",
+            "namespace": "urn:ietf:params:xml:ns:yang:ietf-yang-library",
+            "location": [
+              "http://example.net/yang/ietf-yang-library@2019-01-04"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+)"});
+
+        REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:yang-library/module-set=complete/import-only-module=ietf-inet-types,2013-07-15", {AUTH_ROOT, FORWARDED}) == Response{200, jsonHeaders, R"({
+  "ietf-yang-library:yang-library": {
+    "module-set": [
+      {
+        "name": "complete",
+        "import-only-module": [
+          {
+            "name": "ietf-inet-types",
+            "revision": "2013-07-15",
+            "namespace": "urn:ietf:params:xml:ns:yang:ietf-inet-types",
+            "location": [
+              "http://example.net/yang/ietf-inet-types@2013-07-15"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+)"});
+
+        REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:modules-state/module=ietf-yang-library,2019-01-04", {AUTH_ROOT, FORWARDED}) == Response{200, jsonHeaders, R"({
+  "ietf-yang-library:modules-state": {
+    "module": [
+      {
+        "name": "ietf-yang-library",
+        "revision": "2019-01-04",
+        "schema": "http://example.net/yang/ietf-yang-library@2019-01-04",
+        "namespace": "urn:ietf:params:xml:ns:yang:ietf-yang-library",
+        "conformance-type": "implement"
+      }
+    ]
+  }
+}
+)"});
+
+        REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:modules-state/module=example,", {AUTH_ROOT, FORWARDED}) == Response{200, jsonHeaders, R"({
+  "ietf-yang-library:modules-state": {
+    "module": [
+      {
+        "name": "example",
+        "revision": "",
+        "schema": "http://example.net/yang/example",
+        "namespace": "http://example.tld/example",
+        "feature": [
+          "f1"
+        ],
+        "conformance-type": "implement"
+      }
+    ]
+  }
+}
+)"});
     }
 
-    SECTION("loaded modules")
+    SECTION("get YANG schema")
     {
-        SECTION("module with revision")
+        SECTION("unsupported methods")
         {
-            SECTION("no revision in uri")
-            {
-                REQUIRE(get(YANG_ROOT "/ietf-system", {}) == Response{404, noContentTypeHeaders, ""});
-            }
-            SECTION("correct revision in uri")
-            {
-                auto resp = get(YANG_ROOT "/ietf-system@2014-08-06", {});
-                auto expectedShortenedResp = Response{200, yangHeaders, "module ietf-system {\n  namespa"};
-
-                REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
-                REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
-            }
-            SECTION("wrong revision in uri")
-            {
-                REQUIRE(get(YANG_ROOT "/ietf-system@1999-12-13", {}) == Response{404, noContentTypeHeaders, ""});
-                REQUIRE(get(YANG_ROOT "/ietf-system@abcd-ef-gh", {}) == Response{404, noContentTypeHeaders, ""});
+            for (const std::string httpMethod : {"POST", "PUT", "OPTIONS", "PATCH", "DELETE"}) {
+                CAPTURE(httpMethod);
+                REQUIRE(clientRequest(httpMethod, YANG_ROOT "/ietf-yang-library@2019-01-04", "", {}) == Response{405, noContentTypeHeaders, ""});
             }
         }
 
-        SECTION("module without revision")
+        SECTION("loaded modules")
         {
-            SECTION("no revision in uri")
+            SECTION("module with revision")
             {
-                auto resp = get(YANG_ROOT "/example", {});
-                auto expectedShortenedResp = Response{200, yangHeaders, "module example {\n  yang-versio"};
+                SECTION("no revision in uri")
+                {
+                    REQUIRE(get(YANG_ROOT "/ietf-system", {}) == Response{404, noContentTypeHeaders, ""});
+                }
+                SECTION("correct revision in uri")
+                {
+                    auto resp = get(YANG_ROOT "/ietf-system@2014-08-06", {});
+                    auto expectedShortenedResp = Response{200, yangHeaders, "module ietf-system {\n  namespa"};
 
-                REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
-                REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
+                    REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
+                    REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
+                }
+                SECTION("wrong revision in uri")
+                {
+                    REQUIRE(get(YANG_ROOT "/ietf-system@1999-12-13", {}) == Response{404, noContentTypeHeaders, ""});
+                    REQUIRE(get(YANG_ROOT "/ietf-system@abcd-ef-gh", {}) == Response{404, noContentTypeHeaders, ""});
+                }
             }
-            SECTION("revision in uri")
+
+            SECTION("module without revision")
             {
-                REQUIRE(get(YANG_ROOT "/example@2020-02-02", {}) == Response{404, noContentTypeHeaders, ""});
+                SECTION("no revision in uri")
+                {
+                    auto resp = get(YANG_ROOT "/example", {});
+                    auto expectedShortenedResp = Response{200, yangHeaders, "module example {\n  yang-versio"};
+
+                    REQUIRE(resp.equalStatusCodeAndHeaders(expectedShortenedResp));
+                    REQUIRE(resp.data.substr(0, 30) == expectedShortenedResp.data);
+                }
+                SECTION("revision in uri")
+                {
+                    REQUIRE(get(YANG_ROOT "/example@2020-02-02", {}) == Response{404, noContentTypeHeaders, ""});
+                }
             }
         }
     }

CMakeLists.txt
@@ -98,6 +98,7 @@ target_link_libraries(rousette-auth PUBLIC spdlog::spdlog PkgConfig::SYSREPO-CPP
 
 add_library(rousette-restconf STATIC
     src/restconf/Server.cpp
+    src/restconf/YangSchemaLocations.cpp
     src/restconf/uri.cpp
     src/restconf/utils.cpp
 )

src/restconf/Server.cpp
@@ -17,6 +17,7 @@
 #include "restconf/Nacm.h"
 #include "restconf/PAM.h"
 #include "restconf/Server.h"
+#include "restconf/YangSchemaLocations.h"
 #include "restconf/uri.h"
 #include "restconf/utils.h"
 #include "sr/OpticalEvents.h"
@@ -125,6 +126,19 @@ std::optional<std::string> getHeaderValue(const nghttp2::asio_http2::header_map&
     }
 
     return it->second.value;
+}
+
+std::optional<std::string> parseUrlPrefix(const nghttp2::asio_http2::header_map& headers) {
+    if (auto forward = getHeaderValue(headers, "forward")) {
+        auto [proto, host] = http::parseForwardedHeader(*forward);
+        if (!proto || !host) {
+            return std::nullopt;
+        }
+
+        return *proto + "://" + *host;
+    }
+
+    return std::nullopt;
 }
 
 struct DataFormat {
@@ -557,7 +571,8 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
                                 {"content-type", {asMimeType(dataFormat.response), false}},
                                 {"access-control-allow-origin", {"*", false}},
                             });
-                        res.end(*data->printStr(dataFormat.response, libyang::PrintFlags::WithSiblings));
+
+                        res.end(*replaceYangLibraryLocations(parseUrlPrefix(req.header()), yangSchemaRoot, *data).printStr(dataFormat.response, libyang::PrintFlags::WithSiblings));
                     } else {
                         throw ErrorResponse(404, "application", "invalid-value", "No data from sysrepo.");
                     }

src/restconf/YangSchemaLocations.h
@@ -1,0 +1,12 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ */
+
+#include <libyang-cpp/DataNode.hpp>
+
+namespace rousette::restconf {
+
+libyang::DataNode replaceYangLibraryLocations(const std::optional<std::string>& schemeAndHost, const std::string& urlPrefix, libyang::DataNode& node);
+}

commit f0e256f076ab7385beca2a855171e0b4eee9d968

restconf: output location leaf only if sysrepo reports it
There was a bug in the code adding the location leafs. We added them to
every module even if sysrepo did not return the location. This could
happen in situations where NACM rules set the location as not
readable.

This patch fixes the behaviour by adding the location leaf for a module
only if the location was reported by sysrepo.

Fixes: 9ff26d45b9c6535017c673b9aff0a57a64142c8c
Change-Id: I55a037dd7bfd3965d4cf51c4484fe8a24835ebdf

src/restconf/YangSchemaLocations.cpp
@@ -21,6 +21,7 @@ namespace rousette::restconf {
 libyang::DataNode replaceYangLibraryLocations(const std::optional<std::string>& schemeAndHost, const std::string& urlPrefix, libyang::DataNode& node)
 {
     std::vector<libyang::DataNode> moduleNodes;
+    std::vector<libyang::DataNode> moduleNodesWithLocation;
     for (const auto& n : node.findXPath(moduleNodesXPath)) {
         moduleNodes.emplace_back(n);
     }
@@ -41,6 +42,10 @@ libyang::DataNode replaceYangLibraryLocations(const std::optional<std::string>& 
         for (auto& child : locationNodes) {
             child.unlink();
         }
+
+        if (!locationNodes.empty()) {
+            moduleNodesWithLocation.emplace_back(n);
+        }
     }
 
     // if we were unable to parse scheme and hosts, end without providing URLs of the YANG modules
@@ -48,7 +53,7 @@ libyang::DataNode replaceYangLibraryLocations(const std::optional<std::string>& 
         return node;
     }
 
-    for (const auto& n : moduleNodes) {
+    for (const auto& n : moduleNodesWithLocation) {
         const std::string moduleName = std::string{n.findPath("name")->asTerm().valueStr()};
 
         std::optional<std::string> revision; // in some lists the revision leaf is optional, in some lists it is mandatory but can be empty string

tests/restconf-yang-schema.cpp
@@ -267,6 +267,38 @@ TEST_CASE("obtaining YANG schemas")
 )"});
     }
 
+    SECTION("Location leaf is not added if sysrepo does not report it")
+    {
+        srSess.switchDatastore(sysrepo::Datastore::Running);
+        srSess.setItem("/ietf-netconf-acm:nacm/enable-external-groups", "false");
+        srSess.setItem("/ietf-netconf-acm:nacm/groups/group[name='dwdm']/user-name[.='dwdm']", "");
+
+        srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='rule']/group[.='dwdm']", "");
+        srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='rule']/rule[name='10']/module-name", "ietf-yang-library");
+        srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='rule']/rule[name='10']/action", "deny");
+        srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='rule']/rule[name='10']/access-operations", "*");
+        srSess.setItem("/ietf-netconf-acm:nacm/rule-list[name='rule']/rule[name='10']/path", "/ietf-yang-library:yang-library/module-set[name='complete']/module[name='ietf-yang-library']/location");
+        srSess.applyChanges();
+
+        REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:yang-library/module-set=complete/module=ietf-yang-library", {AUTH_DWDM, FORWARDED}) == Response{200, jsonHeaders, R"({
+  "ietf-yang-library:yang-library": {
+    "module-set": [
+      {
+        "name": "complete",
+        "module": [
+          {
+            "name": "ietf-yang-library",
+            "revision": "2019-01-04",
+            "namespace": "urn:ietf:params:xml:ns:yang:ietf-yang-library"
+          }
+        ]
+      }
+    ]
+  }
+}
+)"});
+    }
+
     SECTION("Submodules are reported")
     {
         REQUIRE(get(RESTCONF_DATA_ROOT "/ietf-yang-library:yang-library/module-set=complete/module=root-mod", {AUTH_DWDM, FORWARDED}) == Response{200, jsonHeaders, R"({