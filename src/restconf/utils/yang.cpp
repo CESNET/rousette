52443a2427af48195e29e0fba6b90f96e84a7032
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-06-12 11:28:16 +0200

    restconf: serialize time point using libyang time utils
    The original code used std::gmtime which is not thread-safe[1].
    libyang-cpp can now serialize time points into yang:date-and-time
    valid strings, so let's use that.
    
    [1] https://en.cppreference.com/w/cpp/chrono/c/gmtime
    
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/7234
    Change-Id: Ie1c2233522a97750381bf62f786dd65979b4e1f9

--- src/restconf/utils/yang.cpp
+++ src/restconf/utils/yang.cpp
@@ -6,48 +6,10 @@
 */
 
 #include <algorithm>
-#include <chrono>
-#include <iomanip>
 #include <libyang-cpp/DataNode.hpp>
 #include <libyang-cpp/SchemaNode.hpp>
 
 namespace rousette::restconf {
-
-namespace {
-// cannot static_asssert(false) directly in constexpr else
-template<bool flag = false> void error_wrong_precision() { static_assert(flag, "Wrong precision for fraction-digit time"); }
-}
-
-/** @short Format YANG's date-and-time from RFC6991 with specified precision */
-template <typename Clock, typename Precision>
-std::string yangDateTime(const std::chrono::time_point<Clock>& timePoint)
-{
-    auto tt = Clock::to_time_t(timePoint);
-    std::ostringstream ss;
-    ss << std::put_time(std::gmtime(&tt), "%Y-%m-%dT%H:%M:%S");
-
-    if constexpr (std::is_same_v<Precision, std::chrono::seconds>) {
-        // do nothing
-    } else {
-        static_assert(Precision::period::num == 1, "Unrecognized fraction format");
-        auto frac = std::chrono::time_point_cast<Precision>(timePoint).time_since_epoch().count() % Precision::period::den;
-        ss << '.';
-        ss.fill('0');
-        if constexpr (std::is_same_v<Precision, std::chrono::nanoseconds>) {
-            ss.width(9);
-        } else if constexpr (std::is_same_v<Precision, std::chrono::microseconds>) {
-            ss.width(6);
-        } else if constexpr (std::is_same_v<Precision, std::chrono::milliseconds>) {
-            ss.width(3);
-        } else {
-            error_wrong_precision();
-        }
-        ss << frac << "-00:00";
-    }
-    return ss.str();
-}
-
-template std::string yangDateTime<std::chrono::system_clock, std::chrono::nanoseconds>(const std::chrono::time_point<std::chrono::system_clock>&);
 
 /** @brief Escapes key with the other type of quotes than found in the string.
  *
