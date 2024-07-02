a99597d6ac860f6bec95697ef9b76a06d629937d
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-07-02 14:39:57 +0200

    restconf: publish stream list
    There is a container in the ietf-restconf-monitoring module that is used
    to publish stream resources locations for users [1].
    
    This patch populates the contents of the container when the RESTCONF
    server starts.
    
    However, there is the same problem as in 9ff26d4 ("restconf: show
    schema locations for modules in ietf-yang-library"). We have to provide
    full URLs with server name, i.e., we have to parse it from the Host HTTP
    header and then edit the YANG data tree.
    
    [1] https://datatracker.ietf.org/doc/html/rfc8040#section-6.2
    
    Change-Id: Ibd2250c9b7033e47356c1b2c08dff3dce78e2061

--- src/restconf/NotificationStream.cpp
+++ src/restconf/NotificationStream.cpp
@@ -14,7 +14,13 @@
 #include "restconf/NotificationStream.h"
 #include "utils/yang.h"
 
+using namespace std::string_literals;
+
 namespace {
+
+const auto streamListXPath = "/ietf-restconf-monitoring:restconf-state/streams/stream"s;
+const auto dummyPrefix = "http://localhost"s;
+
 /** @brief Wraps a notification data tree with RESTCONF notification envelope. */
 std::string as_restconf_notification(const libyang::Context& ctx, libyang::DataFormat dataFormat, libyang::DataNode notification, const sysrepo::NotificationTimeStamp& time)
 {
@@ -106,5 +112,78 @@ NotificationStream::NotificationStream(const nghttp2::asio_http2::server::reques
 void NotificationStream::activate()
 {
     EventStream::activate(m_notificationSignal);
+}
+
+/** @brief Creates and fills ietf-restconf-monitoring:restconf-state/stream. To be called in oper callback. */
+void notificationStreamList(sysrepo::Session& session, std::optional<libyang::DataNode>& parent, const std::string& streamsPrefix)
+{
+    static const auto prefix = "/ietf-restconf-monitoring:restconf-state/streams/stream[name='NETCONF']"s;
+
+    decltype(sysrepo::ModuleReplaySupport::earliestNotification) globalEarliestNotification;
+    bool replayEnabled = false;
+
+    for (const auto& mod : session.getContext().modules()) {
+        if (mod.name() == "sysrepo") {
+            // internal module; get module replay support returns NotFound
+            continue;
+        }
+
+        if (mod.implemented()) {
+            auto replay = session.getConnection().getModuleReplaySupport(mod.name());
+            replayEnabled |= replay.enabled;
+
+            if (replay.earliestNotification) {
+                if (!globalEarliestNotification) {
+                    globalEarliestNotification = replay.earliestNotification;
+                } else {
+                    globalEarliestNotification = std::min(*replay.earliestNotification, *globalEarliestNotification);
+                }
+            }
+        }
+    }
+
+    if (!parent) {
+        parent = session.getContext().newPath(prefix + "/description", "Default NETCONF notification stream");
+    } else {
+        parent->newPath(prefix + "/description", "Default NETCONF notification stream");
+    }
+    parent->newPath(prefix + "/access[encoding='xml']/location", dummyPrefix + streamsPrefix + "NETCONF/XML");
+    parent->newPath(prefix + "/access[encoding='json']/location", dummyPrefix + streamsPrefix + "NETCONF/JSON");
+
+    if (replayEnabled) {
+        parent->newPath(prefix + "/replay-support", "true");
+
+        if (globalEarliestNotification) {
+            parent->newPath(prefix + "/replay-log-creation-time", libyang::yangTimeFormat(*globalEarliestNotification, libyang::TimezoneInterpretation::Local));
+        }
+    }
+}
+
+libyang::DataNode replaceStreamLocations(const std::optional<std::string>& schemeAndHost, libyang::DataNode& node)
+{
+    // if we were unable to parse scheme and hosts, end without providing any streams
+    // the YANG schema specifies that there must be at least one endpoint to each stream but we are unable to provide any
+    if (!schemeAndHost) {
+        std::vector<libyang::DataNode> nodesToDelete;
+        auto nodes = node.findXPath(streamListXPath);
+
+        for (const auto& n : nodes) {
+            nodesToDelete.emplace_back(n);
+        }
+        for (auto n : nodesToDelete) {
+            n.unlink();
+        }
+        return node;
+    }
+
+    auto accessNodes = node.findXPath(streamListXPath + "/access");
+
+    for (const auto& n : accessNodes) {
+        std::string val = n.findPath("location")->asTerm().valueStr();
+        val = val.substr(dummyPrefix.size());
+        n.newPath("location", *schemeAndHost + val, libyang::CreationOptions::Update);
+    }
+
+    return node;
 }
 }
