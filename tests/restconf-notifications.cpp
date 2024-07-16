CESNET/rousette

commit df5d6a0a7b4d414cd7513ac59d1fa03fcf241229

restconf: NETCONF notification streams
RESTCONF supports YANG-defined event notifications. This commit
implements a stream of notifications using Server-Sent Events.

After requesting the stream, clients are receiving all NETCONF
notifications they can read using current NACM settings. The
authorization works the same as for standard RESTCONF requests in
rousette, i.e., via the authorization header.

For the testing purposes the wait-a-bit-longer.cpp file from velia
project (commit 9bd612744ff4139c87ec830be5709e15a4f3996c) was
imported. For some reason (copy-paste I guess), the declaration of
the function waitForCompletionAndBitMore was already present in
tests/trompeloeil_doctest.h but the definition was missing.

[1] https://datatracker.ietf.org/doc/html/rfc8040

Depends-on: https://gerrit.cesnet.cz/c/CzechLight/dependencies/+/7234
Change-Id: I693ba257152f79f6e9acb2e0567a7acdf01e2e2e

src/restconf/Server.cpp
@@ -14,6 +14,7 @@
 #include "http/utils.hpp"
 #include "restconf/Exceptions.h"
 #include "restconf/Nacm.h"
+#include "restconf/NotificationStream.h"
 #include "restconf/PAM.h"
 #include "restconf/Server.h"
 #include "restconf/YangSchemaLocations.h"
@@ -43,6 +44,7 @@ auto as_restconf_push_update(const std::string& content, const T& time)
 
 constexpr auto restconfRoot = "/restconf/";
 constexpr auto yangSchemaRoot = "/yang/";
+constexpr auto netconfStreamRoot = "/streams/";
 
 bool isSameNode(const libyang::DataNode& child, const PathSegment& lastPathSegment)
 {
@@ -540,6 +542,47 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     server->handle("/telemetry/optics", [this](const auto& req, const auto& res) {
         auto client = std::make_shared<http::EventStream>(req, res, opticsChange, as_restconf_push_update(dwdmEvents->currentData(), std::chrono::system_clock::now()));
         client->activate();
+    });
+
+    server->handle(netconfStreamRoot, [this, conn](const auto& req, const auto& res) mutable {
+        auto sess = conn.sessionStart();
+        libyang::DataFormat dataFormat;
+
+        try {
+            authorizeRequest(nacm, sess, req);
+
+            auto streamRequest = asRestconfStreamRequest(req.uri().path);
+
+            switch(streamRequest.type) {
+            case RestconfStreamRequest::Type::NetconfNotificationJSON:
+                dataFormat = libyang::DataFormat::JSON;
+                break;
+            case RestconfStreamRequest::Type::NetconfNotificationXML:
+                dataFormat = libyang::DataFormat::XML;
+                break;
+            }
+
+            // The signal is constructed outside NotificationStream class because it is required to be passed to
+            // NotificationStream's parent (EventStream) constructor where it already must be constructed
+            // Yes, this is a hack.
+            auto client = std::make_shared<NotificationStream>(req, res, std::make_shared<rousette::http::EventStream::Signal>(), sess, dataFormat);
+            client->activate();
+        } catch (const auth::Error& e) {
+            processAuthError(req, res, e, [&res]() {
+                res.write_head(401, {
+                                        {"content-type", {"text/plain", false}},
+                                        {"access-control-allow-origin", {"*", false}},
+                                    });
+                res.end("Access denied.");
+            });
+        } catch (const ErrorResponse& e) {
+            // RFC does not specify how the errors should look like so let's just report the HTTP code and print the error message
+            res.write_head(e.code, {
+                                       {"content-type", {"text/plain", false}},
+                                       {"access-control-allow-origin", {"*", false}},
+                                   });
+            res.end(e.errorMessage);
+        }
     });
 
     server->handle(yangSchemaRoot, [this, conn /* intentional copy */](const auto& req, const auto& res) mutable {

src/restconf/uri.cpp
@@ -559,4 +559,20 @@ std::optional<std::variant<libyang::Module, libyang::SubmoduleParsed>> asYangMod
     }
     return std::nullopt;
 }
+
+RestconfStreamRequest asRestconfStreamRequest(const std::string& uriPath)
+{
+    static const auto netconfStreamRoot = "/streams/NETCONF/";
+    RestconfStreamRequest::Type type;
+
+    if (uriPath == netconfStreamRoot + "XML"s) {
+        type = RestconfStreamRequest::Type::NetconfNotificationXML;
+    } else if (uriPath == netconfStreamRoot + "JSON"s) {
+        type = RestconfStreamRequest::Type::NetconfNotificationJSON;
+    } else {
+        throw ErrorResponse(404, "application", "invalid-value", "Invalid stream");
+    }
+
+    return {type};
+}
 }

src/restconf/uri.h
@@ -136,8 +136,17 @@ struct RestconfRequest {
     RestconfRequest(Type type, const boost::optional<ApiIdentifier>& datastore, const std::string& path, const queryParams::QueryParams& queryParams);
 };
 
+struct RestconfStreamRequest {
+    enum class Type {
+        NetconfNotificationJSON,
+        NetconfNotificationXML,
+    };
+    Type type;
+};
+
 RestconfRequest asRestconfRequest(const libyang::Context& ctx, const std::string& httpMethod, const std::string& uriPath, const std::string& uriQueryString = "");
 std::optional<libyang::SchemaNode> asLibyangSchemaNode(const libyang::Context& ctx, const std::vector<PathSegment>& pathSegments);
 std::pair<std::string, PathSegment> asLibyangPathSplit(const libyang::Context& ctx, const std::string& uriPath);
 std::optional<std::variant<libyang::Module, libyang::SubmoduleParsed>> asYangModule(const libyang::Context& ctx, const std::string& uriPath);
+RestconfStreamRequest asRestconfStreamRequest(const std::string& uriPath);
 }

tests/restconf-notifications.cpp
@@ -1,0 +1,224 @@
+/*
+ * Copyright (C) 2024 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ *
+ */
+
+#include "trompeloeil_doctest.h"
+static const auto SERVER_PORT = "10088";
+#include <latch>
+#include <nghttp2/asio_http2.h>
+#include <spdlog/spdlog.h>
+#include "restconf/Server.h"
+#include "tests/aux-utils.h"
+#include "tests/pretty_printers.h"
+
+#define EXPECT_NOTIFICATION(DATA) expectations.emplace_back(NAMED_REQUIRE_CALL(netconfWatcher, data(DATA)).IN_SEQUENCE(seq1));
+#define SEND_NOTIFICATION(DATA) notifSession.sendNotification(*ctx.parseOp(DATA, libyang::DataFormat::JSON, libyang::OperationType::NotificationYang).op, sysrepo::Wait::No);
+
+using namespace std::chrono_literals;
+
+struct NotificationWatcher {
+    libyang::Context ctx;
+    libyang::DataFormat dataFormat;
+
+    NotificationWatcher(const libyang::Context& ctx, libyang::DataFormat dataFormat)
+        : ctx(ctx)
+        , dataFormat(dataFormat)
+    {
+    }
+
+    void operator()(const std::string& msg) const
+    {
+        spdlog::trace("Client received data: {}", msg);
+        auto notifDataNode = ctx.parseOp(msg,
+                                         dataFormat,
+                                         dataFormat == libyang::DataFormat::JSON ? libyang::OperationType::NotificationRestconf : libyang::OperationType::NotificationNetconf);
+
+        // parsing nested notifications does not return the data tree root node but the notification data node
+        auto dataRoot = notifDataNode.op;
+        while (dataRoot->parent()) {
+            dataRoot = *dataRoot->parent();
+        }
+
+        data(*dataRoot->printStr(libyang::DataFormat::JSON, libyang::PrintFlags::Shrink));
+    }
+
+    MAKE_CONST_MOCK1(data, void(const std::string&));
+};
+
+struct SSEClient {
+    std::shared_ptr<ng_client::session> client;
+    boost::asio::deadline_timer t;
+
+    SSEClient(
+        boost::asio::io_service& io,
+        std::latch& requestSent,
+        const NotificationWatcher& notification,
+        const std::string& uri,
+        const std::map<std::string, std::string>& headers,
+        const boost::posix_time::seconds silenceTimeout = boost::posix_time::seconds(1)) // test code; the server should respond "soon"
+        : client(std::make_shared<ng_client::session>(io, SERVER_ADDRESS, SERVER_PORT))
+        , t(io, silenceTimeout)
+    {
+        ng::header_map reqHeaders;
+        for (const auto& [name, value] : headers) {
+            reqHeaders.insert({name, {value, false}});
+        }
+
+        // shutdown the client after a period of no traffic
+        t.async_wait([maybeClient = std::weak_ptr<ng_client::session>{client}](const boost::system::error_code& ec) {
+            if (ec == boost::asio::error::operation_aborted) {
+                return;
+            }
+            if (auto client = maybeClient.lock()) {
+                client->shutdown();
+            }
+        });
+
+        client->on_connect([&, uri, reqHeaders, silenceTimeout](auto) {
+            boost::system::error_code ec;
+
+            auto req = client->submit(ec, "GET", SERVER_ADDRESS_AND_PORT + uri, "", reqHeaders);
+            req->on_response([&, silenceTimeout](const ng_client::response& res) {
+                requestSent.count_down();
+                res.on_data([&, silenceTimeout](const uint8_t* data, std::size_t len) {
+                    // not a production-ready code. In real-life condition the data received in one callback might probably be incomplete
+                    for (const auto& event : parseEvents(std::string(reinterpret_cast<const char*>(data), len))) {
+                        notification(event);
+                    }
+                    t.expires_from_now(silenceTimeout);
+                });
+            });
+        });
+
+        client->on_error([&](const boost::system::error_code& ec) {
+            throw std::runtime_error{"HTTP client error: " + ec.message()};
+        });
+    }
+
+    static std::vector<std::string> parseEvents(const std::string& msg)
+    {
+        static const std::string prefix = "data:";
+
+        std::vector<std::string> res;
+        std::istringstream iss(msg);
+        std::string line;
+        std::string event;
+
+        while (std::getline(iss, line)) {
+            if (line.compare(0, prefix.size(), prefix) == 0) {
+                event += line.substr(prefix.size());
+            } else if (line.empty()) {
+                res.emplace_back(std::move(event));
+                event.clear();
+            } else {
+                FAIL("Unprefixed response");
+            }
+        }
+        return res;
+    }
+};
+
+TEST_CASE("NETCONF notification streams")
+{
+    trompeloeil::sequence seq1;
+    spdlog::set_level(spdlog::level::trace);
+
+    std::vector<std::unique_ptr<trompeloeil::expectation>> expectations;
+
+    auto srConn = sysrepo::Connection{};
+    auto srSess = srConn.sessionStart(sysrepo::Datastore::Running);
+    srSess.sendRPC(srSess.getContext().newPath("/ietf-factory-default:factory-reset"));
+
+    auto nacmGuard = manageNacm(srSess);
+    auto server = rousette::restconf::Server{srConn, SERVER_ADDRESS, SERVER_PORT};
+    setupRealNacm(srSess);
+
+    SECTION("NETCONF streams")
+    {
+        // parent for nested notification
+        srSess.switchDatastore(sysrepo::Datastore::Operational);
+        srSess.setItem("/example:tlc/list[name='k1']/choice1", "something must me here");
+        srSess.applyChanges();
+
+        std::vector<std::string> notificationsJSON{
+            R"({"example:eventA":{"message":"blabla","progress":11}})",
+            R"({"example:eventB":{}})",
+            R"({"example-notif:something-happened":{}})",
+            R"({"example:eventA":{"message":"almost finished","progress":99}})",
+            R"({"example:tlc":{"list":[{"name":"k1","notif":{"message":"nested"}}]}})",
+        };
+        std::vector<std::string> expectedNotificationsJSON;
+
+        std::string uri;
+        libyang::DataFormat dataFormat;
+        std::map<std::string, std::string> headers;
+
+        SECTION("XML stream")
+        {
+            uri = "/streams/NETCONF/XML";
+            dataFormat = libyang::DataFormat::XML;
+            headers = {AUTH_ROOT};
+            expectedNotificationsJSON = notificationsJSON;
+        }
+
+        SECTION("JSON stream")
+        {
+            uri = "/streams/NETCONF/JSON";
+            dataFormat = libyang::DataFormat::JSON;
+
+            SECTION("anonymous user cannot read example-notif module")
+            {
+                expectedNotificationsJSON = {notificationsJSON[0], notificationsJSON[1], notificationsJSON[3], notificationsJSON[4]};
+            }
+
+            SECTION("root user")
+            {
+                headers = {AUTH_ROOT};
+                expectedNotificationsJSON = notificationsJSON;
+            }
+        }
+
+        boost::asio::io_service io;
+
+        std::latch requestSent(1);
+
+        std::jthread notificationThread = std::jthread([&]() {
+            auto notifSession = sysrepo::Connection{}.sessionStart();
+            auto ctx = notifSession.getContext();
+
+            // wait until the client requests
+            requestSent.wait();
+
+            SEND_NOTIFICATION(notificationsJSON[0]);
+            SEND_NOTIFICATION(notificationsJSON[1]);
+            std::this_thread::sleep_for(500ms); // simulate some delays; server might be slow in creating notifications, client should still remain connected
+            SEND_NOTIFICATION(notificationsJSON[2]);
+            std::this_thread::sleep_for(500ms);
+            SEND_NOTIFICATION(notificationsJSON[3]);
+            SEND_NOTIFICATION(notificationsJSON[4]);
+
+            // stop io_service after everything is processed
+            waitForCompletionAndBitMore(seq1);
+            io.stop();
+        });
+
+        NotificationWatcher netconfWatcher(srConn.sessionStart().getContext(), dataFormat);
+        SSEClient cli(io, requestSent, netconfWatcher, uri, headers);
+
+        for (const auto& notif : expectedNotificationsJSON) {
+            EXPECT_NOTIFICATION(notif);
+        }
+
+        io.run();
+    }
+
+    SECTION("Invalid URLs")
+    {
+        REQUIRE(get("/streams/NETCONF/", {}) == Response{404, plaintextHeaders, "Invalid stream"});
+        REQUIRE(get("/streams/NETCONF/", {AUTH_ROOT}) == Response{404, plaintextHeaders, "Invalid stream"});
+        REQUIRE(get("/streams/NETCONF/bla", {}) == Response{404, plaintextHeaders, "Invalid stream"});
+    }
+}

CMakeLists.txt
@@ -98,6 +98,7 @@ add_library(rousette-auth STATIC
 target_link_libraries(rousette-auth PUBLIC spdlog::spdlog PkgConfig::SYSREPO-CPP PkgConfig::PAM)
 
 add_library(rousette-restconf STATIC
+    src/restconf/NotificationStream.cpp
     src/restconf/Server.cpp
     src/restconf/YangSchemaLocations.cpp
     src/restconf/uri.cpp
@@ -144,6 +145,7 @@ if(BUILD_TESTING)
         tests/datastoreUtils.cpp
         tests/doctest_integration.cpp
         tests/trompeloeil_doctest.h
+        tests/wait-a-bit-longer.cpp
         )
 
     target_include_directories(DoctestIntegration PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/tests/ ${CMAKE_CURRENT_SOURCE_DIR}/src/)
@@ -186,13 +188,15 @@ if(BUILD_TESTING)
         --install ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-restconf-monitoring@2017-01-26.yang
         --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang --enable-feature f1
         --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example-delete.yang
-        --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example-augment.yang)
+        --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example-augment.yang
+        --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example-notif.yang)
     rousette_test(NAME restconf-reading LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-writing LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-delete LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-rpc LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-nacm LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     rousette_test(NAME restconf-defaults LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
+    rousette_test(NAME restconf-notifications LIBRARIES rousette-restconf FIXTURE common-models WRAP_PAM)
     set(nested-models
         ${common-models}
         --install ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/root-mod.yang)

commit c9b0bed7a961c7599247f1218315b37d548cba29

uri: reject wrong HTTP methods for streams
Requesting streams does make sense only with GET and HEAD HTTP
methods. We do not support HEAD yet though.

Fixes: df5d6a0
Change-Id: I563fee08f71ff6637175604badba3f643f127209

src/restconf/Server.cpp
@@ -563,7 +563,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
         try {
             authorizeRequest(nacm, sess, req);
 
-            auto streamRequest = asRestconfStreamRequest(req.uri().path, req.uri().raw_query);
+            auto streamRequest = asRestconfStreamRequest(req.method(), req.uri().path, req.uri().raw_query);
 
             switch(streamRequest.type) {
             case RestconfStreamRequest::Type::NetconfNotificationJSON:

src/restconf/uri.cpp
@@ -590,10 +590,14 @@ std::optional<std::variant<libyang::Module, libyang::SubmoduleParsed>> asYangMod
     return std::nullopt;
 }
 
-RestconfStreamRequest asRestconfStreamRequest(const std::string& uriPath, const std::string& uriQueryString)
+RestconfStreamRequest asRestconfStreamRequest(const std::string& httpMethod, const std::string& uriPath, const std::string& uriQueryString)
 {
     static const auto netconfStreamRoot = "/streams/NETCONF/";
     RestconfStreamRequest::Type type;
+
+    if (httpMethod != "GET") {
+        throw ErrorResponse(405, "application", "operation-not-supported", "Method not allowed.");
+    }
 
     if (uriPath == netconfStreamRoot + "XML"s) {
         type = RestconfStreamRequest::Type::NetconfNotificationXML;

src/restconf/uri.h
@@ -150,5 +150,5 @@ RestconfRequest asRestconfRequest(const libyang::Context& ctx, const std::string
 std::optional<libyang::SchemaNode> asLibyangSchemaNode(const libyang::Context& ctx, const std::vector<PathSegment>& pathSegments);
 std::pair<std::string, PathSegment> asLibyangPathSplit(const libyang::Context& ctx, const std::string& uriPath);
 std::optional<std::variant<libyang::Module, libyang::SubmoduleParsed>> asYangModule(const libyang::Context& ctx, const std::string& uriPath);
-RestconfStreamRequest asRestconfStreamRequest(const std::string& uriPath, const std::string& uriQueryString);
+RestconfStreamRequest asRestconfStreamRequest(const std::string& httpMethod, const std::string& uriPath, const std::string& uriQueryString);
 }

tests/restconf-notifications.cpp
@@ -225,6 +225,15 @@ TEST_CASE("NETCONF notification streams")
         io.run();
     }
 
+    SECTION("Invalid methods")
+    {
+        REQUIRE(clientRequest("PUT", "/streams/NETCONF/XML", "", {AUTH_ROOT}) == Response{405, plaintextHeaders, "Method not allowed."});
+        REQUIRE(clientRequest("POST", "/streams/NETCONF/XML", "", {AUTH_ROOT}) == Response{405, plaintextHeaders, "Method not allowed."});
+        REQUIRE(clientRequest("PATCH", "/streams/NETCONF/XML", "", {AUTH_ROOT}) == Response{405, plaintextHeaders, "Method not allowed."});
+        REQUIRE(clientRequest("OPTIONS", "/streams/NETCONF/XML", "", {AUTH_ROOT}) == Response{405, plaintextHeaders, "Method not allowed."});
+        REQUIRE(clientRequest("DELETE", "/streams/NETCONF/XML", "", {AUTH_ROOT}) == Response{405, plaintextHeaders, "Method not allowed."});
+    }
+
     SECTION("Invalid URLs")
     {
         REQUIRE(get("/streams/NETCONF/", {}) == Response{404, plaintextHeaders, "Invalid stream"});

tests/uri-parser.cpp
@@ -825,7 +825,7 @@ TEST_CASE("URI path parser")
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'depth' can be used only with GET and HEAD methods").c_str(),
                                        rousette::restconf::ErrorResponse);
 
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "depth=1"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "depth=1"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'depth' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
             }
@@ -839,7 +839,7 @@ TEST_CASE("URI path parser")
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'with-defaults' can be used only with GET and HEAD methods").c_str(),
                                        rousette::restconf::ErrorResponse);
 
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "with-defaults=report-all"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "with-defaults=report-all"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'with-defaults' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
             }
@@ -853,7 +853,7 @@ TEST_CASE("URI path parser")
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'content' can be used only with GET and HEAD methods").c_str(),
                                        rousette::restconf::ErrorResponse);
 
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "content=config"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "content=config"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'content' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
             }
@@ -870,7 +870,7 @@ TEST_CASE("URI path parser")
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'insert' can be used only with POST and PUT methods").c_str(),
                                        rousette::restconf::ErrorResponse);
 
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "insert=first"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "insert=first"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'insert' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
             }
@@ -901,17 +901,17 @@ TEST_CASE("URI path parser")
                 REQUIRE_THROWS_WITH_AS(asRestconfRequest(ctx, "POST", "/restconf/data/example:ordered-lists", "point=/example:ordered-lists/ll=key"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'point' must always come with parameter 'insert' set to 'before' or 'after'").c_str(),
                                        rousette::restconf::ErrorResponse);
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "insert=after"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "insert=after"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'insert' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
-                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XML", "insert=after&point=/example:mod"),
+                REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "insert=after&point=/example:mod"),
                                        serializeErrorResponse(400, "protocol", "invalid-value", "Query parameter 'insert' can't be used with streams").c_str(),
                                        rousette::restconf::ErrorResponse);
             }
 
             SECTION("filter")
             {
-                auto resp = asRestconfStreamRequest("/streams/NETCONF/XML", "filter=/asd");
+                auto resp = asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "filter=/asd");
                 REQUIRE(resp.queryParams == QueryParams({{"filter", "/asd"s}}));
 
                 REQUIRE_THROWS_WITH_AS(asRestconfRequest(ctx, "GET", "/restconf/data/example:ordered-lists", "filter=something"),
@@ -921,7 +921,7 @@ TEST_CASE("URI path parser")
 
             SECTION("start-time")
             {
-                auto resp = asRestconfStreamRequest("/streams/NETCONF/XML", "start-time=2024-01-01T01:01:01Z");
+                auto resp = asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "start-time=2024-01-01T01:01:01Z");
                 REQUIRE(resp.queryParams == QueryParams({{"start-time", "2024-01-01T01:01:01Z"s}}));
 
                 REQUIRE_THROWS_WITH_AS(asRestconfRequest(ctx, "GET", "/restconf/data/example:ordered-lists", "start-time=2024-01-01T01:01:01Z"),
@@ -931,7 +931,7 @@ TEST_CASE("URI path parser")
 
             SECTION("stop-time")
             {
-                auto resp = asRestconfStreamRequest("/streams/NETCONF/XML", "stop-time=2024-01-01T01:01:01Z");
+                auto resp = asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "stop-time=2024-01-01T01:01:01Z");
                 REQUIRE(resp.queryParams == QueryParams({{"stop-time", "2024-01-01T01:01:01Z"s}}));
 
                 REQUIRE_THROWS_WITH_AS(asRestconfRequest(ctx, "GET", "/restconf/data/example:ordered-lists", "stop-time=2024-01-01T01:01:01Z"),
@@ -951,28 +951,36 @@ TEST_CASE("URI path parser")
         using rousette::restconf::RestconfStreamRequest;
 
         {
-            auto [type, queryParams] = asRestconfStreamRequest("/streams/NETCONF/XML", "");
+            auto [type, queryParams] = asRestconfStreamRequest("GET", "/streams/NETCONF/XML", "");
             REQUIRE(type == RestconfStreamRequest::Type::NetconfNotificationXML);
             REQUIRE(queryParams.empty());
         }
 
         {
-            auto [type, queryParams] = asRestconfStreamRequest("/streams/NETCONF/JSON", "");
+            auto [type, queryParams] = asRestconfStreamRequest("GET", "/streams/NETCONF/JSON", "");
             REQUIRE(type == RestconfStreamRequest::Type::NetconfNotificationJSON);
             REQUIRE(queryParams.empty());
         }
 
-        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF", ""),
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF", ""),
                 serializeErrorResponse(404, "application", "invalid-value", "Invalid stream").c_str(),
                 rousette::restconf::ErrorResponse);
-        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/restconf/data", ""),
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/restconf/data", ""),
                 serializeErrorResponse(404, "application", "invalid-value", "Invalid stream").c_str(),
                 rousette::restconf::ErrorResponse);
-        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/xml", ""),
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/xml", ""),
                 serializeErrorResponse(404, "application", "invalid-value", "Invalid stream").c_str(),
                 rousette::restconf::ErrorResponse);
-        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("/streams/NETCONF/XM", ""),
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF/XM", ""),
                 serializeErrorResponse(404, "application", "invalid-value", "Invalid stream").c_str(),
                 rousette::restconf::ErrorResponse);
+
+
+        for (const auto& httpMethod : {"HEAD", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"}) {
+            CAPTURE(httpMethod);
+            REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest(httpMethod, "/streams/NETCONF", ""),
+                    serializeErrorResponse(405, "application", "operation-not-supported", "Method not allowed.").c_str(),
+                    rousette::restconf::ErrorResponse);
+        }
     }
 }