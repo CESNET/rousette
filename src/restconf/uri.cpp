CESNET/rousette

commit a564f0cfb24299515b153105b533d40e484e57d9

server: construct libyang paths from URIs
The RESTCONF data resource identifiers [1, sec 3.5] and libyang paths
[2] differ a bit. This patch translates the former into the later.
This is needed so we can use libyang/sysrepo to perform the data
manipulation.

The RFC also states what the data resource is [1, sec. 3.5]. For
instance, you cannot query a whole list. So, some checks are needed when
translating the path.

[1] https://www.rfc-editor.org/rfc/rfc8040.html#section-3.5.3.1
[2] https://netopeer.liberouter.org/doc/libyang/master/html/howto_x_path.html

Change-Id: I541895e612860e4ba37329f7bab377e372aaa111

src/restconf/uri.cpp
@@ -6,7 +6,10 @@
 
 #include "restconf/uri_impl.h"
 
-namespace rousette::restconf::impl {
+using namespace std::string_literals;
+
+namespace rousette::restconf {
+namespace impl {
 
 namespace {
 namespace x3 = boost::spirit::x3;
@@ -71,5 +74,149 @@ PathSegment::PathSegment(const ApiIdentifier& apiIdent, const std::vector<std::s
 ResourcePath::ResourcePath(const std::vector<PathSegment>& segments)
     : segments(segments)
 {
+}
+}
+
+namespace {
+std::optional<libyang::SchemaNode> findChildSchemaNode(libyang::SchemaNode node, const impl::ApiIdentifier& childIdentifier)
+{
+    for (const auto& child : node.childInstantiables()) {
+        if (child.name() == childIdentifier.identifier) {
+            // If the prefix is not specified then we must ensure that child's module is the same as the node's module so that we don't accidentally return a child that was inserted here via an augment
+            if (
+                (!childIdentifier.prefix && std::string{node.module().name()} == std::string{child.module().name()}) || (childIdentifier.prefix && std::string{child.module().name()} == *childIdentifier.prefix)) {
+                return child;
+            }
+        }
+    }
+
+    return std::nullopt;
+}
+
+/** @brief Construct a fully qualified name of the node if needed
+ *
+ * @return string in the form <module>:<nodeName> if the parent module does not exist or is different from module of @p node else return only name of @p node.
+ */
+std::string maybeQualified(libyang::SchemaNode currentNode)
+{
+    using namespace std::string_literals;
+
+    std::string res;
+
+    if (!currentNode.parent() || currentNode.parent()->module().name() != currentNode.module().name()) {
+        res += std::string{currentNode.module().name()} + ":";
+    }
+
+    return res + std::string{currentNode.name()};
+}
+
+/** @brief Escapes key with the other type of quotes than found in the string.
+ *
+ *  @throws InvalidURIException if both single and double quotes used in the input
+ * */
+std::string escapeListKey(const std::string& str)
+{
+    auto singleQuotes = str.find('\'') != std::string::npos;
+    auto doubleQuotes = str.find('\"') != std::string::npos;
+
+    if (singleQuotes && doubleQuotes) {
+        throw InvalidURIException("Encountered mixed single and double quotes in XPath. Can't properly escape.");
+    } else if (singleQuotes) {
+        return '\"' + str + '\"';
+    } else {
+        return '\'' + str + '\'';
+    }
+}
+
+std::string apiIdentName(const impl::ApiIdentifier& apiIdent)
+{
+    if (!apiIdent.prefix) {
+        return apiIdent.identifier;
+    }
+    return *apiIdent.prefix + ":" + apiIdent.identifier;
+}
+
+bool isValidDataResource(libyang::SchemaNode node)
+{
+    switch (node.nodeType()) {
+    case libyang::NodeType::Container:
+    case libyang::NodeType::Leaf:
+    case libyang::NodeType::AnyXML:
+    case libyang::NodeType::AnyData:
+    /* querying the actual (leaf-)list node is not a valid data resource, only (leaf-)list entries are.
+     * Yet we consider this as a valid resource here. If this function is called we already checked if the keys are specified in the caller.
+     * If they were correctly specified, then we are querying instance. If not, then the code already throwed.
+     */
+    case libyang::NodeType::Leaflist:
+    case libyang::NodeType::List:
+        return true;
+    default:
+        return false;
+    }
+}
+}
+
+/** @brief Transforms URI path (i.e., data resource identifier) into a path that is understood by libyang
+ *
+ * @throws InvalidURIException When the path is contextually invalid
+ * @throws InvalidURIException When URI cannot be parsed
+ * @throws InvalidURIException When unable to properly escape YANG list key value (i.e., the list value contains both single and double quotes).
+ * @return libyang path as a string or std::nullopt when the path has wrong format or contextually wrong (nonexistent leafs, wrong list keys, etc.).
+ */
+std::optional<std::string> asLibyangPath(const libyang::Context& ctx, const std::string& uriPath)
+{
+    std::optional<libyang::SchemaNode> currentNode;
+    std::string res;
+
+    auto resourcePath = impl::parseUriPath(uriPath);
+    if (!resourcePath) {
+        throw InvalidURIException("Syntax error");
+    }
+
+    for (auto it = resourcePath->segments.begin(); it != resourcePath->segments.end(); ++it) {
+        if (auto prevNode = currentNode) {
+            if (!(currentNode = findChildSchemaNode(*currentNode, it->apiIdent))) {
+                throw InvalidURIException("Node '" + apiIdentName(it->apiIdent) + "' is not a child of '" + prevNode->path() + "'");
+            }
+        } else { // we are starting at root (no parent)
+            try {
+                currentNode = ctx.findPath("/" + *it->apiIdent.prefix + ":" + it->apiIdent.identifier);
+            } catch (const libyang::Error& e) {
+                throw InvalidURIException(""s + e.what());
+            }
+        }
+
+        res += "/" + maybeQualified(*currentNode);
+
+        if (currentNode->nodeType() == libyang::NodeType::List) {
+            const auto& listKeys = currentNode->asList().keys();
+
+            if (listKeys.size() == 0) {
+                throw InvalidURIException("List '" + currentNode->path() + "' has no keys. It can not be accessed directly");
+            } else if (it->keys.size() != listKeys.size()) {
+                throw InvalidURIException("List '" + currentNode->path() + "' requires " + std::to_string(listKeys.size()) + " keys");
+            }
+
+            // FIXME: use std::views::zip in C++23
+            auto itKeyValue = it->keys.begin();
+            for (auto itKeyName = listKeys.begin(); itKeyName != listKeys.end(); ++itKeyName, ++itKeyValue) {
+                res += '[' + std::string{itKeyName->name()} + "=" + escapeListKey(*itKeyValue) + ']';
+            }
+        } else if (currentNode->nodeType() == libyang::NodeType::Leaflist) {
+            if (it->keys.size() != 1) {
+                throw InvalidURIException("Leaf-list '" + currentNode->path() + "' requires exactly one key");
+            }
+
+            res += "[.=" + escapeListKey(it->keys.front()) + ']';
+        } else if (it->keys.size() > 0) {
+            throw InvalidURIException("No keys allowed for node '" + currentNode->path() + "'");
+        }
+
+        if (!isValidDataResource(*currentNode)) {
+            throw InvalidURIException("'"s + currentNode->path() + "' is not a data resource");
+        }
+    }
+
+    return res;
 }
 }

CMakeLists.txt
@@ -181,10 +181,14 @@ if(BUILD_TESTING)
     endfunction()
 
     rousette_test(NAME http-server LIBRARIES rousette-restconf)
-    rousette_test(NAME uri-parser LIBRARIES rousette-restconf)
 
     set(fixture-http
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/ietf-system@2014-08-06.yang FEATURE radius
         YANG ${CMAKE_CURRENT_SOURCE_DIR}/yang/ietf-restconf@2017-01-26.yang)
     rousette_test(NAME http-communication LIBRARIES rousette-restconf FIXTURE fixture-http)
+
+    set(fixture-uri-parser
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example.yang FEATURE f1
+        YANG ${CMAKE_CURRENT_SOURCE_DIR}/tests/yang/example-augment.yang)
+    rousette_test(NAME uri-parser LIBRARIES rousette-restconf FIXTURE fixture-uri-parser)
 endif()

src/restconf/uri.h
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Tomáš Pecka <tomas.pecka@cesnet.cz>
+ */
+
+#pragma once
+#include <optional>
+#include <stdexcept>
+#include <string>
+
+namespace libyang {
+class Context;
+}
+
+namespace rousette::restconf {
+std::optional<std::string> asLibyangPath(const libyang::Context& ctx, const std::string& uriPath);
+
+class InvalidURIException : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
+};
+
+}

src/restconf/uri_impl.h
@@ -9,6 +9,7 @@
 #include <boost/spirit/home/x3.hpp>
 #include <string>
 #include <sysrepo-cpp/Session.hpp>
+#include "restconf/uri.h"
 
 namespace rousette::restconf {
 

tests/uri-parser.cpp
@@ -8,6 +8,8 @@
 
 #include "trompeloeil_doctest.h"
 #include <experimental/iterator>
+#include <sysrepo-cpp/Connection.hpp>
+#include "restconf/uri.h"
 #include "restconf/uri_impl.h"
 
 namespace rousette::restconf::impl {
@@ -44,6 +46,22 @@ std::ostream& operator<<(std::ostream& os, const ResourcePath& obj)
 }
 
 namespace doctest {
+template <>
+struct StringMaker<std::optional<std::string>> {
+    static String convert(const std::optional<std::string>& obj)
+    {
+        std::ostringstream oss;
+
+        if (obj) {
+            oss << "optional{" << *obj << "}";
+        } else {
+            oss << "nullopt{}";
+        }
+
+        return oss.str().c_str();
+    }
+};
+
 template <>
 struct StringMaker<std::optional<rousette::restconf::impl::ResourcePath>> {
     static String convert(const std::optional<rousette::restconf::impl::ResourcePath>& obj)
@@ -232,6 +250,73 @@ TEST_CASE("URI path parser")
 
             CAPTURE(uriPath);
             REQUIRE(!rousette::restconf::impl::parseUriPath(uriPath));
+        }
+    }
+
+    SECTION("Translation to libyang path")
+    {
+        auto conn = sysrepo::Connection{};
+        auto sess = conn.sessionStart();
+
+        REQUIRE(sess.getContext().getModuleImplemented("example"));
+        REQUIRE(sess.getContext().getModuleImplemented("example-augment"));
+
+        SECTION("Contextually valid paths")
+        {
+            for (const auto& [uriPath, expectedLyPath] : {
+                     std::pair<std::string, std::optional<std::string>>{"/restconf/data/example:top-level-leaf", "/example:top-level-leaf"},
+                     {"/restconf/data/example:top-level-list=hello", "/example:top-level-list[name='hello']"},
+                     {"/restconf/data/example:l/list=eth0", "/example:l/list[name='eth0']"},
+                     {R"(/restconf/data/example:l/list=et"h0)", R"(/example:l/list[name='et"h0'])"},
+                     {R"(/restconf/data/example:l/list=et%22h0)", R"(/example:l/list[name='et"h0'])"},
+                     {R"(/restconf/data/example:l/list=et%27h0)", R"(/example:l/list[name="et'h0"])"},
+                     {"/restconf/data/example:l/list=eth0/name", "/example:l/list[name='eth0']/name"},
+                     {"/restconf/data/example:l/list=eth0/nested=1,2,3", "/example:l/list[name='eth0']/nested[first='1'][second='2'][third='3']"},
+                     {"/restconf/data/example:l/list=eth0/nested=,2,3", "/example:l/list[name='eth0']/nested[first=''][second='2'][third='3']"},
+                     {"/restconf/data/example:l/list=eth0/nested=,2,", "/example:l/list[name='eth0']/nested[first=''][second='2'][third='']"},
+                     {"/restconf/data/example:l/list=eth0/choice1", "/example:l/list[name='eth0']/choice1"},
+                     {"/restconf/data/example:l/list=eth0/choice2", "/example:l/list[name='eth0']/choice2"},
+                     {"/restconf/data/example:l/list=eth0/collection=val", "/example:l/list[name='eth0']/collection[.='val']"},
+                     {"/restconf/data/example:l/status", "/example:l/status"},
+                     // container example:a has a container b inserted locally and also via an augment. Check that we return the correct one
+                     {"/restconf/data/example:a/b", "/example:a/b"},
+                     {"/restconf/data/example:a/b/c", "/example:a/b/c"},
+                     {"/restconf/data/example:a/b/c/enabled", "/example:a/b/c/enabled"},
+                     {"/restconf/data/example:a/example-augment:b", "/example:a/example-augment:b"},
+                     {"/restconf/data/example:a/example-augment:b/c", "/example:a/example-augment:b/c"},
+                     {"/restconf/data/example:a/example-augment:b/example-augment:c", "/example:a/example-augment:b/c"},
+                     {"/restconf/data/example:a/example-augment:b/c/enabled", "/example:a/example-augment:b/c/enabled"},
+                 }) {
+                CAPTURE(uriPath);
+                REQUIRE(rousette::restconf::impl::parseUriPath(uriPath));
+                REQUIRE(rousette::restconf::asLibyangPath(sess.getContext(), uriPath) == expectedLyPath);
+            }
+        }
+
+        SECTION("Contextually invalid paths")
+        {
+            for (const auto& uriPath : std::vector<std::string>{
+                     "/restconf/data/hello:world", // nonexistent module
+                     "/restconf/data/example:foo", // nonexistent top-level node
+                     "/restconf/data/example-augment:b", // nonexistent top-level node
+                     "/restconf/data/example:l/hello-world", // nonexistent node
+                     "/restconf/data/example:f", // feature not enabled
+                     "/restconf/data/example:top-level-list", // list is not a data resource
+                     "/restconf/data/example:l/key-less-list", // list is not a data resource
+                     "/restconf/data/example:l/list=eth0/collection", // leaf-list is not a data resource
+                     "/restconf/data/example:test-rpc", // RPC is not a data resource
+                     "/restconf/data/example:test-rpc/i", // RPC input node is not a data resource
+                     "/restconf/data/example:test-rpc/o", // RPC output node is not a data resource
+                     "/restconf/data/example:l=eth0", // node not a (leaf-)list
+                     "/restconf/data/example:l/list=eth0,eth1", // wrong number of list elements
+                     "/restconf/data/example:l/list=eth0/collection=br0,eth1", // wrong number of keys for a leaf-list
+                     "/restconf/data/example:l/list=eth0/choose", // schema nodes should not be visible
+                     "/restconf/data/example:l/list=eth0/choose/choice1", // schema nodes should not be visible
+                 }) {
+                CAPTURE(uriPath);
+                REQUIRE(rousette::restconf::impl::parseUriPath(uriPath));
+                REQUIRE_THROWS_AS(rousette::restconf::asLibyangPath(sess.getContext(), uriPath), rousette::restconf::InvalidURIException);
+            }
         }
     }
 }

tests/yang/example-augment.yang
@@ -1,0 +1,20 @@
+module example-augment {
+  yang-version 1.1;
+  namespace "http://example.tld/example-augment";
+  prefix aug;
+
+  import example {
+    prefix ex;
+  }
+
+  augment "/ex:a" {
+    container b {
+      container c {
+        leaf enabled {
+          type boolean;
+          default true;
+        }
+      }
+    }
+  }
+}

tests/yang/example.yang
@@ -1,0 +1,69 @@
+module example {
+  yang-version 1.1;
+  namespace "http://example.tld/example";
+  prefix ex;
+
+  feature f1 { }
+  feature f2 { }
+
+  leaf top-level-leaf { type string; }
+  list top-level-list {
+    key "name";
+    leaf name { type string; }
+  }
+
+  container l {
+    if-feature f1;
+    list list {
+      key "name";
+      leaf name { type string; }
+      leaf-list collection { type int32; }
+      list nested {
+        key "first second third";
+        leaf first { type string; }
+        leaf second { type int32; }
+        leaf third { type string; }
+      }
+      choice choose {
+        mandatory true;
+        leaf choice1 { type string; }
+        leaf choice2 { type string; }
+      }
+    }
+    list key-less-list {
+      config false;
+      leaf name { type string; }
+    }
+    leaf status {
+      type enumeration {
+        enum on { }
+        enum off { }
+      }
+    }
+  }
+
+  container f {
+    if-feature f2;
+    leaf hello { type string; }
+  }
+
+  container a {
+    container b {
+      container c {
+        leaf enabled {
+          type boolean;
+          default true;
+        }
+      }
+    }
+  }
+
+  rpc test-rpc {
+    input {
+      leaf i { type string; }
+    }
+    output {
+      leaf o { type string; }
+    }
+  }
+}

commit ff92540de63717692909445016438fe001e71da4

refactor: ApiIdentifier already has a conversion function
Change-Id: I2cd7ace084a1fd6f3677fea9183843517d5fbc56
Fixes: a564f0c server: construct libyang paths from URIs

src/restconf/uri.cpp
@@ -451,7 +451,7 @@ SchemaNodeAndPath asLibyangPath(const libyang::Context& ctx, const std::vector<P
             }
         } else { // we are starting at root (no parent)
             try {
-                currentNode = ctx.findPath("/" + *it->apiIdent.prefix + ":" + it->apiIdent.identifier);
+                currentNode = ctx.findPath("/" + apiIdentName(it->apiIdent));
             } catch (const libyang::Error& e) {
                 throw ErrorResponse(400, "application", "operation-failed", e.what());
             }