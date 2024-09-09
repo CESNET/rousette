11f201da27f4eb7b748baaf3f47495863a8e8d6b
Jan Kundrát <jan.kundrat@cesnet.cz>
2024-09-09 23:35:32 +0200

    actions: allow execution via and NMDA URL
    As per [1], actions can be executed through an NMDA URL when targeting
    the operational datastore.
    
    [1] https://datatracker.ietf.org/doc/html/rfc8527#section-3.1
    
    Change-Id: I732c91c604f14ce22502ec677d16a6441708f1d9

--- tests/uri-parser.cpp
+++ tests/uri-parser.cpp
@@ -414,6 +414,16 @@ TEST_CASE("URI path parser")
                 REQUIRE(action == RestconfRequest::Type::Execute);
                 REQUIRE(queryParams.empty());
             }
+
+            SECTION("POST (action NMDA)")
+            {
+                auto [action, datastore, path, queryParams] = rousette::restconf::asRestconfRequest(ctx, "POST",
+                        "/restconf/ds/ietf-datastores:operational/example:tlc/list=hello-world/example-action");
+                REQUIRE(path == "/example:tlc/list[name='hello-world']/example-action");
+                REQUIRE(datastore == sysrepo::Datastore::Operational);
+                REQUIRE(action == RestconfRequest::Type::Execute);
+                REQUIRE(queryParams.empty());
+            }
         }
 
         SECTION("Contextually invalid paths")
@@ -573,10 +583,15 @@ TEST_CASE("URI path parser")
                     uriPath = "/restconf/data/example:test-rpc";
                     expectedErrorMessage = "RPC '/example:test-rpc' must be requested using operation prefix";
                 }
-                SECTION("actions must be invoked via /restconf/data")
+                SECTION("action via operations prefix")
                 {
                     uriPath = "/restconf/operations/example:tlc/list=eth0/example-action";
-                    expectedErrorMessage = "Action '/example:tlc/list/example-action' must be requested using data prefix";
+                    expectedErrorMessage = "Action '/example:tlc/list/example-action' must be requested using data prefix or via operational NMDA";
+                }
+                SECTION("action via NMDA running DS")
+                {
+                    uriPath = "/restconf/ds/ietf-datastores:running/example:tlc/list=eth0/example-action";
+                    expectedErrorMessage = "Action '/example:tlc/list/example-action' must be requested using data prefix or via operational NMDA";
                 }
 
                 SECTION("RPC and action input/output nodes")
@@ -1029,6 +1044,11 @@ TEST_CASE("URI path parser")
                 expected = {"GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"};
                 SECTION("Basic root") { uri = "/restconf/data"; }
                 SECTION("NMDA running") { uri = "/restconf/ds/ietf-datastores:running"; }
+            }
+            SECTION("NMDA action")
+            {
+                expected = {"OPTIONS", "POST"};
+                SECTION("Action") { uri = "/restconf/ds/ietf-datastores:operational/example:tlc/list=key/example-action"; }
             }
             SECTION("Invalid path")
             {
