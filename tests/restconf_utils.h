7d15f59d20079ba94224e0bc308682aa5a004483
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-12-02 20:15:05 +0100

    tests: make SSEClient reusable
    We will need it in yang push tests
    
    Change-Id: I22432553f3abff0de91b3c406abc5567de656065

--- tests/restconf_utils.h
+++ tests/restconf_utils.h
@@ -8,7 +8,9 @@
 
 #pragma once
 #include "trompeloeil_doctest.h"
+#include <latch>
 #include <nghttp2/asio_http2_client.h>
+#include "event_watchers.h"
 #include "UniqueResource.h"
 
 namespace sysrepo {
@@ -81,3 +83,48 @@ Response clientRequest(
 
 UniqueResource manageNacm(sysrepo::Session session);
 void setupRealNacm(sysrepo::Session session);
+
+struct SSEClient {
+    std::shared_ptr<ng_client::session> client;
+    boost::asio::deadline_timer t;
+
+    SSEClient(
+        boost::asio::io_service& io,
+        const std::string& server_address,
+        const std::string& server_port,
+        std::latch& requestSent,
+        const RestconfNotificationWatcher& notification,
+        const std::string& uri,
+        const std::map<std::string, std::string>& headers,
+        const boost::posix_time::seconds silenceTimeout = boost::posix_time::seconds(1)); // test code; the server should respond "soon"
+
+    static std::vector<std::string> parseEvents(const std::string& msg);
+};
+
+#define PREPARE_LOOP_WITH_EXCEPTIONS \
+    boost::asio::io_service io; \
+    std::promise<void> bg; \
+    std::latch requestSent(1);
+
+#define RUN_LOOP_WITH_EXCEPTIONS \
+    do { \
+        io.run(); \
+        auto fut = bg.get_future(); \
+        REQUIRE(fut.wait_for(666ms /* "plenty of time" for the notificationThread to exit after it has called io.stop() */) == std::future_status::ready); \
+        fut.get(); \
+    } while (false)
+
+inline auto wrap_exceptions_and_asio(std::promise<void>& bg, boost::asio::io_service& io, std::function<void()> func)
+{
+    return [&bg, &io, func]()
+    {
+        try {
+            func();
+        } catch (...) {
+            bg.set_exception(std::current_exception());
+            return;
+        }
+        bg.set_value();
+        io.stop();
+    };
+}
