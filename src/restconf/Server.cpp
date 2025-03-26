6ad606cb9ad49ca7bfb8056b9695ab271a33e790
Tomáš Pecka <tomas.pecka@cesnet.cz>
2025-03-26 18:56:06 +0100

    make sure that edit tree is the first sibling
    After a bug in velia [1], where we passed to Session::editBatch a tree
    which was not the first sibling (and this caused that all the left
    siblings of the node were not considered in the edit), I am making sure
    that we do not lose any data in this project due to this too.
    
    [1] https://github.com/CESNET/velia/commit/2b7187405740fe61e8b37e0efab8c3173d398a26
    
    Fixes: 25f3e3b ("restconf: support for YANG patch")
    Change-Id: I7211168325fbfddd75dc1520c9dd83e31138d3bc

--- src/restconf/Server.cpp
+++ src/restconf/Server.cpp
@@ -600,7 +600,7 @@ void processYangPatchEdit(const std::shared_ptr<RequestContext>& requestCtx, con
     if (!mergedEdits) {
         mergedEdits = *singleEdit;
     } else {
-        mergedEdits->insertSibling(*singleEdit);
+        mergedEdits = mergedEdits->insertSibling(*singleEdit); // make sure we point to the first sibling, sysrepo::editBatch requires that
     }
 }
 
