4621843b9ee8a816822abeae8c44e86004405b1a
Tomáš Pecka <tomas.pecka@cesnet.cz>
2025-04-08 17:00:59 +0200

    restconf: add {kill,delete}-subscription RPCs
    Add implementations for deleting and killing subscriptions [1,2].
    
    Rousette allows establishing subscriptions by anonymous users. Anonymous
    users can not call delete-subscription RPC though because we do not know
    if it is the same anonymous user who initiated the subscription.
    The subscriptions are killed after a minute of inactivity, so the
    subscription will get deleted by rousette automatically after the client
    disconnects.
    
    [1] https://datatracker.ietf.org/doc/html/rfc8639.html#section-2.4.4
    [2] https://datatracker.ietf.org/doc/html/rfc8639.html#section-2.4.5
    
    Change-Id: I443d6fd88f2797045b5ae0a7d7e1a761fe74e7cb

--- src/restconf/Server.cpp
+++ src/restconf/Server.cpp
@@ -444,6 +444,10 @@ std::optional<libyang::DataNode> processInternalRPC(sysrepo::Session& sess, liby
     const std::map<std::string, InternalRPCHandler> handlers{
         {"/ietf-subscribed-notifications:establish-subscription",
          [&dynamicSubscriptions](auto&&... args) { return dynamicSubscriptions.establishSubscription(std::forward<decltype(args)>(args)...); }},
+        {"/ietf-subscribed-notifications:kill-subscription",
+         [&dynamicSubscriptions](auto&&... args) { return dynamicSubscriptions.deleteSubscription(std::forward<decltype(args)>(args)...); }},
+        {"/ietf-subscribed-notifications:delete-subscription",
+         [&dynamicSubscriptions](auto&&... args) { return dynamicSubscriptions.deleteSubscription(std::forward<decltype(args)>(args)...); }},
     };
 
     const auto rpcPath = rpcInput.path();
