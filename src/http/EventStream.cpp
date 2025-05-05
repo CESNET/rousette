CESNET/rousette

commit 438a18f5884ace3063f00a2dfc53dd2b4034f53e

restconf: add access log to stream root
Change-Id: I5e1db3f40bc6a7f294e6a5fef9402e0855055258

src/restconf/Server.cpp
@@ -902,6 +902,9 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle(netconfStreamRoot, [this, conn](const auto& req, const auto& res) mutable {
+        const auto& peer = http::peer_from_request(req);
+        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+
         std::optional<std::string> xpathFilter;
         std::optional<sysrepo::NotificationTimeStamp> startTime;
         std::optional<sysrepo::NotificationTimeStamp> stopTime;

commit 1bce2fe4595655f16a457a16491b518d1569c28a

restconf: log requests exactly once
In the previous commit 438a18f ("restconf: add access log to stream
root") I added logging for restconf stream root access. However, I did
not realize that the request was also logged inside EventStream class so
now it is logged twice.

This commit removes the logging from EventStream class, which I find
confusing. It also adds a logging function that is called from each
server endpoint to log.

Fixes: 438a18f ("restconf: add access log to stream root")
Change-Id: I2f0fcb156217a19d39a20706089a6ab691807d79

src/http/EventStream.cpp
@@ -22,8 +22,6 @@ EventStream::EventStream(const server::request& req, const server::response& res
     : res{res}
     , peer{peer_from_request(req)}
 {
-    spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
-
     if (initialEvent) {
         enqueue(*initialEvent);
     }

src/restconf/Server.cpp
@@ -41,6 +41,12 @@ constexpr auto notifPrefix = R"json({"ietf-restconf:notification":{"eventTime":"
 constexpr auto notifMid = R"json(","ietf-yang-push:push-update":{"datastore-contents":)json";
 constexpr auto notifSuffix = R"json(}}})json";
 
+void logRequest(const auto& request)
+{
+    const auto& peer = http::peer_from_request(request);
+    spdlog::info("{}: {} {}", peer, request.method(), request.uri().raw_path);
+}
+
 template <typename T>
 auto as_restconf_push_update(const std::string& content, const T& time)
 {
@@ -878,15 +884,15 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle("/", [](const auto& req, const auto& res) {
-        const auto& peer = http::peer_from_request(req);
-        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        logRequest(req);
+
         res.write_head(404, {TEXT_PLAIN, CORS});
         res.end();
     });
 
     server->handle("/.well-known/host-meta", [](const auto& req, const auto& res) {
-        const auto& peer = http::peer_from_request(req);
-        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        logRequest(req);
+
         res.write_head(
                    200,
                    {
@@ -897,13 +903,14 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle("/telemetry/optics", [this](const auto& req, const auto& res) {
+        logRequest(req);
+
         auto client = std::make_shared<http::EventStream>(req, res, opticsChange, as_restconf_push_update(dwdmEvents->currentData(), std::chrono::system_clock::now()));
         client->activate();
     });
 
     server->handle(netconfStreamRoot, [this, conn](const auto& req, const auto& res) mutable {
-        const auto& peer = http::peer_from_request(req);
-        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        logRequest(req);
 
         std::optional<std::string> xpathFilter;
         std::optional<sysrepo::NotificationTimeStamp> startTime;
@@ -956,8 +963,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
     });
 
     server->handle(yangSchemaRoot, [this, conn /* intentional copy */](const auto& req, const auto& res) mutable {
-        const auto& peer = http::peer_from_request(req);
-        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        logRequest(req);
 
         if (req.method() == "OPTIONS" || (req.method() != "GET" && req.method() != "HEAD")) {
             res.write_head(req.method() == "OPTIONS" ? 200 : 405, {CORS, ALLOW_GET_HEAD_OPTIONS});
@@ -992,8 +998,7 @@ Server::Server(sysrepo::Connection conn, const std::string& address, const std::
 
     server->handle(restconfRoot,
         [conn /* intentionally by value, otherwise conn gets destroyed when the ctor returns */, this, timeout](const auto& req, const auto& res) mutable {
-            const auto& peer = http::peer_from_request(req);
-            spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+            logRequest(req);
 
             auto sess = conn.sessionStart(sysrepo::Datastore::Operational);
             DataFormat dataFormat;