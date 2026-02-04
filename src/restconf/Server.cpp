CESNET/rousette

commit 52050e50d8a28edf4f279ad20336d4a680bcf623

handle RESTCONF root autodiscovery
The RFC says [1] that a magic .well-known URL should return the
prefix as a specially formatted XML.

[1] https://www.rfc-editor.org/rfc/rfc8040#section-3.1

Change-Id: If659d6848a477e9885f49b4310be971371c7e215

src/restconf/Server.cpp
@@ -115,6 +115,18 @@ Server::Server(sysrepo::Connection conn)
         rejectResponse(req, res, 404, "resource does not exist");
     });
 
+    server->handle("/.well-known/host-meta", [](const auto& req, const auto& res) {
+        const auto& peer = http::peer_from_request(req);
+        spdlog::info("{}: {} {}", peer, req.method(), req.uri().raw_path);
+        res.write_head(
+                   200,
+                   {
+                       {"content-type", {"application/xrd+xml", false}},
+                       {"access-control-allow-origin", {"*", false}},
+                   });
+        res.end("<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'><Link rel='restconf' href='"s + restconfRoot + "'></XRD>"s);
+    });
+
     server->handle("/telemetry/optics", [this](const auto& req, const auto& res) {
         auto client = std::make_shared<http::EventStream>(req, res);
         client->activate(opticsChange, as_restconf_push_update(dwdmEvents->currentData(), std::chrono::system_clock::now()));

commit 96a885371112b69b5c5b5079a4eb0d060d9e7496

return valid XML in the .well-known response
Bug: https://github.com/CESNET/rousette/issues/22
Fixes: 52050e50 handle RESTCONF root autodiscovery
Change-Id: Ibb1f63c1db7fed0fed30c0be50b1634d8258cfab

src/restconf/Server.cpp
@@ -985,7 +985,7 @@ Server::Server(
                        contentType("application/xrd+xml"),
                        CORS,
                    });
-        res.end("<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'><Link rel='restconf' href='"s + restconfRoot + "'></XRD>"s);
+        res.end("<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'><Link rel='restconf' href='"s + restconfRoot + "'/></XRD>"s);
     });
 
     server->handle("/telemetry/optics", [this, keepAlivePingInterval](const auto& req, const auto& res) {