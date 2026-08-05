9bdc282d1c3caa2e121f3c6da6c05ded7fca96dc
Jan Kundrát <jan.kundrat@cesnet.cz>
2026-08-05 17:11:48 +0200

    tests: run a YANG linter on all files
    Since the NACM module is imported from `ietf-subscribed-notifications`,
    it has to move outside of the test directory.
    
    Change-Id: I2454a90b30eabffff380ece18fbb8e96a9da32b1
    Suggested-by: Tomáš Pecka <tomas.pecka@cesnet.cz>

--- tests/uri-parser.cpp
+++ tests/uri-parser.cpp
@@ -715,9 +715,10 @@ TEST_CASE("URI path parser")
         SECTION("Get modules")
         {
             auto ctx = libyang::Context{libyang::internalModuleDirectory(), libyang::ContextOptions::DisableSearchCwd};
+            ctx.setSearchDir(std::filesystem::path{CMAKE_CURRENT_SOURCE_DIR} / "yang");
+            ctx.loadModule("ietf-netconf-acm", "2018-02-14");
             ctx.setSearchDir(std::filesystem::path{CMAKE_CURRENT_SOURCE_DIR} / "tests" / "yang");
             auto mod = ctx.loadModule("example", std::nullopt, {"f1"});
-            ctx.loadModule("ietf-netconf-acm", "2018-02-14");
             ctx.loadModule("root-mod", std::nullopt);
 
             auto modName = [](auto&& mod) { return std::visit([](auto&& arg) { return arg.name(); }, mod); };
