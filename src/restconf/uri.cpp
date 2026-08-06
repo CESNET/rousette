CESNET/rousette

commit 6216ff53d90e30b2ce239c653b3fbace4e96cf9c

uri: parse /streams/configured/<name>
Add a ConfiguredStreamRequest production to the RESTCONF stream URI grammar
as a preparation for configured HTTP subscription streams.
It reserves /streams/configured/<name>.

Change-Id: I0ec3dda4c92000f2f3d32d7b13bd43217dd1a04e

src/restconf/uri.cpp
@@ -84,13 +84,14 @@ const auto uuid_impl = x3::rule<class uuid_impl, std::string>{"UUID (xxxxxxxx-xx
     x3::repeat(12)[x3::xdigit];
 const auto uuid = x3::rule<class uuid, boost::uuids::uuid>{"UUID"} = uuid_impl[string_to_uuid];
 const auto subscribedStream = x3::rule<class subscribedStream, SubscribedStreamRequest>{"subscribed stream"} = x3::lit("subscribed") > x3::lit("/") > uuid;
+const auto configuredStream = x3::rule<class configuredStream, ConfiguredStreamRequest>{"configured stream"} = x3::lit("configured") > x3::lit("/") > identifier;
 
 const auto xmlStream = x3::rule<class xmlStream, libyang::DataFormat>{"'XML'"} = x3::lit("XML") > x3::attr(libyang::DataFormat::XML);
 const auto jsonStream = x3::rule<class jsonStream, libyang::DataFormat>{"'JSON'"} = x3::lit("JSON") > x3::attr(libyang::DataFormat::JSON);
 const auto streamFormat = x3::rule<class streamFormat, libyang::DataFormat>{"stream format ('XML' or 'JSON')"} = (xmlStream | jsonStream);
 const auto notificationStream = x3::rule<class notificationStream, NotificationStreamRequest>{"notification stream"} = identifier > x3::lit("/") > streamFormat;
-const auto streamGrammar = x3::rule<class streamGrammar, std::variant<NotificationStreamRequest, SubscribedStreamRequest>>{"stream URI"} =
-    x3::lit("/") > x3::lit("streams") > x3::lit("/") > (subscribedStream | notificationStream);
+const auto streamGrammar = x3::rule<class streamGrammar, std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>>{"stream URI"} =
+    x3::lit("/") > x3::lit("streams") > x3::lit("/") > (subscribedStream | configuredStream | notificationStream);
 
 // clang-format on
 }
@@ -262,9 +263,9 @@ queryParams::QueryParams parseQueryParams(const std::string& querystring, const 
     return parse<queryParams::QueryParams, UriQuerySyntaxError>(querystring, queryParamGrammar, pathLength);
 }
 
-std::variant<NotificationStreamRequest, SubscribedStreamRequest> parseStreamUri(const std::string& input)
+std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest> parseStreamUri(const std::string& input)
 {
-    return parse<std::variant<NotificationStreamRequest, SubscribedStreamRequest>, UriPathSyntaxError>(input, streamGrammar);
+    return parse<std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>, UriPathSyntaxError>(input, streamGrammar);
 }
 
 URIPrefix::URIPrefix()
@@ -707,6 +708,13 @@ SubscribedStreamRequest::SubscribedStreamRequest()
 
 SubscribedStreamRequest::SubscribedStreamRequest(const boost::uuids::uuid& uuid)
     : uuid(uuid)
+{
+}
+
+ConfiguredStreamRequest::ConfiguredStreamRequest() = default;
+
+ConfiguredStreamRequest::ConfiguredStreamRequest(const std::string& name)
+    : name(name)
 {
 }
 

src/restconf/uri.h
@@ -214,7 +214,15 @@ struct SubscribedStreamRequest {
     SubscribedStreamRequest(const boost::uuids::uuid& uuid);
 };
 
-using RestconfStreamRequest = std::variant<NotificationStreamRequest, SubscribedStreamRequest>;
+/** @brief A request for a server-configured stream served under /streams/configured/<name>. */
+struct ConfiguredStreamRequest {
+    std::string name;
+
+    ConfiguredStreamRequest();
+    ConfiguredStreamRequest(const std::string& name);
+};
+
+using RestconfStreamRequest = std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>;
 
 RestconfRequest asRestconfRequest(const libyang::Context& ctx, const std::string& httpMethod, const std::string& uriPath, const std::string& uriQueryString = "");
 std::optional<libyang::SchemaNode> asLibyangSchemaNode(const libyang::Context& ctx, const std::vector<PathSegment>& pathSegments);

src/restconf/uri_impl.h
@@ -68,6 +68,7 @@ BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ApiIdentifier, prefix, identifier)
 
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::NotificationStreamRequest, stream, encoding);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::SubscribedStreamRequest, uuid);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ConfiguredStreamRequest, name);
 
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::queryParams::fields::ParenExpr, lhs, rhs);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::queryParams::fields::SlashExpr, lhs, rhs);

tests/uri-parser.cpp
@@ -1107,6 +1107,7 @@ TEST_CASE("URI path parser")
     {
         using rousette::restconf::asRestconfStreamRequest;
         using rousette::restconf::NotificationStreamRequest;
+        using rousette::restconf::ConfiguredStreamRequest;
         using rousette::restconf::SubscribedStreamRequest;
 
         {
@@ -1138,6 +1139,22 @@ TEST_CASE("URI path parser")
             REQUIRE(std::holds_alternative<SubscribedStreamRequest>(req));
             REQUIRE(std::get<SubscribedStreamRequest>(req).uuid == boost::uuids::string_generator()("a40f0a50-061a-4832-a6ac-c4db7df81a10"));
         }
+
+        {
+            auto req = asRestconfStreamRequest("GET", "/streams/configured/example", "");
+            REQUIRE(std::holds_alternative<ConfiguredStreamRequest>(req));
+            REQUIRE(std::get<ConfiguredStreamRequest>(req).name == "example");
+        }
+
+        {
+            auto req = asRestconfStreamRequest("GET", "/streams/configured/example-xml", "");
+            REQUIRE(std::holds_alternative<ConfiguredStreamRequest>(req));
+            REQUIRE(std::get<ConfiguredStreamRequest>(req).name == "example-xml");
+        }
+
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/configured", ""),
+                               serializeErrorResponse(400, "protocol", "invalid-value", "Syntax error in URI (path) at position 19: expected \"/\"").c_str(),
+                               rousette::restconf::ErrorResponse);
 
         REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF", ""),
                                serializeErrorResponse(400, "protocol", "invalid-value", "Syntax error in URI (path) at position 16: expected \"/\"").c_str(),

commit 8e41fd956e9950c9d8973b72c240e4295532a2f0

uri: change the SSE proxy URIs to /streams/rousette:sse-proxy/<name>
We decided to change what we introduced in 6216ff53 ("uri: parse
/streams/configured/<name>"). This patch only renames things.

Fixes: 6216ff53 ("uri: parse /streams/configured/<name>")
Change-Id: Ia00af8c02c790244af188386932492e5269bcbd5

src/restconf/uri.cpp
@@ -84,14 +84,14 @@ const auto uuid_impl = x3::rule<class uuid_impl, std::string>{"UUID (xxxxxxxx-xx
     x3::repeat(12)[x3::xdigit];
 const auto uuid = x3::rule<class uuid, boost::uuids::uuid>{"UUID"} = uuid_impl[string_to_uuid];
 const auto subscribedStream = x3::rule<class subscribedStream, SubscribedStreamRequest>{"subscribed stream"} = x3::lit("subscribed") > x3::lit("/") > uuid;
-const auto configuredStream = x3::rule<class configuredStream, ConfiguredStreamRequest>{"configured stream"} = x3::lit("configured") > x3::lit("/") > identifier;
+const auto sseProxyStream = x3::rule<class sseProxyStream, SseProxyRequest>{"SSE proxy stream"} = x3::lit("rousette:sse-proxy") > x3::lit("/") > identifier;
 
 const auto xmlStream = x3::rule<class xmlStream, libyang::DataFormat>{"'XML'"} = x3::lit("XML") > x3::attr(libyang::DataFormat::XML);
 const auto jsonStream = x3::rule<class jsonStream, libyang::DataFormat>{"'JSON'"} = x3::lit("JSON") > x3::attr(libyang::DataFormat::JSON);
 const auto streamFormat = x3::rule<class streamFormat, libyang::DataFormat>{"stream format ('XML' or 'JSON')"} = (xmlStream | jsonStream);
 const auto notificationStream = x3::rule<class notificationStream, NotificationStreamRequest>{"notification stream"} = identifier > x3::lit("/") > streamFormat;
-const auto streamGrammar = x3::rule<class streamGrammar, std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>>{"stream URI"} =
-    x3::lit("/") > x3::lit("streams") > x3::lit("/") > (subscribedStream | configuredStream | notificationStream);
+const auto streamGrammar = x3::rule<class streamGrammar, std::variant<NotificationStreamRequest, SubscribedStreamRequest, SseProxyRequest>>{"stream URI"} =
+    x3::lit("/") > x3::lit("streams") > x3::lit("/") > (subscribedStream | sseProxyStream | notificationStream);
 
 // clang-format on
 }
@@ -263,9 +263,9 @@ queryParams::QueryParams parseQueryParams(const std::string& querystring, const 
     return parse<queryParams::QueryParams, UriQuerySyntaxError>(querystring, queryParamGrammar, pathLength);
 }
 
-std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest> parseStreamUri(const std::string& input)
+std::variant<NotificationStreamRequest, SubscribedStreamRequest, SseProxyRequest> parseStreamUri(const std::string& input)
 {
-    return parse<std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>, UriPathSyntaxError>(input, streamGrammar);
+    return parse<std::variant<NotificationStreamRequest, SubscribedStreamRequest, SseProxyRequest>, UriPathSyntaxError>(input, streamGrammar);
 }
 
 URIPrefix::URIPrefix()
@@ -711,9 +711,9 @@ SubscribedStreamRequest::SubscribedStreamRequest(const boost::uuids::uuid& uuid)
 {
 }
 
-ConfiguredStreamRequest::ConfiguredStreamRequest() = default;
+SseProxyRequest::SseProxyRequest() = default;
 
-ConfiguredStreamRequest::ConfiguredStreamRequest(const std::string& name)
+SseProxyRequest::SseProxyRequest(const std::string& name)
     : name(name)
 {
 }

src/restconf/uri.h
@@ -214,15 +214,15 @@ struct SubscribedStreamRequest {
     SubscribedStreamRequest(const boost::uuids::uuid& uuid);
 };
 
-/** @brief A request for a server-configured stream served under /streams/configured/<name>. */
-struct ConfiguredStreamRequest {
+/** @brief A request for a stream of a configured subscription, served under /streams/rousette:sse-proxy/<name>. */
+struct SseProxyRequest {
     std::string name;
 
-    ConfiguredStreamRequest();
-    ConfiguredStreamRequest(const std::string& name);
+    SseProxyRequest();
+    SseProxyRequest(const std::string& name);
 };
 
-using RestconfStreamRequest = std::variant<NotificationStreamRequest, SubscribedStreamRequest, ConfiguredStreamRequest>;
+using RestconfStreamRequest = std::variant<NotificationStreamRequest, SubscribedStreamRequest, SseProxyRequest>;
 
 RestconfRequest asRestconfRequest(const libyang::Context& ctx, const std::string& httpMethod, const std::string& uriPath, const std::string& uriQueryString = "");
 std::optional<libyang::SchemaNode> asLibyangSchemaNode(const libyang::Context& ctx, const std::vector<PathSegment>& pathSegments);

src/restconf/uri_impl.h
@@ -68,7 +68,7 @@ BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ApiIdentifier, prefix, identifier)
 
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::NotificationStreamRequest, stream, encoding);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::SubscribedStreamRequest, uuid);
-BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::ConfiguredStreamRequest, name);
+BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::SseProxyRequest, name);
 
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::queryParams::fields::ParenExpr, lhs, rhs);
 BOOST_FUSION_ADAPT_STRUCT(rousette::restconf::queryParams::fields::SlashExpr, lhs, rhs);

tests/uri-parser.cpp
@@ -1108,7 +1108,7 @@ TEST_CASE("URI path parser")
     {
         using rousette::restconf::asRestconfStreamRequest;
         using rousette::restconf::NotificationStreamRequest;
-        using rousette::restconf::ConfiguredStreamRequest;
+        using rousette::restconf::SseProxyRequest;
         using rousette::restconf::SubscribedStreamRequest;
 
         {
@@ -1142,19 +1142,19 @@ TEST_CASE("URI path parser")
         }
 
         {
-            auto req = asRestconfStreamRequest("GET", "/streams/configured/example", "");
-            REQUIRE(std::holds_alternative<ConfiguredStreamRequest>(req));
-            REQUIRE(std::get<ConfiguredStreamRequest>(req).name == "example");
+            auto req = asRestconfStreamRequest("GET", "/streams/rousette:sse-proxy/example", "");
+            REQUIRE(std::holds_alternative<SseProxyRequest>(req));
+            REQUIRE(std::get<SseProxyRequest>(req).name == "example");
         }
 
         {
-            auto req = asRestconfStreamRequest("GET", "/streams/configured/example-xml", "");
-            REQUIRE(std::holds_alternative<ConfiguredStreamRequest>(req));
-            REQUIRE(std::get<ConfiguredStreamRequest>(req).name == "example-xml");
+            auto req = asRestconfStreamRequest("GET", "/streams/rousette:sse-proxy/example-xml", "");
+            REQUIRE(std::holds_alternative<SseProxyRequest>(req));
+            REQUIRE(std::get<SseProxyRequest>(req).name == "example-xml");
         }
 
-        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/configured", ""),
-                               serializeErrorResponse(400, "protocol", "invalid-value", "Syntax error in URI (path) at position 19: expected \"/\"").c_str(),
+        REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/rousette:sse-proxy", ""),
+                               serializeErrorResponse(400, "protocol", "invalid-value", "Syntax error in URI (path) at position 27: expected \"/\"").c_str(),
                                rousette::restconf::ErrorResponse);
 
         REQUIRE_THROWS_WITH_AS(asRestconfStreamRequest("GET", "/streams/NETCONF", ""),